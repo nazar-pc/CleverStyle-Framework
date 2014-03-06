4504778c642ddd87b6a446d35788eb3795287427
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-03-06 19:41:48 +0100

    New protected method Mail::body_normalization() that will allow email customization.
    System version 0.475
    modules:
    Blogs 0.088
    Comments 0.024
    Cron 0.008
    Deferred tasks 0.1.2+build-5
    Disqus 1.0.1+build-3
    Feedback 0.006
    HybridAuth 2.1.1-cs39
    OAuth2 0.032
    Photo_gallery 0.024
    Plupload 2.1.1-cs1
    Static_pages 0.042
    plugins:
    Admin_default_theme 0.005
    Fotorama 4.4.9-cs1
    SimpleImage 2.5.1-cs1
    TinyMCE 4.0.16-cs1

--- core/classes/Mail.php
+++ core/classes/Mail.php
@@ -34,6 +34,34 @@ class Mail extends PHPMailer {
 		$this->IsHTML();
 	}
 	/**
+	 * @param string	$body
+	 * @param string	$signature
+	 *
+	 * @return string
+	 */
+	protected function body_normalization ($body, $signature) {
+		if (strpos($body, '<!doctype') === 0 && strpos($body, '<body') !== false) {
+			$body	= "<!doctype html>\n$body";
+		}
+		if (strpos($body, '<html') === false) {
+			if (substr($body, 0, 5) != '<body') {
+				$body = h::body($body.$signature);
+			} else {
+				$body = str_replace('</body>', "$signature</body>", $body);
+			}
+			$body = h::html(
+				h::{'head meta'}([
+					'content'		=> 'text/html; charset=utf-8',
+					'http-equiv'	=> 'Content-Type'
+				]).
+				$body
+			);
+		} else {
+			$body = str_replace('</body>', "$signature</body>", $body);
+		}
+		return $body;
+	}
+	/**
 	 * Sending of email
 	 *
 	 * @param array|string|string[]			$email			if emails without names - string (may be several emails separated by comma) or
@@ -83,23 +111,7 @@ class Mail extends PHPMailer {
 		} else {
 			$signature = '';
 		}
-		if (substr($body, 0, 5) != '<html') {
-			if (substr($body, 0, 5) != '<body') {
-				$body = h::body($body.$signature);
-			} else {
-				$body = str_replace('</body>', "$signature</body>", $body);
-			}
-			$body = h::html(
-				h::{'head meta'}([
-					'content'		=> 'text/html; charset=utf-8',
-					'http-equiv'	=> 'Content-Type'
-				]).
-				$body
-			);
-		} else {
-			$body = str_replace('</body>', "$signature</body>", $body);
-		}
-		$this->Body = $body;
+		$this->Body = $this->body_normalization($body, $signature);
 		if ($body_text) {
 			$this->AltBody = $body_text.strip_tags($signature);
 		}
