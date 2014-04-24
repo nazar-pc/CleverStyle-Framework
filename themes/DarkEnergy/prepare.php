7476b74d5fb6801d5dc819307dbab771b42704db
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-04-24 03:28:50 +0300

    New responsive theme DarkEnergy. Theme originally created for CleverStyle.org website.

---
+++ themes/DarkEnergy/prepare.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package		ClevereStyle CMS
+ * @subpackage	DarkEnergy theme
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+namespace	cs;
+$Page	= Page::instance();
+if (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/msie|trident/i',$_SERVER['HTTP_USER_AGENT'])) {
+	$Page->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+}
+$Page->Head	.= '<meta name="viewport" content="width=device-width">';
