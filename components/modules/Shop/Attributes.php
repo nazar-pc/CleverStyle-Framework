48b4274a701aea12da1ce76f257a33dc73770fce
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-12-05 16:21:11 +0100

    `cs\modules\Shop\Tags` class added

--- components/modules/Shop/Attributes.php
+++ components/modules/Shop/Attributes.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package        Shop
- * @category       modules
- * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright      Copyright (c) 2014, Nazar Mokrynskyi
- * @license        MIT License, see license.txt
+ * @package   Shop
+ * @category  modules
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2014, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
 namespace cs\modules\Shop;
 use
