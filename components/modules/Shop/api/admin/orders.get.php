30195351c08a2bdb8ddcfe2aa919014abadddf21
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-12-11 09:08:12 +0100

    Order items getting and putting admin API.

--- components/modules/Shop/api/admin/orders.get.php
+++ components/modules/Shop/api/admin/orders.get.php
@@ -14,7 +14,11 @@ use
 $Index          = Index::instance();
 $Page           = Page::instance();
 $Orders = Orders::instance();
-if (isset($Index->route_ids[0])) {
+if (isset($Index->route_ids[0], $Index->route_path[2]) && $Index->route_path[2] == 'items') {
+	$Page->json(
+		$Orders->get_items($Index->route_ids[0])
+	);
+} elseif (isset($Index->route_ids[0])) {
 	$orders = $Orders->get($Index->route_ids[0]);
 	if (!$orders) {
 		error_code(404);
--- components/modules/Shop/api/admin/orders.put.php
+++ components/modules/Shop/api/admin/orders.put.php
@@ -12,27 +12,49 @@ use
 	cs\Page;
 
 $Index = Index::instance();
-if (!isset(
-	$Index->route_ids[0],
-	$_POST['user'],
-	$_POST['shipping_type'],
-	$_POST['shipping_phone'],
-	$_POST['shipping_address'],
-	$_POST['status'],
-	$_POST['comment']
-)) {
+if (!isset($Index->route_ids[0])) {
 	error_code(400);
 	return;
 }
-$result = Orders::instance()->set(
-	$Index->route_ids[0],
-	$_POST['user'],
-	$_POST['shipping_type'],
-	$_POST['shipping_phone'],
-	$_POST['shipping_address'],
-	$_POST['status'],
-	$_POST['comment']
-);
+$Orders		= Orders::instance();
+$order_id	= $Index->route_ids[0];
+if (isset($Index->route_path[2]) && $Index->route_path[2] == 'items') {
+	if (!isset($_POST['items']) || empty($_POST['items'])) {
+		error_code(400);
+		return;
+	}
+	$items	= $Orders->get_items($order_id);
+	foreach (array_column($items, 'item') as $item) {
+		$Orders->del($order_id, $item);
+	}
+	unset($items, $item);
+	$result = true;
+	$items	= array_map(null, $_POST['items']['item'], $_POST['items']['units'], $_POST['items']['price'], $_POST['items']['unit_price']);
+	foreach ($items as $item) {
+		$result	= $Orders->add_item($order_id, $item[0], $item[1], $item[2], $item[3]) && $result;
+	}
+} else {
+	if (!isset(
+		$_POST['user'],
+		$_POST['shipping_type'],
+		$_POST['shipping_phone'],
+		$_POST['shipping_address'],
+		$_POST['status'],
+		$_POST['comment']
+	)) {
+		error_code(400);
+		return;
+	}
+	$result = $Orders->set(
+		$order_id,
+		$_POST['user'],
+		$_POST['shipping_type'],
+		$_POST['shipping_phone'],
+		$_POST['shipping_address'],
+		$_POST['status'],
+		$_POST['comment']
+	);
+}
 if (!$result) {
 	error_code(500);
 	return;
