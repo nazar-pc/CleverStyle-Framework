c30bd474e4110fcc31d6568d0bc8762b522430f6
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-01-05 01:37:35 +0100

    `[prefix]texts` and `[prefix]texts_data` tables should be installed with Shop module in target DB doesn't have it already.

--- components/modules/Shop/meta/install_db/shop/MySQLi.sql
+++ components/modules/Shop/meta/install_db/shop/MySQLi.sql
@@ -110,6 +110,23 @@ CREATE TABLE IF NOT EXISTS `[prefix]shop_tags` (
 	`text` varchar(1024) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `[prefix]texts` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`label` varchar(1024) NOT NULL,
+	`group` varchar(1024) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `label` (`label`(255),`group`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `[prefix]texts_data` (
+	`id` bigint(20) NOT NULL COMMENT 'id from texts table',
+	`id_` varchar(25) NOT NULL,
+	`lang` varchar(2) NOT NULL,
+	`text` mediumtext NOT NULL,
+	PRIMARY KEY (`id`,`lang`),
+	KEY `id_` (`id_`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 ALTER TABLE `[prefix]shop_attributes`
 ADD PRIMARY KEY (`id`), ADD KEY `type` (`type`), ADD KEY `title_internal` (`title_internal`(255));
 
