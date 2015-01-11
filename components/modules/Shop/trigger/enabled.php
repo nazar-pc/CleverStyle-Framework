a48bfc5b4ead6f81f5e8a0909a8d7c9425f49d7a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-01-11 04:54:49 +0100

    Update paid date

--- components/modules/Shop/trigger/enabled.php
+++ components/modules/Shop/trigger/enabled.php
@@ -23,13 +23,28 @@ Trigger::instance()
 			if ($purpose[0] != 'orders') {
 				return;
 			}
-			$Config  = Config::instance();
-			Orders::instance()->set_status(
+			$Config = Config::instance();
+			$Orders = Orders::instance();
+			$order  = $Orders->get($purpose[1]);
+			$Orders->set(
+				$order['id'],
+				$order['user'],
+				$order['shipping_type'],
+				$order['shipping_cost'],
+				$order['shipping_username'],
+				$order['shipping_phone'],
+				$order['shipping_address'],
+				$order['payment_method'],
+				1,
+				$order['status'],
+				$order['comment']
+			);
+			$Orders->set_status(
 				$purpose[1],
 				$Config->module('Shop')->default_paid_order_status,
 				'' // TODO: possibly some comment message here
 			);
-			$L       = new Prefix('shop_');
+			$L = new Prefix('shop_');
 			interface_off();
 			header('Location: '.$Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?paid='.(int)$purpose[1]); // TODO: handle successful payment in interface
 		}
@@ -44,8 +59,8 @@ Trigger::instance()
 			if ($purpose[0] != 'orders') {
 				return;
 			}
-			$Config  = Config::instance();
-			$L       = new Prefix('shop_');
+			$Config = Config::instance();
+			$L      = new Prefix('shop_');
 			interface_off();
 			header('Location: '.$Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?error='.(int)$purpose[1]); // TODO: handle failed payment in interface
 		}
