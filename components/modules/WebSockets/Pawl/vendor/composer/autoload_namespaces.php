392b27623d48c8d1e679872e889f2d718a04ce17
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-01-25 21:19:39 +0100

    Manually removed Symfony components since they are not used anywhere and have significant size

--- components/modules/WebSockets/Pawl/vendor/composer/autoload_namespaces.php
+++ components/modules/WebSockets/Pawl/vendor/composer/autoload_namespaces.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Symfony\\Component\\Routing\\' => array($vendorDir . '/symfony/routing'),
-    'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
-    'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
     'Ratchet' => array($vendorDir . '/cboden/ratchet/src'),
     'Guzzle\\Stream' => array($vendorDir . '/guzzle/stream'),
     'Guzzle\\Parser' => array($vendorDir . '/guzzle/parser'),
