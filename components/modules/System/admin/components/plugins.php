a787dede82ff16796d3abac26f21f3c6f732908e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-01-26 18:37:47 +0100

    Reformatting

--- components/modules/System/admin/components/plugins.php
+++ components/modules/System/admin/components/plugins.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @subpackage	System module
- * @category	modules
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package    CleverStyle CMS
+ * @subpackage System module
+ * @category   modules
+ * @author     Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright  Copyright (c) 2011-2015, Nazar Mokrynskyi
+ * @license    MIT License, see license.txt
  */
-namespace	cs\modules\System;
+namespace cs\modules\System;
 use
 	h,
 	cs\Config,
@@ -15,13 +15,13 @@ use
 	cs\Language,
 	cs\Page,
 	cs\User;
-$Config			= Config::instance();
-$L				= Language::instance();
-$Page			= Page::instance();
-$a				= Index::instance();
-$rc				= $Config->route;
-$plugins		= get_files_list(PLUGINS, false, 'd');
-$a->buttons		= false;
+$Config     = Config::instance();
+$L          = Language::instance();
+$Page       = Page::instance();
+$a          = Index::instance();
+$rc         = $Config->route;
+$plugins    = get_files_list(PLUGINS, false, 'd');
+$a->buttons = false;
 if (
 	isset($rc[2]) &&
 	!empty($rc[2]) &&
@@ -56,31 +56,31 @@ if (
 				}
 				$tmp_file = TEMP.'/'.md5($_FILES['upload_plugin']['tmp_name'].MICROTIME).'.phar';
 				move_uploaded_file($_FILES['upload_plugin']['tmp_name'], $tmp_file);
-				$tmp_dir	= "phar://$tmp_file";
-				$plugin		= file_get_contents("$tmp_dir/dir");
+				$tmp_dir = "phar://$tmp_file";
+				$plugin  = file_get_contents("$tmp_dir/dir");
 				if (!$plugin) {
 					unlink($tmp_file);
 					break;
 				}
-				$rc[3]		= $plugin;
+				$rc[3] = $plugin;
 				if (!file_exists("$tmp_dir/meta.json") || file_get_json("$tmp_dir/meta.json")['category'] != 'plugins') {
 					$Page->warning($L->this_is_not_plugin_installer_file);
 					unlink($tmp_file);
 					break;
 				}
 				if (in_array($plugin, $Config->components['plugins'])) {
-					$current_version		= file_get_json(PLUGINS."/$plugin/meta.json")['version'];
-					$new_version			= file_get_json("$tmp_dir/meta.json")['version'];
+					$current_version = file_get_json(PLUGINS."/$plugin/meta.json")['version'];
+					$new_version     = file_get_json("$tmp_dir/meta.json")['version'];
 					if (!version_compare($current_version, $new_version, '<')) {
 						$Page->warning($L->update_plugin_impossible_older_version($plugin));
 						unlink($tmp_file);
 						break;
 					}
-					$check_dependencies		= check_dependencies($plugin, 'plugin', $tmp_dir);
+					$check_dependencies = check_dependencies($plugin, 'plugin', $tmp_dir);
 					if (!$check_dependencies && $Config->core['simple_admin_mode']) {
 						break;
 					}
-					$rc[2]					= 'update';
+					$rc[2] = 'update';
 					$Page->title($L->updating_of_plugin($plugin));
 					rename($tmp_file, $tmp_file = TEMP.'/'.User::instance()->get_session_id().'_plugin_update.phar.php');
 					$a->content(
@@ -92,15 +92,15 @@ if (
 							)
 						).
 						h::{'input[type=hidden]'}([
-							'name'	=> 'mode',
-							'value'	=> $rc[2]
+							'name'  => 'mode',
+							'value' => $rc[2]
 						]).
 						h::{'input[type=hidden]'}([
-							'name'	=> 'plugin',
-							'value'	=> $rc[3]
+							'name'  => 'plugin',
+							'value' => $rc[3]
 						])
 					);
-					$a->cancel_button_back	= true;
+					$a->cancel_button_back = true;
 					$a->content(
 						h::{'button.uk-button[type=submit]'}($L->{$check_dependencies ? $L->yes : 'force_update_not_recommended'})
 					);
@@ -111,8 +111,8 @@ if (
 					unlink($tmp_file);
 					break;
 				}
-				$fs				= file_get_json("$tmp_dir/fs.json");
-				$extract		= array_product(
+				$fs      = file_get_json("$tmp_dir/fs.json");
+				$extract = array_product(
 					array_map(
 						function ($index, $file) use ($tmp_dir, $plugin) {
 							if (
@@ -134,7 +134,7 @@ if (
 					break;
 				}
 				unlink($tmp_file);
-				$plugins[]		= $plugin;
+				$plugins[] = $plugin;
 				unset($tmp_file, $plugin);
 			}
 			if (!in_array($rc[3], $Config->components['plugins']) && in_array($rc[3], $plugins)) {
@@ -144,12 +144,12 @@ if (
 						$L->enabling_of_plugin($rc[3])
 					)
 				);
-				$check_dependencies		= check_dependencies($rc[3], 'plugin');
+				$check_dependencies = check_dependencies($rc[3], 'plugin');
 				if (!$check_dependencies && $Config->core['simple_admin_mode']) {
 					break;
 				}
 				if (file_exists(PLUGINS."/$rc[3]/meta.json")) {
-					$meta	= file_get_json(PLUGINS."/$rc[3]/meta.json");
+					$meta = file_get_json(PLUGINS."/$rc[3]/meta.json");
 					if (isset($meta['optional'])) {
 						$Page->success(
 							$L->for_complete_feature_set(
@@ -159,23 +159,23 @@ if (
 					}
 					unset($meta);
 				}
-				$a->cancel_button_back	= true;
+				$a->cancel_button_back = true;
 				$a->content(
 					h::{'button.uk-button[type=submit]'}(
 						$L->{$check_dependencies ? 'enable' : 'force_enable_not_recommended'}
 					).
 					h::{'input[type=hidden]'}([
-						'name'	=> 'mode',
-						'value'	=> $rc[2]
+						'name'  => 'mode',
+						'value' => $rc[2]
 					]).
 					h::{'input[type=hidden]'}([
-						'name'	=> 'plugin',
-						'value'	=> $rc[3]
+						'name'  => 'plugin',
+						'value' => $rc[3]
 					])
 				);
 				return;
 			}
-		break;
+			break;
 		case 'disable':
 			if (in_array($rc[3], $Config->components['plugins'])) {
 				$Page->title($L->disabling_of_plugin($rc[3]));
@@ -184,27 +184,27 @@ if (
 						$L->disabling_of_plugin($rc[3])
 					)
 				);
-				$check_dependencies		= check_backward_dependencies($rc[3], 'plugin');
+				$check_dependencies = check_backward_dependencies($rc[3], 'plugin');
 				if (!$check_dependencies && $Config->core['simple_admin_mode']) {
 					break;
 				}
-				$a->cancel_button_back	= true;
+				$a->cancel_button_back = true;
 				$a->content(
 					h::{'button.uk-button[type=submit]'}(
 						$L->{$check_dependencies ? 'disable' : 'force_disable_not_recommended'}
 					).
 					h::{'input[type=hidden]'}([
-						'name'	=> 'mode',
-						'value'	=> $rc[2]
+						'name'  => 'mode',
+						'value' => $rc[2]
 					]).
 					h::{'input[type=hidden]'}([
-						'name'	=> 'plugin',
-						'value'	=> $rc[3]
+						'name'  => 'plugin',
+						'value' => $rc[3]
 					])
 				);
 			}
 			return;
-		break;
+			break;
 		case 'remove':
 			$Page->title($L->complete_removal_of_plugin($_POST['remove_plugin']));
 			$a->content(
@@ -212,26 +212,26 @@ if (
 					$L->completely_remove_plugin($_POST['remove_plugin'])
 				)
 			);
-			$a->cancel_button_back	= true;
+			$a->cancel_button_back = true;
 			$a->content(
 				h::{'button.uk-button[type=submit]'}($L->yes).
 				h::{'input[type=hidden]'}([
-					'name'	=> 'mode',
-					'value'	=> $rc[2]
+					'name'  => 'mode',
+					'value' => $rc[2]
 				]).
 				h::{'input[type=hidden]'}([
-					'name'	=> 'plugin',
-					'value'	=> $_POST['remove_plugin']
+					'name'  => 'plugin',
+					'value' => $_POST['remove_plugin']
 				])
 			);
 			return;
-		break;
+			break;
 	}
 }
 unset($rc);
-$a->buttons		= false;
-$a->file_upload	= true;
-$plugins_list	= [];
+$a->buttons     = false;
+$a->file_upload = true;
+$plugins_list   = [];
 if (!empty($plugins)) {
 	foreach ($plugins as $plugin) {
 		$addition_state = $action = '';
@@ -240,24 +240,24 @@ if (!empty($plugins)) {
 		 */
 		if (file_exists($file = PLUGINS."/$plugin/readme.txt") || file_exists($file = PLUGINS."/$plugin/readme.html")) {
 			if (substr($file, -3) == 'txt') {
-				$tag		= 'pre';
+				$tag = 'pre';
 			} else {
 				$tag = 'div';
 			}
-			$uniqid			= uniqid('module_info_');
+			$uniqid = uniqid('module_info_');
 			$Page->replace($uniqid, $tag == 'pre' ? prepare_attr_value(file_get_contents($file)) : file_get_contents($file));
 			$addition_state .= h::{'div.uk-modal.cs-left'}(
 				h::{"$tag.uk-modal-dialog-large"}($uniqid),
 				[
-					'id'			=> "{$plugin}_readme",
-					'title'			=> "$plugin -> $L->information_about_plugin"
+					'id'    => "{$plugin}_readme",
+					'title' => "$plugin -> $L->information_about_plugin"
 				]
 			).
 			h::{'icon.cs-pointer'}(
 				'exclamation',
 				[
-					'data-title'	=> $L->information_about_plugin.h::br().$L->click_to_view_details,
-					'onClick'		=> "$('#{$plugin}_readme').cs().modal('show');"
+					'data-title' => $L->information_about_plugin.h::br().$L->click_to_view_details,
+					'onClick'    => "$('#{$plugin}_readme').cs().modal('show');"
 				]
 			);
 			unset($uniqid);
@@ -275,15 +275,15 @@ if (!empty($plugins)) {
 			$addition_state .= h::{'div.uk-modal.cs-left'}(
 				h::{"$tag.uk-modal-dialog-large"}($tag == 'pre' ? prepare_attr_value(file_get_contents($file)) : file_get_contents($file)),
 				[
-					'id'			=> "{$plugin}_license",
-					'title'			=> "$plugin -> $L->license"
+					'id'    => "{$plugin}_license",
+					'title' => "$plugin -> $L->license"
 				]
 			).
 			h::{'icon.cs-pointer'}(
 				'legal',
 				[
-					'data-title'	=> $L->license.h::br().$L->click_to_view_details,
-					'onClick'		=> "$('#{$plugin}_license').cs().modal('show');"
+					'data-title' => $L->license.h::br().$L->click_to_view_details,
+					'onClick'    => "$('#{$plugin}_license').cs().modal('show');"
 				]
 			);
 		}
@@ -292,14 +292,14 @@ if (!empty($plugins)) {
 		$action .= h::{'a.uk-button.cs-button-compact'}(
 			h::icon($state ? 'minus' : 'check'),
 			[
-				'href'			=> $a->action.($state ? '/disable/' : '/enable/').$plugin,
-				'data-title'	=> $state ? $L->disable : $L->enable
+				'href'       => $a->action.($state ? '/disable/' : '/enable/').$plugin,
+				'data-title' => $state ? $L->disable : $L->enable
 			]
 		);
-		$plugin_info	= false;
+		$plugin_info = false;
 		if (file_exists(PLUGINS."/$plugin/meta.json")) {
-			$plugin_meta	= file_get_json(PLUGINS."/$plugin/meta.json");
-			$plugin_info	= $L->plugin_info(
+			$plugin_meta = file_get_json(PLUGINS."/$plugin/meta.json");
+			$plugin_info = $L->plugin_info(
 				$plugin_meta['package'],
 				$plugin_meta['version'],
 				$plugin_meta['description'],
@@ -316,25 +316,25 @@ if (!empty($plugins)) {
 			);
 		}
 		unset($plugin_meta);
-		$plugins_list[]	= [
+		$plugins_list[] = [
 			[
 				h::span(
 					$L->$plugin,
 					[
-						'data-title'	=> $plugin_info
+						'data-title' => $plugin_info
 					]
 				),
 				h::icon(
 					$state ? 'check' : 'minus',
 					[
-						'data-title'	=> $state ? $L->enabled : $L->disabled
+						'data-title' => $state ? $L->enabled : $L->disabled
 					]
 				).
 				$addition_state,
 				$action
 			],
 			[
-				'class'	=> $state ? 'uk-alert-success' : 'uk-alert-warning'
+				'class' => $state ? 'uk-alert-success' : 'uk-alert-warning'
 			]
 		];
 		unset($plugin_info);
@@ -358,26 +358,26 @@ $a->content(
 	).
 	h::p(
 		h::{'input[type=file][name=upload_plugin]'}([
-			'style'	=> 'position: relative;'
+			'style' => 'position: relative;'
 		]).
 		h::{'button.uk-button[type=submit]'}(
 			h::icon('upload').$L->upload_and_install_update_plugin,
 			[
-				'formaction'	=>  "$a->action/enable/upload"
+				'formaction' => "$a->action/enable/upload"
 			]
 		)
 	).
 	(
-		$plugins_for_removal
-			? h::p(
-				h::{'select[name=remove_plugin]'}($plugins_for_removal).
-				h::{'button.uk-button[type=submit]'}(
-					h::icon('trash-o').$L->complete_plugin_removal,
-					[
-						'formaction'	=>  "$a->action/remove"
-					]
-				)
-			)
-			: ''
+	$plugins_for_removal
+		? h::p(
+		h::{'select[name=remove_plugin]'}($plugins_for_removal).
+		h::{'button.uk-button[type=submit]'}(
+			h::icon('trash-o').$L->complete_plugin_removal,
+			[
+				'formaction' => "$a->action/remove"
+			]
+		)
+	)
+		: ''
 	)
 );
