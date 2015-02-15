d30a6fe484900f2cbd6ca1b3d35859e83b3fbbd3
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-02-15 05:25:00 +0100

    Small fix in Blogs module

--- components/modules/Blogs/prepare.php
+++ components/modules/Blogs/prepare.php
@@ -16,31 +16,33 @@ use
 	cs\Page,
 	cs\User;
 if (!api_path()) {
-	function get_sections_select_post (&$disabled, $current = null, $structure = null, $level = 0) {
-		$list	= [
-			'in'	=> [],
-			'value'	=> []
-		];
-		if ($structure === null) {
-			$structure			= Blogs::instance()->get_sections_structure();
-			$list['in'][]		= Language::instance()->root_section;
-			$list['value'][]	= 0;
-		} else {
-			if ($structure['id'] == $current) {
-				return $list;
+	if (!function_exists(__NAMESPACE__.'\\get_sections_select_post')) {
+		function get_sections_select_post (&$disabled, $current = null, $structure = null, $level = 0) {
+			$list	= [
+				'in'	=> [],
+				'value'	=> []
+			];
+			if ($structure === null) {
+				$structure			= Blogs::instance()->get_sections_structure();
+				$list['in'][]		= Language::instance()->root_section;
+				$list['value'][]	= 0;
+			} else {
+				if ($structure['id'] == $current) {
+					return $list;
+				}
+				$list['in'][]		= str_repeat('&nbsp;', $level).$structure['title'];
+				$list['value'][]	= $structure['id'];
 			}
-			$list['in'][]		= str_repeat('&nbsp;', $level).$structure['title'];
-			$list['value'][]	= $structure['id'];
-		}
-		if (!empty($structure['sections'])) {
-			$disabled[]			= $structure['id'];
-			foreach ($structure['sections'] as $section) {
-				$tmp			= get_sections_select_post($disabled, $current, $section, $level+1);
-				$list['in']		= array_merge($list['in'], $tmp['in']);
-				$list['value']	= array_merge($list['value'], $tmp['value']);
+			if (!empty($structure['sections'])) {
+				$disabled[]			= $structure['id'];
+				foreach ($structure['sections'] as $section) {
+					$tmp			= get_sections_select_post($disabled, $current, $section, $level+1);
+					$list['in']		= array_merge($list['in'], $tmp['in']);
+					$list['value']	= array_merge($list['value'], $tmp['value']);
+				}
 			}
+			return $list;
 		}
-		return $list;
 	}
 	if (!function_exists(__NAMESPACE__.'\\get_posts_list')) {
 		function get_posts_list ($posts) {
@@ -128,44 +130,46 @@ if (!api_path()) {
 			return h::article($content);
 		}
 	}
-	function head_actions () {
-		$User	= User::instance();
-		if ($User->user()) {
-			$Index					= Index::instance();
-			$L						= Language::instance();
-			$module					= path($L->Blogs);
-			$module_data	= Config::instance()->module('Blogs');
-			/**
-			 * If administrator
-			 */
-			if ($User->admin() && $User->get_permission('admin/Blogs', 'index')) {
+	if (!function_exists(__NAMESPACE__.'\\head_actions')) {
+		function head_actions () {
+			$User	= User::instance();
+			if ($User->user()) {
+				$Index					= Index::instance();
+				$L						= Language::instance();
+				$module					= path($L->Blogs);
+				$module_data	= Config::instance()->module('Blogs');
+				/**
+				 * If administrator
+				 */
+				if ($User->admin() && $User->get_permission('admin/Blogs', 'index')) {
+					$Index->content(
+						h::{'a.uk-button'}(
+							h::icon('gears'),
+							[
+								'href'			=> 'admin/Blogs',
+								'data-title'	=> $L->administration
+							]
+						)
+					);
+				}
 				$Index->content(
-					h::{'a.uk-button'}(
-						h::icon('gears'),
+					$User->admin() || !$module_data->new_posts_only_from_admins ? h::{'a.uk-button'}(
+						h::icon('pencil').$L->new_post,
 						[
-							'href'			=> 'admin/Blogs',
-							'data-title'	=> $L->administration
+							'href'			=> "$module/new_post",
+							'data-title'	=> $L->new_post
 						]
-					)
+					).
+					h::{'a.uk-button'}(
+						h::icon('archive').$L->drafts,
+						[
+							'href'			=> "$module/".path($L->drafts),
+							'data-title'	=> $L->drafts
+						]
+					).
+					h::br() : ''
 				);
 			}
-			$Index->content(
-				$User->admin() || !$module_data->new_posts_only_from_admins ? h::{'a.uk-button'}(
-					h::icon('pencil').$L->new_post,
-					[
-						'href'			=> "$module/new_post",
-						'data-title'	=> $L->new_post
-					]
-				).
-				h::{'a.uk-button'}(
-					h::icon('archive').$L->drafts,
-					[
-						'href'			=> "$module/".path($L->drafts),
-						'data-title'	=> $L->drafts
-					]
-				).
-				h::br() : ''
-			);
 		}
 	}
 }
