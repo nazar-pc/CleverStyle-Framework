2dbfc7af1b9ff37735814378b91c93c03257072c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-03-22 02:36:51 +0100

    Additional parameter in payment confirmation event - callback, to avoid redirects when it is not desirable. Repeated payment confirmation will have no effect, so can be freely executed as many times as needed.

--- components/modules/Shop/events/enabled.php
+++ components/modules/Shop/events/enabled.php
@@ -25,27 +25,28 @@ Event::instance()
 			$Config = Config::instance();
 			$Orders = Orders::instance();
 			$order  = $Orders->get($purpose[1]);
-			$Orders->set(
-				$order['id'],
-				$order['user'],
-				$order['shipping_type'],
-				$order['shipping_cost'],
-				$order['shipping_username'],
-				$order['shipping_phone'],
-				$order['shipping_address'],
-				$order['payment_method'],
-				1,
-				$order['status'],
-				$order['comment']
-			);
-			$Orders->set_status(
-				$purpose[1],
-				$Config->module('Shop')->default_paid_order_status,
-				'' // TODO: possibly some comment message here
-			);
+			if (!$order['paid']) {
+				$Orders->set(
+					$order['id'],
+					$order['user'],
+					$order['shipping_type'],
+					$order['shipping_cost'],
+					$order['shipping_username'],
+					$order['shipping_phone'],
+					$order['shipping_address'],
+					$order['payment_method'],
+					1,
+					$order['status'],
+					$order['comment']
+				);
+				$Orders->set_status(
+					$purpose[1],
+					$Config->module('Shop')->default_paid_order_status,
+					'' // TODO: possibly some comment message here
+				);
+			}
 			$L = new Prefix('shop_');
-			interface_off();
-			_header('Location: '.$Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?paid_success='.(int)$purpose[1]);
+			$data['callback'] = $Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?paid_success='.(int)$purpose[1];
 		}
 	)
 	->on(
@@ -60,7 +61,6 @@ Event::instance()
 			}
 			$Config = Config::instance();
 			$L      = new Prefix('shop_');
-			interface_off();
-			_header('Location: '.$Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?paid_error='.(int)$purpose[1]);
+			$data['callback'] = $Config->core_url().'/'.path($L->shop).'/'.path($L->orders).'/?paid_error='.(int)$purpose[1];
 		}
 	);
