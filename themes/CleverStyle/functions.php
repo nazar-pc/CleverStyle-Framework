225c8b8484e460ab06ba51b446a2730e860ab447
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-03-27 02:53:31 +0100

    Support of new `meta.json` option for hiding module in main menu

--- themes/CleverStyle/functions.php
+++ themes/CleverStyle/functions.php
@@ -50,9 +50,10 @@ function get_main_menu () {
 	 */
 	foreach ($Config->components['modules'] as $module => $module_data) {
 		if (
+			$module != 'System' &&
 			$module_data['active'] == 1 &&
 			$module != $Config->core['default_module'] &&
-			$module != 'System' &&
+			!@file_get_json(MODULES."/$module/meta.json")['hide_in_menu'] &&
 			$User->get_permission($module, 'index') &&
 			(
 				file_exists(MODULES."/$module/index.php") ||
