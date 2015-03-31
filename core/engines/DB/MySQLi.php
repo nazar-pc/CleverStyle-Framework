4b52cc42ccc663b7257a55ca73356b71552bb0f0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-03-31 12:27:08 +0200

    Refactoring of `\cs\DB\MySQLi`

--- core/engines/DB/MySQLi.php
+++ core/engines/DB/MySQLi.php
@@ -21,20 +21,24 @@ class MySQLi extends _Abstract {
 		 */
 		$host = explode(':', $host);
 		$port = ini_get('mysqli.default_port') ?: 3306;
-		if (count($host) == 1) {
-			$host = $host[0];
-		} elseif (count($host) == 2) {
-			if ($host[0] == 'p') {
-				$host = "$host[0]:$host[1]";
-			} else {
-				$port = $host[1];
+		switch (count($host)) {
+			case 1:
 				$host = $host[0];
-			}
-		} elseif (count($host) == 3) {
-			$port = $host[2];
-			$host = "$host[0]:$host[1]";
+				break;
+			case 2:
+				if ($host[0] == 'p') {
+					$host = "$host[0]:$host[1]";
+				} else {
+					$port = $host[1];
+					$host = $host[0];
+				}
+				break;
+			case 3:
+				$port = $host[2];
+				$host = "$host[0]:$host[1]";
 		}
 		$this->instance = @new \MySQLi($host, $user, $password, $database, $port);
+		/** @noinspection NotOptimalIfConditionsInspection */
 		if (is_object($this->instance) && !$this->instance->connect_errno) {
 			$this->database = $database;
 			/**
@@ -55,29 +59,17 @@ class MySQLi extends _Abstract {
 	 * @inheritdoc
 	 */
 	protected function q_internal ($query) {
-		if ($this->async && defined('MYSQLI_ASYNC')) {
-			$result = @$this->instance->query($query, MYSQLI_ASYNC);
-			// In case of MySQL Client error - try to fix everything, but only once
-			if (
-				!$result &&
-				$this->instance->errno >= 2000 &&
-				$this->instance->ping()
-			) {
-				$result = @$this->instance->query($query, MYSQLI_ASYNC);
-			}
-			return $result;
-		} else {
-			$result = @$this->instance->query($query);
-			// In case of MySQL Client error - try to fix everything, but only once
-			if (
-				!$result &&
-				$this->instance->errno >= 2000 &&
-				$this->instance->ping()
-			) {
-				$result = @$this->instance->query($query);
-			}
-			return $result;
+		$result_mode = $this->async && defined('MYSQLI_ASYNC') ? MYSQLI_ASYNC : MYSQLI_STORE_RESULT;
+		$result      = @$this->instance->query($query, $result_mode);
+		// In case of MySQL Client error - try to fix everything, but only once
+		if (
+			!$result &&
+			$this->instance->errno >= 2000 &&
+			$this->instance->ping()
+		) {
+			$result = @$this->instance->query($query, $result_mode);
 		}
+		return $result;
 	}
 	/**
 	 * @inheritdoc
@@ -85,6 +77,7 @@ class MySQLi extends _Abstract {
 	protected function q_multi_internal ($query) {
 		$query  = implode(';', $query);
 		$return = @$this->instance->multi_query($query);
+		/** @noinspection LoopWhichDoesNotLoopInspection */
 		while ($this->instance->more_results() && $this->instance->next_result()) {
 			//Nothing, just finish multi_query
 		}
@@ -104,35 +97,29 @@ class MySQLi extends _Abstract {
 	 * @inheritdoc
 	 */
 	function f ($query_result, $single_column = false, $array = false, $indexed = false) {
-		if ($single_column) {
-			$result_type = MYSQLI_NUM;
-		} else {
-			$result_type = $indexed ? MYSQLI_NUM : MYSQLI_ASSOC;
-		}
-		if (is_object($query_result)) {
-			if ($array) {
-				$result = [];
-				if ($single_column === false) {
-					while ($current = $query_result->fetch_array($result_type)) {
-						$result[] = $current;
-					}
-				} else {
-					while ($current = $query_result->fetch_array($result_type)) {
-						$result[] = $current[0];
-					}
-				}
-				$this->free($query_result);
-				return $result;
-			} else {
-				$result = $query_result->fetch_array($result_type);
-				if ($single_column && is_array($result)) {
-					return $result[0];
-				}
-				return $result;
-			}
-		} else {
+		if (!is_object($query_result)) {
 			return false;
 		}
+		$result_type = $single_column || $indexed ? MYSQLI_NUM : MYSQLI_ASSOC;
+		if ($array) {
+			$result = [];
+			if ($single_column === false) {
+				while ($current = $query_result->fetch_array($result_type)) {
+					$result[] = $current;
+				}
+			} else {
+				while ($current = $query_result->fetch_array($result_type)) {
+					$result[] = $current[0];
+				}
+			}
+			$this->free($query_result);
+			return $result;
+		}
+		$result = $query_result->fetch_array($result_type);
+		if ($single_column) {
+			return is_array($result) ? $result[0] : false;
+		}
+		return $result;
 	}
 	/**
 	 * @inheritdoc
