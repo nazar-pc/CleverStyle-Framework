b1ccc1045e8c60cb8b2241116b078fee88047e85
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-03-31 06:57:41 +0200

    `\cs\Mail` class reformatting

--- core/classes/Mail.php
+++ core/classes/Mail.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CleverStyle CMS
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2011-2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs;
+namespace cs;
 use
 	h,
 	PHPMailer;
@@ -19,32 +19,32 @@ class Mail extends PHPMailer {
 	 * Setting base mail sending parameters according to system configuration
 	 */
 	function construct () {
-		$Config			= Config::instance();
+		$Config = Config::instance();
 		if ($Config->core['smtp']) {
 			$this->IsSMTP();
-			$this->Host			= $Config->core['smtp_host'];
-			$this->Port			= $Config->core['smtp_port'] ?: $Config->core['smtp_secure'] ? 465 : 25;
-			$this->SMTPSecure	= $Config->core['smtp_secure'];
+			$this->Host       = $Config->core['smtp_host'];
+			$this->Port       = $Config->core['smtp_port'] ?: $Config->core['smtp_secure'] ? 465 : 25;
+			$this->SMTPSecure = $Config->core['smtp_secure'];
 			if ($Config->core['smtp_auth']) {
-				$this->SMTPAuth	= true;
-				$this->Username	= $Config->core['smtp_user'];
-				$this->Password	= $Config->core['smtp_password'];
+				$this->SMTPAuth = true;
+				$this->Username = $Config->core['smtp_user'];
+				$this->Password = $Config->core['smtp_password'];
 			}
 		}
-		$this->From		= $Config->core['mail_from'];
-		$this->FromName	= get_core_ml_text('mail_from_name');
-		$this->CharSet	= 'utf-8';
+		$this->From     = $Config->core['mail_from'];
+		$this->FromName = get_core_ml_text('mail_from_name');
+		$this->CharSet  = 'utf-8';
 		$this->IsHTML();
 	}
 	/**
-	 * @param string	$body
-	 * @param string	$signature
+	 * @param string $body
+	 * @param string $signature
 	 *
 	 * @return string
 	 */
 	protected function body_normalization ($body, $signature) {
 		if (strpos($body, '<!doctype') === 0 && strpos($body, '<body') !== false) {
-			$body	= "<!doctype html>\n$body";
+			$body = "<!doctype html>\n$body";
 		}
 		if (strpos($body, '<html') === false) {
 			if (substr($body, 0, 5) != '<body') {
@@ -53,10 +53,12 @@ class Mail extends PHPMailer {
 				$body = str_replace('</body>', "$signature</body>", $body);
 			}
 			$body = h::html(
-				h::{'head meta'}([
-					'content'		=> 'text/html; charset=utf-8',
-					'http-equiv'	=> 'Content-Type'
-				]).
+				h::{'head meta'}(
+					[
+						'content'    => 'text/html; charset=utf-8',
+						'http-equiv' => 'Content-Type'
+					]
+				).
 				$body
 			);
 		} else {
@@ -67,18 +69,19 @@ class Mail extends PHPMailer {
 	/**
 	 * Sending of email
 	 *
-	 * @param array|string|string[]			$email			if emails without names - string (may be several emails separated by comma) or
-	 * 														1-dimensional array(<i>email</i>)<br>
-	 * 														else - 2-dimensional array(<i>email</i>, <i>name</i>) must be given
-	 * @param string						$subject		Mail subject
-	 * @param string						$body			html body
-	 * @param string|null					$body_text		plain text body
-	 * @param array|null|string				$attachments	1- or 2-dimensional array of array(<i>path</i>, <i>name</i>) or simply string
-	 * 														with path to the file in file system
-	 * @param array|null|string|string[]	$reply_to		Similar to <b>$email</b>
-	 * @param bool|string					$signature		<b>true</b> - add system signature<br>
-	 * 														<b>false</b> - without signature<br>
-	 * 														<b>string</b> - custom signature
+	 * @param array|string|string[]      $email       if emails without names - string (may be several emails separated by comma) or
+	 *                                                1-dimensional array(<i>email</i>)<br>
+	 *                                                else - 2-dimensional array(<i>email</i>, <i>name</i>) must be given
+	 * @param string                     $subject     Mail subject
+	 * @param string                     $body        html body
+	 * @param string|null                $body_text   plain text body
+	 * @param array|null|string          $attachments 1- or 2-dimensional array of array(<i>path</i>, <i>name</i>) or simply string with path to the file in
+	 *                                                file system
+	 * @param array|null|string|string[] $reply_to    Similar to <b>$email</b>
+	 * @param bool|string                $signature   <b>true</b> - add system signature<br>
+	 *                                                <b>false</b> - without signature<br>
+	 *                                                <b>string</b> - custom signature
+	 *
 	 * @return bool
 	 */
 	function send_to ($email, $subject, $body, $body_text = null, $attachments = null, $reply_to = null, $signature = true) {
@@ -147,7 +150,7 @@ class Mail extends PHPMailer {
 			}
 			return $emails;
 		}
-		$email	= _trim(explode(',', $email));
+		$email = _trim(explode(',', $email));
 		foreach ($email as &$e) {
 			$e = [$e, ''];
 		}
