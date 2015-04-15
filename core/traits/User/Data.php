adb74e3734df5f8897ef89a088364cf4a989db25
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-04-15 21:54:04 +0200

    Reformatting of `\cs\User\Data`

--- core/traits/User/Data.php
+++ core/traits/User/Data.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CleverStyle CMS
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2011-2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs\User;
+namespace cs\User;
 use
 	cs\Config,
 	cs\Language,
@@ -28,45 +28,48 @@ trait Data {
 	 * Copy of columns list of users table for internal needs without Cache usage
 	 * @var array
 	 */
-	protected	$users_columns	= [];
+	protected $users_columns = [];
 	/**
 	 * Do we need to update users cache, if so - array will not be empty
 	 * @var array
 	 */
-	protected	$update_cache	= [];
+	protected $update_cache = [];
 	/**
 	 * Local cache of users data
 	 * @var array
 	 */
-	protected	$data			= [];
+	protected $data = [];
 	/**
 	 * Changed users data, at the finish, data in db must be replaced by this data
 	 * @var array
 	 */
-	protected	$data_set		= [];
+	protected $data_set = [];
 	/**
 	 * Whether to use memory cache (locally, inside object, may require a lot of memory if working with many users together)
 	 * @var bool
 	 */
-	protected	$memory_cache	= true;
+	protected $memory_cache = true;
 	protected function initialize_data () {
-		$this->users_columns = $this->cache->get('columns', function () {
-			return $this->db()->columns('[prefix]users');
-		});
+		$this->users_columns = $this->cache->get(
+			'columns',
+			function () {
+				return $this->db()->columns('[prefix]users');
+			}
+		);
 	}
 	/**
 	 * Get data item of specified user
 	 *
-	 * @param string|string[]					$item
-	 * @param bool|int 							$user	If not specified - current user assumed
+	 * @param string|string[] $item
+	 * @param bool|int        $user If not specified - current user assumed
 	 *
-	 * @return false|string|mixed[]|Properties			If <i>$item</i> is integer - cs\User\Properties object will be returned
+	 * @return false|string|mixed[]|Properties            If <i>$item</i> is integer - cs\User\Properties object will be returned
 	 */
 	function get ($item, $user = false) {
 		if (is_scalar($item) && preg_match('/^[0-9]+$/', $item)) {
 			return new Properties($item);
 		}
-		$result	= $this->get_internal($item, $user);
+		$result = $this->get_internal($item, $user);
 		if (!$this->memory_cache) {
 			$this->__finish();
 		}
@@ -75,9 +78,9 @@ trait Data {
 	/**
 	 * Get data item of specified user
 	 *
-	 * @param string|string[]		$item
-	 * @param bool|int 				$user		If not specified - current user assumed
-	 * @param bool					$cache_only
+	 * @param string|string[] $item
+	 * @param bool|int        $user If not specified - current user assumed
+	 * @param bool            $cache_only
 	 *
 	 * @return false|string|mixed[]
 	 */
@@ -113,8 +116,8 @@ trait Data {
 			/**
 			 * If there are missing values - get them from the database
 			 */
-			$new_items	= '`'.implode('`, `', $new_items).'`';
-			$res		= $this->db()->qf(
+			$new_items = '`'.implode('`, `', $new_items).'`';
+			$res       = $this->db()->qf(
 				"SELECT $new_items
 				FROM `[prefix]users`
 				WHERE `id` = '$user'
@@ -122,9 +125,9 @@ trait Data {
 			);
 			unset($new_items);
 			if (is_array($res)) {
-				$this->update_cache[$user]	= true;
-				$data						= array_merge($res, $data ?: []);
-				$result						= array_merge($result, $res);
+				$this->update_cache[$user] = true;
+				$data                      = array_merge($res, $data ?: []);
+				$result                    = array_merge($result, $res);
 				/**
 				 * Sorting the resulting array in the same manner as the input array
 				 */
@@ -143,10 +146,10 @@ trait Data {
 		return $this->get_internal_one_item($item, $user, $data, $cache_only);
 	}
 	/**
-	 * @param string	$item
-	 * @param int		$user
-	 * @param mixed[]	$data
-	 * @param bool		$cache_only
+	 * @param string  $item
+	 * @param int     $user
+	 * @param mixed[] $data
+	 * @param bool    $cache_only
 	 *
 	 * @return array|false
 	 */
@@ -163,7 +166,7 @@ trait Data {
 		/**
 		 * Try to get data from the cache
 		 */
-		$data_from_cache	= $this->cache->$user;
+		$data_from_cache = $this->cache->$user;
 		if (is_array($data_from_cache)) {
 			/**
 			 * Update the local cache
@@ -184,7 +187,7 @@ trait Data {
 				LIMIT 1"
 			);
 			if ($data_from_db !== false) {
-				$this->update_cache[$user]	= true;
+				$this->update_cache[$user] = true;
 				return $data[$item] = $data_from_db;
 			}
 		}
@@ -193,14 +196,14 @@ trait Data {
 	/**
 	 * Set data item of specified user
 	 *
-	 * @param array|string	$item	Item-value array may be specified for setting several items at once
-	 * @param mixed|null	$value
-	 * @param bool|int		$user	If not specified - current user assumed
+	 * @param array|string $item Item-value array may be specified for setting several items at once
+	 * @param mixed|null   $value
+	 * @param bool|int     $user If not specified - current user assumed
 	 *
 	 * @return bool
 	 */
 	function set ($item, $value = null, $user = false) {
-		$result	= $this->set_internal($item, $value, $user);
+		$result = $this->set_internal($item, $value, $user);
 		if (!$this->memory_cache) {
 			$this->__finish();
 		}
@@ -209,9 +212,9 @@ trait Data {
 	/**
 	 * Set data item of specified user
 	 *
-	 * @param array|string	$item	Item-value array may be specified for setting several items at once
-	 * @param mixed|null	$value
-	 * @param bool|int		$user	If not specified - current user assumed
+	 * @param array|string $item Item-value array may be specified for setting several items at once
+	 * @param mixed|null   $value
+	 * @param bool|int     $user If not specified - current user assumed
 	 *
 	 * @return bool
 	 */
@@ -231,7 +234,7 @@ trait Data {
 				return true;
 			}
 			if ($item == 'login' || $item == 'email') {
-				$value	= mb_strtolower($value);
+				$value = mb_strtolower($value);
 				/** @noinspection NotOptimalIfConditionsInspection */
 				if ($item == 'email' && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
 					return false;
@@ -240,25 +243,25 @@ trait Data {
 					return false;
 				}
 			} elseif ($item == 'language') {
-				$L	= Language::instance();
+				$L = Language::instance();
 				if ($user == $this->id) {
 					$L->change($value);
-					$value	= $value ? $L->clanguage : '';
+					$value = $value ? $L->clanguage : '';
 				}
 			} elseif ($item == 'avatar') {
 				if (
 					$value &&
 					strpos($value, 'http') === false
 				) {
-					$value	= '';
+					$value = '';
 				}
 			}
-			$this->update_cache[$user]		= true;
-			$this->data[$user][$item]		= $value;
-			$this->data_set[$user][$item]	= $value;
+			$this->update_cache[$user]    = true;
+			$this->data[$user][$item]     = $value;
+			$this->data_set[$user][$item] = $value;
 			if ($item == 'login' || $item == 'email') {
-				$this->data[$user][$item.'_hash']		= hash('sha224', $value);
-				$this->data_set[$user][$item.'_hash']	= hash('sha224', $value);
+				$this->data[$user][$item.'_hash']     = hash('sha224', $value);
+				$this->data_set[$user][$item.'_hash'] = hash('sha224', $value);
 				unset($this->cache->{hash('sha224', $this->$item)});
 			} elseif ($item == 'password_hash' || ($item == 'status' && $value == 0)) {
 				Session::instance()->del_all($user);
@@ -269,100 +272,106 @@ trait Data {
 	/**
 	 * Getting additional data item(s) of specified user
 	 *
-	 * @param string|string[]		$item
-	 * @param bool|int				$user	If not specified - current user assumed
+	 * @param string|string[] $item
+	 * @param bool|int        $user If not specified - current user assumed
 	 *
 	 * @return false|string|mixed[]
 	 */
 	function get_data ($item, $user = false) {
-		$user	= (int)$user ?: $this->id;
+		$user = (int)$user ?: $this->id;
 		if (!$user || !$item || $user == User::GUEST_ID) {
 			return false;
 		}
-		$Cache	= $this->cache;
-		$data	= $Cache->{"data/$user"} ?: [];
+		$Cache = $this->cache;
+		$data  = $Cache->{"data/$user"} ?: [];
 		if (is_array($item)) {
-			$result	= [];
-			$absent	= [];
+			$result = [];
+			$absent = [];
 			foreach ($item as $i) {
 				if (isset($data[$i])) {
-					$result[$i]	= $data[$i];
+					$result[$i] = $data[$i];
 				} else {
-					$absent[]	= $i;
+					$absent[] = $i;
 				}
 			}
 			if ($absent) {
-				$absent					= implode(
+				$absent = implode(
 					',',
 					$this->db()->s($absent)
 				);
-				$absent					= array_column(
-					$this->db()->qfa([
-						"SELECT `item`, `value`
-						FROM `[prefix]users_data`
-						WHERE
-							`id`	= '$user' AND
-							`item`	IN($absent)"
-					]),
+				$absent = array_column(
+					$this->db()->qfa(
+						[
+							"SELECT `item`, `value`
+							FROM `[prefix]users_data`
+							WHERE
+								`id`	= '$user' AND
+								`item`	IN($absent)"
+						]
+					),
 					'value',
 					'item'
 				);
 				foreach ($absent as &$a) {
-					$a	= _json_decode($a);
+					$a = _json_decode($a);
 					if ($a === null) {
-						$a	= false;
+						$a = false;
 					}
 				}
 				unset($a);
-				$result					+= $absent;
-				$data					+= $absent;
-				$Cache->{"data/$user"}	= $data;
+				$result += $absent;
+				$data += $absent;
+				$Cache->{"data/$user"} = $data;
 			}
 			return $result;
 		}
 		if ($data === false || !isset($data[$item])) {
 			if (!is_array($data)) {
-				$data	= [];
+				$data = [];
 			}
-			$data[$item]			= _json_decode($this->db()->qfs([
-				"SELECT `value`
-				FROM `[prefix]users_data`
-				WHERE
-					`id`	= '$user' AND
-					`item`	= '%s'",
-				$item
-			]));
+			$data[$item] = _json_decode(
+				$this->db()->qfs(
+					[
+						"SELECT `value`
+						FROM `[prefix]users_data`
+						WHERE
+							`id`	= '$user' AND
+							`item`	= '%s'",
+						$item
+					]
+				)
+			);
 			if ($data[$item] === null) {
-				$data[$item]	= false;
+				$data[$item] = false;
 			}
-			$Cache->{"data/$user"}	= $data;
+			$Cache->{"data/$user"} = $data;
 		}
 		return $data[$item];
 	}
 	/**
 	 * Setting additional data item(s) of specified user
 	 *
-	 * @param array|string	$item	Item-value array may be specified for setting several items at once
-	 * @param mixed|null	$value
-	 * @param bool|int		$user	If not specified - current user assumed
+	 * @param array|string $item Item-value array may be specified for setting several items at once
+	 * @param mixed|null   $value
+	 * @param bool|int     $user If not specified - current user assumed
 	 *
 	 * @return bool
 	 */
 	function set_data ($item, $value = null, $user = false) {
-		$user	= (int)$user ?: $this->id;
+		$user = (int)$user ?: $this->id;
 		if (!$user || !$item || $user == User::GUEST_ID) {
 			return false;
 		}
 		if (is_array($item)) {
-			$params	= [];
+			$params = [];
 			foreach ($item as $i => $v) {
-				$params[]	= [
+				$params[] = [
 					$i,
 					_json_encode($v)
 				];
 			}
 			unset($i, $v);
-			$result			= $this->db_prime()->insert(
+			$result = $this->db_prime()->insert(
 				"INSERT INTO `[prefix]users_data`
 					(
 						`id`,
@@ -377,7 +386,7 @@ trait Data {
 				$params
 			);
 		} else {
-			$result	= $this->db_prime()->q(
+			$result = $this->db_prime()->q(
 				"INSERT INTO `[prefix]users_data`
 					(
 						`id`,
@@ -399,21 +408,21 @@ trait Data {
 	/**
 	 * Deletion of additional data item(s) of specified user
 	 *
-	 * @param string|string[]	$item
-	 * @param bool|int			$user	If not specified - current user assumed
+	 * @param string|string[] $item
+	 * @param bool|int        $user If not specified - current user assumed
 	 *
 	 * @return bool
 	 */
 	function del_data ($item, $user = false) {
-		$user	= (int)$user ?: $this->id;
+		$user = (int)$user ?: $this->id;
 		if (!$user || !$item || $user == User::GUEST_ID) {
 			return false;
 		}
-		$item	= implode(
+		$item   = implode(
 			',',
 			$this->db_prime()->s((array)$item)
 		);
-		$result	= $this->db_prime()->q(
+		$result = $this->db_prime()->q(
 			"DELETE FROM `[prefix]users_data`
 			WHERE
 				`id`	= '$user' AND
@@ -425,45 +434,51 @@ trait Data {
 	/**
 	 * Get user id by login or email hash (sha224) (hash from lowercase string)
 	 *
-	 * @param  string $login_hash	Login or email hash
+	 * @param  string $login_hash Login or email hash
 	 *
-	 * @return false|int			User id if found and not guest, otherwise - boolean <i>false</i>
+	 * @return false|int            User id if found and not guest, otherwise - boolean <i>false</i>
 	 */
 	function get_id ($login_hash) {
 		if (!preg_match('/^[0-9a-z]{56}$/', $login_hash)) {
 			return false;
 		}
-		$id	= $this->cache->get($login_hash, function () use ($login_hash) {
-			return $this->db()->qfs([
-				"SELECT `id`
-				FROM `[prefix]users`
-				WHERE
-					`login_hash`	= '%s' OR
-					`email_hash`	= '%s'
-				LIMIT 1",
-				$login_hash,
-				$login_hash
-			]) ?: false;
-		});
+		$id = $this->cache->get(
+			$login_hash,
+			function () use ($login_hash) {
+				return $this->db()->qfs(
+					[
+						"SELECT `id`
+						FROM `[prefix]users`
+						WHERE
+							`login_hash`	= '%s' OR
+							`email_hash`	= '%s'
+						LIMIT 1",
+						$login_hash,
+						$login_hash
+					]
+				) ?: false;
+			}
+		);
 		return $id && $id != User::GUEST_ID ? $id : false;
 	}
 	/**
 	 * Get user avatar, if no one present - uses Gravatar
 	 *
-	 * @param int|null	$size	Avatar size, if not specified or resizing is not possible - original image is used
-	 * @param bool|int	$user	If not specified - current user assumed
+	 * @param int|null $size Avatar size, if not specified or resizing is not possible - original image is used
+	 * @param bool|int $user If not specified - current user assumed
 	 *
 	 * @return string
 	 */
 	function avatar ($size = null, $user = false) {
-		$user	= (int)$user ?: $this->id;
-		$avatar	= $this->get('avatar', $user);
+		$user   = (int)$user ?: $this->id;
+		$avatar = $this->get('avatar', $user);
 		if (!$avatar && $this->id != User::GUEST_ID) {
-			$avatar	= 'https://www.gravatar.com/avatar/'.md5($this->get('email', $user))."?d=mm&s=$size";
-			$avatar	.= '&d='.urlencode(Config::instance()->core_url().'/includes/img/guest.svg');
+			$email_hash     = md5($this->get('email', $user));
+			$default_avatar = urlencode(Config::instance()->core_url().'/includes/img/guest.svg');
+			$avatar         = "https://www.gravatar.com/avatar/$email_hash?d=mm&s=$size&d=$default_avatar";
 		}
 		if (!$avatar) {
-			$avatar	= '/includes/img/guest.svg';
+			$avatar = '/includes/img/guest.svg';
 		}
 		return h::prepare_url($avatar, true);
 	}
@@ -492,7 +507,7 @@ trait Data {
 	 * But in case of working with large amount of users this cache can be too large. Disabling will cause some performance drop, but save a lot of RAM.
 	 */
 	function disable_memory_cache () {
-		$this->memory_cache	= false;
+		$this->memory_cache = false;
 	}
 	/**
 	 * Returns array of users columns, available for getting of data
@@ -515,16 +530,16 @@ trait Data {
 				$data = [];
 				foreach ($data_set as $i => $value) {
 					if ($i != 'id' && in_array($i, $this->users_columns)) {
-						$value	= xap($value, false);
-						$data[]	= "`$i` = ".$this->db_prime()->s($value);
+						$value  = xap($value, false);
+						$data[] = "`$i` = ".$this->db_prime()->s($value);
 					} elseif ($i != 'id') {
 						unset($data_set[$i]);
 					}
 				}
 				unset($i, $value);
 				if (!empty($data)) {
-					$data		= implode(', ', $data);
-					$update[]	= "UPDATE `[prefix]users`
+					$data     = implode(', ', $data);
+					$update[] = "UPDATE `[prefix]users`
 						SET $data
 						WHERE `id` = '$id'";
 				}
@@ -541,12 +556,12 @@ trait Data {
 		 */
 		foreach ($this->data as $id => $data) {
 			if (isset($this->update_cache[$id]) && $this->update_cache[$id]) {
-				$data['id']			= $id;
-				$this->cache->$id	= $data;
+				$data['id']       = $id;
+				$this->cache->$id = $data;
 			}
 		}
 		unset($id, $data);
 		$this->update_cache = [];
-		$this->data_set = [];
+		$this->data_set     = [];
 	}
 }
