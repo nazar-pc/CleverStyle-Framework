771b65a9f731ab766f7446407996d10fd748c09a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-04-16 03:35:24 +0200

    Reformatting of `\cs\User\Management` and one tiny fix

--- core/traits/User/Management.php
+++ core/traits/User/Management.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CleverStyle CMS
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2011-2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs\User;
+namespace cs\User;
 use
 	cs\Config,
 	cs\Core,
@@ -16,9 +16,9 @@ use
 /**
  * Trait that contains all methods from <i>>cs\User</i> for working with user management (creation, modification, deletion)
  *
- * @property int				$id
- * @property \cs\Cache\Prefix	$cache
- * @property string				$ip
+ * @property int              $id
+ * @property \cs\Cache\Prefix $cache
+ * @property string           $ip
  *
  * @method \cs\DB\_Abstract db()
  * @method \cs\DB\_Abstract db_prime()
@@ -33,54 +33,54 @@ trait Management {
 	 * User id after registration
 	 * @var int
 	 */
-	protected	$reg_id			= 0;
+	protected $reg_id = 0;
 	/**
 	 * Search keyword in login, username and email
 	 *
-	 * @param string		$search_phrase
+	 * @param string $search_phrase
 	 *
 	 * @return false|int[]
 	 */
 	function search_users ($search_phrase) {
 		$search_phrase = trim($search_phrase, "%\n");
-		$found_users   = $this->db()->qfas([
-			"SELECT `id`
-			FROM `[prefix]users`
-			WHERE
-				(
-					`login`		LIKE '%s' OR
-					`username`	LIKE '%s' OR
-					`email`		LIKE '%s'
-				) AND
-				`status` != '%s'",
-			$search_phrase,
-			$search_phrase,
-			$search_phrase,
-			User::STATUS_NOT_ACTIVATED
-		]);
+		$found_users   = $this->db()->qfas(
+			[
+				"SELECT `id`
+				FROM `[prefix]users`
+				WHERE
+					(
+						`login`		LIKE '%s' OR
+						`username`	LIKE '%s' OR
+						`email`		LIKE '%s'
+					) AND
+					`status` != '%s'",
+				$search_phrase,
+				$search_phrase,
+				$search_phrase,
+				User::STATUS_NOT_ACTIVATED
+			]
+		);
 		return $found_users;
 	}
 	/**
 	 * User registration
 	 *
-	 * @param string 				$email
-	 * @param bool					$confirmation	If <b>true</b> - default system option is used, if <b>false</b> - registration will be
-	 *												finished without necessity of confirmation, independently from default system option
-	 *												(is used for manual registration).
-	 * @param bool					$auto_sign_in	If <b>false</b> - no auto sign in, if <b>true</b> - according to system configuration
+	 * @param string $email
+	 * @param bool   $confirmation If <b>true</b> - default system option is used, if <b>false</b> - registration will be finished without necessity of
+	 *                             confirmation, independently from default system option (is used for manual registration).
+	 * @param bool   $auto_sign_in If <b>false</b> - no auto sign in, if <b>true</b> - according to system configuration
 	 *
-	 * @return array|false|string					<b>exists</b>	- if user with such email is already registered<br>
-	 * 												<b>error</b>	- if error occurred<br>
-	 * 												<b>false</b>	- if email is incorrect<br>
-	 * 												<b>array(<br>
-	 * 												&nbsp;'reg_key'		=> *,</b>	//Registration confirmation key, or <b>true</b>
-	 * 																					if confirmation is not required<br>
-	 * 												&nbsp;<b>'password'	=> *,</b>	//Automatically generated password<br>
-	 * 												&nbsp;<b>'id'		=> *</b>	//Id of registered user in DB<br>
-	 * 												<b>)</b>
+	 * @return array|false|string  <b>exists</b> - if user with such email is already registered<br>
+	 *                             <b>error</b> - if error occurred<br>
+	 *                             <b>false</b> - if email is incorrect<br>
+	 *                             <b>[<br>
+	 *                             &nbsp;'reg_key'     => *,</b> //Registration confirmation key, or <b>true</b> if confirmation is not required<br>
+	 *                             &nbsp;<b>'password' => *,</b> //Automatically generated password<br>
+	 *                             &nbsp;<b>'id'       => *</b> //Id of registered user in DB<br>
+	 *                             <b>]</b>
 	 */
 	function registration ($email, $confirmation = true, $auto_sign_in = true) {
-		$email			= mb_strtolower($email);
+		$email = mb_strtolower($email);
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			return false;
 		}
@@ -88,14 +88,15 @@ trait Management {
 		if (!Event::instance()->fire(
 			'System/User/registration/before',
 			[
-				'email'	=> $email
+				'email' => $email
 			]
-		)) {
+		)
+		) {
 			return false;
 		}
-		$email_hash		= hash('sha224', $email);
-		$login			= strstr($email, '@', true);
-		$login_hash		= hash('sha224', $login);
+		$email_hash = hash('sha224', $email);
+		$login      = strstr($email, '@', true);
+		$login_hash = hash('sha224', $login);
 		if (
 			$this->get_id($login_hash) !== false ||
 			(
@@ -103,25 +104,28 @@ trait Management {
 				in_array($login, file_get_json(MODULES.'/System/index.json')['profile'])
 			)
 		) {
-			$login		= $email;
-			$login_hash	= $email_hash;
+			$login      = $email;
+			$login_hash = $email_hash;
 		}
-		if ($this->db_prime()->qf([
-			"SELECT `id`
-			FROM `[prefix]users`
-			WHERE `email_hash` = '%s'
-			LIMIT 1",
-			$email_hash
-		])) {
+		if ($this->db_prime()->qf(
+			[
+				"SELECT `id`
+				FROM `[prefix]users`
+				WHERE `email_hash` = '%s'
+				LIMIT 1",
+				$email_hash
+			]
+		)
+		) {
 			return 'exists';
 		}
-		$Config			= Config::instance();
-		$password		= password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
+		$Config   = Config::instance();
+		$password = password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
 		/**
 		 * @var \cs\_SERVER $_SERVER
 		 */
-		$reg_key		= md5(openssl_random_pseudo_bytes(1000));
-		$confirmation	= $confirmation && $Config->core['require_registration_confirmation'];
+		$reg_key      = md5(openssl_random_pseudo_bytes(1000));
+		$confirmation = $confirmation && $Config->core['require_registration_confirmation'];
 		if ($this->db_prime()->q(
 			"INSERT INTO `[prefix]users` (
 				`login`,
@@ -150,7 +154,8 @@ trait Management {
 			ip2hex($_SERVER->ip),
 			$reg_key,
 			!$confirmation ? 1 : -1
-		)) {
+		)
+		) {
 			$this->reg_id = $this->db_prime()->id();
 			$this->set_password($password, $this->reg_id);
 			if (!$confirmation) {
@@ -162,9 +167,10 @@ trait Management {
 			if (!Event::instance()->fire(
 				'System/User/registration/after',
 				[
-					'id'	=> $this->reg_id
+					'id' => $this->reg_id
 				]
-			)) {
+			)
+			) {
 				$this->registration_cancel();
 				return false;
 			}
@@ -173,9 +179,9 @@ trait Management {
 			}
 			unset($this->cache->$login_hash);
 			return [
-				'reg_key'	=> !$confirmation ? true : $reg_key,
-				'password'	=> $password,
-				'id'		=> $this->reg_id
+				'reg_key'  => !$confirmation ? true : $reg_key,
+				'password' => $password,
+				'id'       => $this->reg_id
 			];
 		} else {
 			return 'error';
@@ -184,9 +190,9 @@ trait Management {
 	/**
 	 * Confirmation of registration process
 	 *
-	 * @param string		$reg_key
+	 * @param string $reg_key
 	 *
-	 * @return array|false				array('id' => <i>id</i>, 'email' => <i>email</i>, 'password' => <i>password</i>) or <b>false</b> on failure
+	 * @return array|false ['id' => <i>id</i>, 'email' => <i>email</i>, 'password' => <i>password</i>] or <b>false</b> on failure
 	 */
 	function registration_confirmation ($reg_key) {
 		if (!is_md5($reg_key)) {
@@ -195,32 +201,35 @@ trait Management {
 		if (!Event::instance()->fire(
 			'System/User/registration/confirmation/before',
 			[
-				'reg_key'	=> $reg_key
+				'reg_key' => $reg_key
 			]
-		)) {
+		)
+		) {
 			$this->registration_cancel();
 			return false;
 		}
 		$this->delete_unconfirmed_users();
-		$data			= $this->db_prime()->qf([
-			"SELECT
-				`id`,
-				`login_hash`,
-				`email`
-			FROM `[prefix]users`
-			WHERE
-				`reg_key`	= '%s' AND
-				`status`	= '%s'
-			LIMIT 1",
-			$reg_key,
-			User::STATUS_NOT_ACTIVATED
-		]);
+		$data = $this->db_prime()->qf(
+			[
+				"SELECT
+					`id`,
+					`login_hash`,
+					`email`
+				FROM `[prefix]users`
+				WHERE
+					`reg_key`	= '%s' AND
+					`status`	= '%s'
+				LIMIT 1",
+				$reg_key,
+				User::STATUS_NOT_ACTIVATED
+			]
+		);
 		if (!$data) {
 			return false;
 		}
-		$this->reg_id	= $data['id'];
-		$Config			= Config::instance();
-		$password		= password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
+		$this->reg_id = $data['id'];
+		$Config       = Config::instance();
+		$password     = password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
 		$this->set_password($password, $this->reg_id);
 		$this->set('status', User::STATUS_ACTIVE, $this->reg_id);
 		$this->set_groups([User::USER_GROUP_ID], $this->reg_id);
@@ -228,17 +237,18 @@ trait Management {
 		if (!Event::instance()->fire(
 			'System/User/registration/confirmation/after',
 			[
-				'id'	=> $this->reg_id
+				'id' => $this->reg_id
 			]
-		)) {
+		)
+		) {
 			$this->registration_cancel();
 			return false;
 		}
 		unset($this->cache->{$data['login_hash']});
 		return [
-			'id'		=> $this->reg_id,
-			'email'		=> $data['email'],
-			'password'	=> $password
+			'id'       => $this->reg_id,
+			'email'    => $data['email'],
+			'password' => $password
 		];
 	}
 	/**
@@ -256,32 +266,35 @@ trait Management {
 	 * Checks for unconfirmed registrations and deletes expired
 	 */
 	protected function delete_unconfirmed_users () {
-		$reg_date		= time() - Config::instance()->core['registration_confirmation_time'] * 86400;	//1 day = 86400 seconds
-		$ids			= $this->db_prime()->qfas([
-			"SELECT `id`
-			FROM `[prefix]users`
-			WHERE
-				`last_sign_in`	= 0 AND
-				`status`		= '%s' AND
-				`reg_date`		< $reg_date",
-			User::STATUS_NOT_ACTIVATED
-		]);
-		$this->del_user($ids);
-
+		$reg_date = time() - Config::instance()->core['registration_confirmation_time'] * 86400;    //1 day = 86400 seconds
+		$ids      = $this->db_prime()->qfas(
+			[
+				"SELECT `id`
+				FROM `[prefix]users`
+				WHERE
+					`last_sign_in`	= 0 AND
+					`status`		= '%s' AND
+					`reg_date`		< $reg_date",
+				User::STATUS_NOT_ACTIVATED
+			]
+		);
+		if ($ids) {
+			$this->del_user($ids);
+		}
 	}
 	/**
 	 * Proper password setting without any need to deal with low-level implementation
 	 *
-	 * @param string	$new_password
-	 * @param bool		$user
-	 * @param bool		$already_prepared	If true - assumed that `sha512(sha512(password) + public_key)` was applied to password
+	 * @param string $new_password
+	 * @param bool   $user
+	 * @param bool   $already_prepared If true - assumed that `sha512(sha512(password) + public_key)` was applied to password
 	 *
 	 * @return bool
 	 */
 	function set_password ($new_password, $user = false, $already_prepared = false) {
-		$public_key	= Core::instance()->public_key;
+		$public_key = Core::instance()->public_key;
 		if (!$already_prepared) {
-			$new_password	=  hash('sha512', hash('sha512', $new_password).$public_key);
+			$new_password = hash('sha512', hash('sha512', $new_password).$public_key);
 		}
 		/**
 		 * Do not allow to set password to empty
@@ -294,23 +307,23 @@ trait Management {
 	/**
 	 * Proper password validation without any need to deal with low-level implementation
 	 *
-	 * @param string	$password
-	 * @param bool		$user
-	 * @param bool		$already_prepared	If true - assumed that `sha512(sha512(password) + public_key)` was applied to password
+	 * @param string $password
+	 * @param bool   $user
+	 * @param bool   $already_prepared If true - assumed that `sha512(sha512(password) + public_key)` was applied to password
 	 *
 	 * @return bool
 	 */
 	function validate_password ($password, $user = false, $already_prepared = false) {
 		if (!$already_prepared) {
-			$password	=  hash('sha512', hash('sha512', $password).Core::instance()->public_key);
+			$password = hash('sha512', hash('sha512', $password).Core::instance()->public_key);
 		}
-		$user			= (int)$user ?: $this->id;
-		$password_hash	= $this->get('password_hash', $user);
+		$user          = (int)$user ?: $this->id;
+		$password_hash = $this->get('password_hash', $user);
 		/**
 		 * TODO This is fallback for smooth upgrade of old passwords, will be removed in future
 		 */
 		if (password_get_info($password_hash)['algo'] === 0 && $password == $password_hash) {
-			$current_user_id	= $this->id;
+			$current_user_id = $this->id;
 			$this->set_password($password, $user, true);
 			if ($current_user_id == $user) {
 				Session::instance()->add($current_user_id);
@@ -324,7 +337,7 @@ trait Management {
 		 * Rehash password if needed
 		 */
 		if (password_needs_rehash($password_hash, PASSWORD_DEFAULT)) {
-			$current_user_id	= $this->id;
+			$current_user_id = $this->id;
 			$this->set_password($password, $user, true);
 			if ($current_user_id == $user) {
 				Session::instance()->add($current_user_id);
@@ -335,16 +348,16 @@ trait Management {
 	/**
 	 * Restoring of password
 	 *
-	 * @param int			$user
+	 * @param int $user
 	 *
-	 * @return false|string			Key for confirmation or <b>false</b> on failure
+	 * @return false|string Key for confirmation or <b>false</b> on failure
 	 */
 	function restore_password ($user) {
 		if ($user && $user != User::GUEST_ID) {
-			$reg_key		= md5(openssl_random_pseudo_bytes(1000));
+			$reg_key = md5(openssl_random_pseudo_bytes(1000));
 			if ($this->set('reg_key', $reg_key, $user)) {
-				$data					= $this->get('data', $user);
-				$data['restore_until']	= time() + Config::instance()->core['registration_confirmation_time'] * 86400;
+				$data                  = $this->get('data', $user);
+				$data['restore_until'] = time() + Config::instance()->core['registration_confirmation_time'] * 86400;
 				if ($this->set('data', $data, $user)) {
 					return $reg_key;
 				}
@@ -355,28 +368,30 @@ trait Management {
 	/**
 	 * Confirmation of password restoring process
 	 *
-	 * @param string		$key
+	 * @param string $key
 	 *
-	 * @return array|false			array('id' => <i>id</i>, 'password' => <i>password</i>) or <b>false</b> on failure
+	 * @return array|false ['id' => <i>id</i>, 'password' => <i>password</i>] or <b>false</b> on failure
 	 */
 	function restore_password_confirmation ($key) {
 		if (!is_md5($key)) {
 			return false;
 		}
-		$id			= $this->db_prime()->qfs([
-			"SELECT `id`
-			FROM `[prefix]users`
-			WHERE
-				`reg_key`	= '%s' AND
-				`status`	= '%s'
-			LIMIT 1",
-			$key,
-			User::STATUS_ACTIVE
-		]);
+		$id = $this->db_prime()->qfs(
+			[
+				"SELECT `id`
+				FROM `[prefix]users`
+				WHERE
+					`reg_key`	= '%s' AND
+					`status`	= '%s'
+				LIMIT 1",
+				$key,
+				User::STATUS_ACTIVE
+			]
+		);
 		if (!$id) {
 			return false;
 		}
-		$data		= $this->get('data', $id);
+		$data = $this->get('data', $id);
 		if (!isset($data['restore_until'])) {
 			return false;
 		} elseif ($data['restore_until'] < time()) {
@@ -385,20 +400,20 @@ trait Management {
 			return false;
 		}
 		unset($data['restore_until']);
-		$Config		= Config::instance();
-		$password	= password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
+		$Config   = Config::instance();
+		$password = password_generate($Config->core['password_min_length'], $Config->core['password_min_strength']);
 		$this->set_password($password, $id);
 		$this->set('data', $data, $id);
 		Session::instance()->add($id);
 		return [
-			'id'		=> $id,
-			'password'	=> $password
+			'id'       => $id,
+			'password' => $password
 		];
 	}
 	/**
 	 * Delete specified user or array of users
 	 *
-	 * @param int|int[]	$user	User id or array of users ids
+	 * @param int|int[] $user User id or array of users ids
 	 */
 	function del_user ($user) {
 		$this->del_user_internal($user);
@@ -406,15 +421,15 @@ trait Management {
 	/**
 	 * Delete specified user or array of users
 	 *
-	 * @param int|int[]	$user
-	 * @param bool		$update
+	 * @param int|int[] $user
+	 * @param bool      $update
 	 */
 	protected function del_user_internal ($user, $update = true) {
-		$Cache	= $this->cache;
+		$Cache = $this->cache;
 		Event::instance()->fire(
 			'System/User/del/before',
 			[
-				'id'	=> $user
+				'id' => $user
 			]
 		);
 		if (is_array($user)) {
@@ -448,7 +463,7 @@ trait Management {
 			Event::instance()->fire(
 				'System/User/del/after',
 				[
-					'id'	=> $user
+					'id' => $user
 				]
 			);
 		}
@@ -456,11 +471,11 @@ trait Management {
 	/**
 	 * Add bot
 	 *
-	 * @param string	$name		Bot name
-	 * @param string	$user_agent	User Agent string or regular expression
-	 * @param string	$ip			IP string or regular expression
+	 * @param string $name       Bot name
+	 * @param string $user_agent User Agent string or regular expression
+	 * @param string $ip         IP string or regular expression
 	 *
-	 * @return false|int			Bot <b>id</b> in DB or <b>false</b> on failure
+	 * @return false|int Bot <b>id</b> in DB or <b>false</b> on failure
 	 */
 	function add_bot ($name, $user_agent, $ip) {
 		if ($this->db_prime()->q(
@@ -480,13 +495,14 @@ trait Management {
 			xap($user_agent),
 			xap($ip),
 			User::STATUS_ACTIVE
-		)) {
-			$id	= $this->db_prime()->id();
+		)
+		) {
+			$id = $this->db_prime()->id();
 			$this->set_groups([User::BOT_GROUP_ID], $id);
 			Event::instance()->fire(
 				'System/User/add_bot',
 				[
-					'id'	=> $id
+					'id' => $id
 				]
 			);
 			return $id;
@@ -496,19 +512,19 @@ trait Management {
 	/**
 	 * Set bot
 	 *
-	 * @param int		$id			Bot id
-	 * @param string	$name		Bot name
-	 * @param string	$user_agent	User Agent string or regular expression
-	 * @param string	$ip			IP string or regular expression
+	 * @param int    $id         Bot id
+	 * @param string $name       Bot name
+	 * @param string $user_agent User Agent string or regular expression
+	 * @param string $ip         IP string or regular expression
 	 *
 	 * @return bool
 	 */
 	function set_bot ($id, $name, $user_agent, $ip) {
-		$result	= $this->set(
+		$result = $this->set(
 			[
-				'username'	=> $name,
-				'login'		=> $user_agent,
-				'email'		=> $ip
+				'username' => $name,
+				'login'    => $user_agent,
+				'email'    => $ip
 			],
 			'',
 			$id
@@ -519,7 +535,7 @@ trait Management {
 	/**
 	 * Delete specified bot or array of bots
 	 *
-	 * @param int|int[]	$bot	Bot id or array of bots ids
+	 * @param int|int[] $bot Bot id or array of bots ids
 	 */
 	function del_bot ($bot) {
 		$this->del_user($bot);
@@ -528,21 +544,21 @@ trait Management {
 	/**
 	 * Returns array of user id, that are associated as contacts
 	 *
-	 * @param	bool|int	$user	If not specified - current user assumed
+	 * @param bool|int $user If not specified - current user assumed
 	 *
-	 * @return	int[]				Array of user id
+	 * @return int[] Array of user id
 	 */
 	function get_contacts ($user = false) {
 		$user = (int)$user ?: $this->id;
 		if (!$user || $user == User::GUEST_ID) {
 			return [];
 		}
-		$contacts	= [];
+		$contacts = [];
 		Event::instance()->fire(
 			'System/User/get_contacts',
 			[
-				'id'		=> $user,
-				'contacts'	=> &$contacts
+				'id'       => $user,
+				'contacts' => &$contacts
 			]
 		);
 		return array_unique($contacts);
