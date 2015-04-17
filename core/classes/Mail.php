5197eb2af4692c166499e95a21b48c23149f36c3
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-04-17 05:20:36 +0200

    Refactoring of `\cs\Mail::send_to()`. Improved signature formatting in HTML emails. Fixed attachments addition to emails sent.

--- core/classes/Mail.php
+++ core/classes/Mail.php
@@ -37,12 +37,78 @@ class Mail extends PHPMailer {
 		$this->IsHTML();
 	}
 	/**
+	 * Sending of email
+	 *
+	 * @param array|string|string[]      $email       if emails without names - string (may be several emails separated by comma) or
+	 *                                                1-dimensional array(<i>email</i>)<br>
+	 *                                                2-dimensional of emails or array(<i>email</i>, <i>name</i>) must be given
+	 * @param string                     $subject     Mail subject
+	 * @param string                     $body        html body
+	 * @param string|null                $body_text   plain text body
+	 * @param array|null|string          $attachments 1- or 2-dimensional array of array(<i>path</i>, <i>name</i>) or simply string with path to the file in
+	 *                                                file system
+	 * @param array|null|string|string[] $reply_to    Similar to <b>$email</b>, but multiple emails are not supported
+	 * @param bool|string                $signature   <b>true</b> - add system signature<br>
+	 *                                                <b>false</b> - without signature<br>
+	 *                                                <b>string</b> - custom signature
+	 *
+	 * @return bool
+	 */
+	function send_to ($email, $subject, $body, $body_text = null, $attachments = null, $reply_to = null, $signature = true) {
+		if (!$email || !$subject || !$body) {
+			return false;
+		}
+		foreach ($this->normalize_email($email) as $e) {
+			call_user_func_array([$this, 'addAddress'], $e);
+		}
+		foreach ($this->normalize_email($reply_to) as $r) {
+			call_user_func_array([$this, 'AddReplyTo'], $r);
+		}
+		foreach ($this->normalize_attachment($attachments) as $a) {
+			call_user_func_array([$this, 'AddAttachment'], $a);
+		}
+		$this->Subject = $subject;
+		$signature     = $this->make_signature($signature);
+		$this->Body    = $this->normalize_body($body, $signature);
+		if ($body_text) {
+			$this->AltBody = $body_text.strip_tags($signature);
+		}
+		$result = $this->Send();
+		$this->ClearAddresses();
+		$this->ClearAttachments();
+		$this->ClearReplyTos();
+		return $result;
+	}
+	/**
+	 * @param array|string|string[] $email
+	 *
+	 * @return string[][]
+	 */
+	protected function normalize_email ($email) {
+		if (!$email) {
+			return [];
+		}
+		if (!is_array($email)) {
+			$email = _trim(explode(',', $email));
+		} elseif (!is_array($email[0])) {
+			$email = [$email];
+		}
+		return _array($email);
+	}
+	protected function make_signature ($signature) {
+		if ($signature === true) {
+			$signature = get_core_ml_text('mail_signature');
+			return $signature ? "<br>$this->LE<br>$this->LE-- $this->LE<br>$signature" : '';
+		}
+		return $signature ? "<br>$this->LE<br>$this->LE-- $this->LE<br>".xap($signature, true) : '';
+	}
+	/**
 	 * @param string $body
 	 * @param string $signature
 	 *
 	 * @return string
 	 */
-	protected function body_normalization ($body, $signature) {
+	protected function normalize_body ($body, $signature) {
 		if (strpos($body, '<!doctype') === 0 && strpos($body, '<body') !== false) {
 			$body = "<!doctype html>\n$body";
 		}
@@ -67,93 +133,17 @@ class Mail extends PHPMailer {
 		return $body;
 	}
 	/**
-	 * Sending of email
+	 * @param array|null|string $attachments
 	 *
-	 * @param array|string|string[]      $email       if emails without names - string (may be several emails separated by comma) or
-	 *                                                1-dimensional array(<i>email</i>)<br>
-	 *                                                else - 2-dimensional array(<i>email</i>, <i>name</i>) must be given
-	 * @param string                     $subject     Mail subject
-	 * @param string                     $body        html body
-	 * @param string|null                $body_text   plain text body
-	 * @param array|null|string          $attachments 1- or 2-dimensional array of array(<i>path</i>, <i>name</i>) or simply string with path to the file in
-	 *                                                file system
-	 * @param array|null|string|string[] $reply_to    Similar to <b>$email</b>
-	 * @param bool|string                $signature   <b>true</b> - add system signature<br>
-	 *                                                <b>false</b> - without signature<br>
-	 *                                                <b>string</b> - custom signature
-	 *
-	 * @return bool
+	 * @return array
 	 */
-	function send_to ($email, $subject, $body, $body_text = null, $attachments = null, $reply_to = null, $signature = true) {
-		if (!$email || !$subject || !$body) {
-			return false;
-		}
-		foreach ($this->normalize_email($email) as $e) {
-			$this->addAddress($e[0], $e[1]);
-		}
-		unset($e);
-		$this->Subject = $subject;
-		if ($signature === true) {
-			if ($signature = get_core_ml_text('mail_signature')) {
-				$signature = "$this->LE-- $this->LE.$signature";
-			}
-		} elseif ($signature) {
-			$signature = "$this->LE-- $this->LE".xap($signature, true);
-		} else {
-			$signature = '';
-		}
-		$this->Body = $this->body_normalization($body, $signature);
-		if ($body_text) {
-			$this->AltBody = $body_text.strip_tags($signature);
-		}
-		if (is_array($attachments)) {
-			if (count($attachments) == 2) {
-				$this->AddStringAttachment($attachments[0], $attachments[1]);
-			} else {
-				foreach ($attachments as $a) {
-					if (is_array($a)) {
-						$this->AddStringAttachment($a[0], $a[1]);
-					} else {
-						$this->AddStringAttachment($a, pathinfo($a, PATHINFO_FILENAME));
-					}
-				}
-			}
-		} elseif (is_string($attachments)) {
-			$this->AddStringAttachment($attachments, pathinfo($attachments, PATHINFO_FILENAME));
-		}
-		foreach ($this->normalize_email($reply_to) as $r) {
-			$this->AddReplyTo($r[0], $r[1]);
-		}
-		unset($r);
-		$result = $this->Send();
-		$this->ClearAddresses();
-		$this->ClearAttachments();
-		$this->ClearReplyTos();
-		return $result;
-	}
-	/**
-	 * @param string $email
-	 *
-	 * @return string[][]
-	 */
-	protected function normalize_email ($email) {
-		if (!$email) {
+	protected function normalize_attachment ($attachments) {
+		if (!$attachments) {
 			return [];
 		}
-		if (is_array($email)) {
-			if (count($email) == 2) {
-				return [$email];
-			}
-			$emails = [];
-			foreach ($email as $m) {
-				$emails[] = is_array($m) ? $m : [$m, ''];
-			}
-			return $emails;
+		if (!is_array($attachments) || !is_array($attachments[0])) {
+			$attachments = [$attachments];
 		}
-		$email = _trim(explode(',', $email));
-		foreach ($email as &$e) {
-			$e = [$e, ''];
-		}
-		return $email;
+		return _array($attachments);
 	}
 }
