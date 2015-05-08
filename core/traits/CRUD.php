f25dfed39fd8a588303da82538a1df8536104237
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-05-08 02:41:17 +0200

    `\cs\CRUD` can now handle even more work with added joined tables support

--- core/traits/CRUD.php
+++ core/traits/CRUD.php
@@ -29,14 +29,20 @@ trait CRUD {
 	 * @param bool                $update_needed If on creation request without specified primary key and multilingual fields present - update needed
 	 *                                           after creation (there is no id before creation)
 	 *
-	 * @return array
+	 * @return array[]
 	 */
 	private function crud_arguments_preparation ($data_model, $arguments, $id = false, &$update_needed = false) {
-		$arguments = array_combine(array_keys($data_model), $arguments);
+		$arguments     = array_combine(array_keys($data_model), $arguments);
+		$joined_tables = [];
 		foreach ($arguments as $item => &$argument) {
 			$model = $data_model[$item];
 			if (is_callable($model)) {
 				$argument = $model($argument);
+				continue;
+			}
+			if (isset($model['data_model'])) {
+				$joined_tables[$item] = $this->joined_tables_model($model, $argument);
+				unset($arguments[$item]);
 				continue;
 			}
 			$model              = explode(':', $model, 2);
@@ -66,7 +72,41 @@ trait CRUD {
 				}
 			}
 		}
-		return $arguments;
+		return [
+			$arguments,
+			$joined_tables
+		];
+	}
+	/**
+	 * @param array $structure
+	 * @param array $arguments
+	 *
+	 * @return array
+	 */
+	private function joined_tables_model ($structure, $arguments) {
+		$new_structure = [
+			'id_field' => array_keys($structure['data_model'])[0],
+			'fields'   => array_slice($structure['data_model'], 1),
+			'data'     => []
+		];
+		if (isset($structure['language_field'])) {
+			$new_structure['language_field'] = $structure['language_field'];
+		}
+		$arguments       = is_array_assoc($arguments) ? [$arguments] : _array((array)$arguments);
+		$arguments_assoc = is_array_assoc($arguments[0]);
+		foreach ($new_structure['fields'] as $field_name => $field_model) {
+			$field_model = explode(':', $field_model, 2);
+			$type        = $field_model[0];
+			$format      = isset($field_model[1]) ? $field_model[1] : null;
+			/**
+			 * Both associative and indexed arrays are supported - that is why we determine key for array
+			 */
+			$key = $arguments_assoc ? $field_name : array_search($field_name, $new_structure['fields']);
+			foreach ($arguments as $index => $arguments_local) {
+				$new_structure['data'][$index][$field_name] = $this->crud_argument_preparation($type, $format, $arguments_local[$key]);
+			}
+		}
+		return $new_structure;
 	}
 	/**
 	 * @param string $type
@@ -161,16 +201,16 @@ trait CRUD {
 	 * @return false|int|string Id of created item on success (or specified primary key), `false` otherwise
 	 */
 	private function create_internal ($table, $data_model, $arguments) {
-		$insert_id          = count($data_model) == count($arguments) ? $arguments[0] : false;
-		$prepared_arguments = self::crud_arguments_preparation(
+		$insert_id = count($data_model) == count($arguments) ? $arguments[0] : false;
+		list($prepared_arguments, $joined_tables) = self::crud_arguments_preparation(
 			$insert_id !== false ? $data_model : array_slice($data_model, 1),
 			$arguments,
 			$insert_id,
 			$update_needed
 		);
-		$columns            = "`".implode("`,`", array_keys($insert_id !== false ? $data_model : array_slice($data_model, 1)))."`";
-		$values             = implode(',', array_fill(0, count($prepared_arguments), "'%s'"));
-		$return             = $this->db_prime()->q(
+		$columns = "`".implode("`,`", array_keys($prepared_arguments))."`";
+		$values  = implode(',', array_fill(0, count($prepared_arguments), "'%s'"));
+		$return  = $this->db_prime()->q(
 			"INSERT IGNORE INTO `$table`
 				(
 					$columns
@@ -179,14 +219,15 @@ trait CRUD {
 				)",
 			$prepared_arguments
 		);
-		$id                 = $insert_id !== false ? $insert_id : $this->db_prime()->id();
+		$id      = $insert_id !== false ? $insert_id : $this->db_prime()->id();
 		/**
 		 * Id might be 0 if insertion failed or if we insert duplicate entry (which is fine since we use 'INSERT IGNORE'
 		 */
 		if (!$return || $id === 0) {
 			return false;
 		}
-		$this->find_update_files_tags($id, [], $prepared_arguments);
+		$this->update_joined_tables($id, $joined_tables);
+		$this->find_update_files_tags($id, [], $arguments);
 		/**
 		 * If on creation request without specified primary key and multilingual fields present - update needed
 		 * after creation (there is no id before creation)
@@ -197,141 +238,57 @@ trait CRUD {
 		return $id;
 	}
 	/**
-	 * @deprecated
-	 * @todo remove in future versions
-	 *
-	 * @param array $arguments First element `id` can be omitted if it is autoincrement field
-	 *
-	 * @return false|int|string Id of created item on success, `false` otherwise
+	 * @param int|string $id
+	 * @param array      $joined_tables
 	 */
-	protected function create_simple ($arguments) {
-		return $this->create($arguments);
-	}
-	/**
-	 * Read item
-	 *
-	 * @param int|int[]|string|string[] $id
-	 *
-	 * @return array|false
-	 */
-	protected function read ($id) {
-		//TODO remove in future versions
-		if (func_num_args() === 3) {
-			return call_user_func([$this, 'read_internal'], func_get_args());
-		}
-		return $this->read_internal($this->table, $this->data_model, $id);
-	}
-	/**
-	 * Read item
-	 *
-	 * @param string                    $table
-	 * @param callable[]|string[]       $data_model
-	 * @param int|int[]|string|string[] $id
-	 *
-	 * @return array|false
-	 */
-	private function read_internal ($table, $data_model, $id) {
-		if (is_array($id)) {
-			foreach ($id as &$i) {
-				$i = $this->read_internal($table, $data_model, $i);
+	private function update_joined_tables ($id, $joined_tables) {
+		$clang = $this->db_prime()->s(Language::instance()->clang, false);
+		/**
+		 * At first we remove all old data
+		 */
+		foreach ($this->data_model as $table => $model) {
+			if (!isset($model['data_model'])) {
+				continue;
 			}
-			return $id;
-		}
-		$columns      = "`".implode("`,`", array_keys($data_model))."`";
-		$first_column = array_keys($data_model)[0];
-		$data         = $this->db()->qf(
-			[
-				"SELECT $columns
-				FROM `$table`
-				WHERE `$first_column` = '%s'
-				LIMIT 1",
+			$id_field                 = array_keys($model['data_model'])[0];
+			$language_field_condition = isset($model['language_field'])
+				? "AND `$model[language_field]` = '$clang'"
+				: '';
+			$this->db_prime()->q(
+				"DELETE FROM `{$this->table}_$table`
+				WHERE
+					`$id_field`	= '%s'
+					$language_field_condition",
 				$id
-			]
-		) ?: false;
-		foreach (array_keys($this->data_model) as $field) {
-			/**
-			 * Handle multilingual fields automatically
-			 */
-			if (strpos($this->data_model[$field], 'ml:') === 0) {
-				$data[$field] = Text::instance()->process($this->cdb(), $data[$field], true);
+			);
+		}
+		$id = $this->db_prime()->s($id, false);
+		/**
+		 * Now walk through all tables and insert new valued
+		 */
+		foreach ($joined_tables as $table => $model) {
+			if (!$model['data']) {
+				continue;
 			}
-			/**
-			 * Decode JSON fields
-			 */
-			if (in_array($this->data_model[$field], ['json', 'ml:json'])) {
-				$data[$field] = _json_decode($data[$field]);
+			$fields = "`$model[id_field]`, ";
+			/** @noinspection DisconnectedForeachInstructionInspection */
+			$values = "'$id'";
+			if (isset($model['language_field'])) {
+				$fields .= "`$model[language_field]`, ";
+				$values .= ",'$clang'";
 			}
+			$fields .= '`'.implode('`,`', array_keys($model['fields'])).'`';
+			$values .= str_repeat(",'%s'", count($model['fields']));
+			$this->db_prime()->insert(
+				"INSERT INTO `{$this->table}_$table`
+					(
+						$fields
+					) VALUES (
+						$values
+					)",
+				$model['data']
+			);
 		}
-		return $data;
-	}
-	/**
-	 * @deprecated
-	 * @todo remove in future versions
-	 *
-	 * @param int|int[]|string|string[] $id
-	 *
-	 * @return array|false
-	 */
-	protected function read_simple ($id) {
-		return $this->read($id);
-	}
-	/**
-	 * Update item
-	 *
-	 * @param array $arguments
-	 *
-	 * @return bool
-	 */
-	protected function update ($arguments) {
-		//TODO remove in future versions
-		if (func_num_args() === 3) {
-			return call_user_func([$this, 'update_internal'], func_get_args());
-		}
-		return $this->update_internal($this->table, $this->data_model, $arguments);
-	}
-	/**
-	 * Update item
-	 *
-	 * @param string              $table
-	 * @param callable[]|string[] $data_model
-	 * @param array               $arguments
-	 * @param bool                $files_update
-	 *
-	 * @return bool
-	 */
-	private function update_internal ($table, $data_model, $arguments, $files_update = true) {
-		$prepared_arguments = $arguments;
-		$id                 = array_shift($prepared_arguments);
-		if ($files_update) {
-			$data_before = $this->read_internal($table, $data_model, $id);
-		}
-		$prepared_arguments   = self::crud_arguments_preparation(array_slice($data_model, 1), $prepared_arguments, $id);
-		$columns              = implode(
-			',',
-			array_map(
-				function ($column) {
-					return "`$column` = '%s'";
-				},
-				array_keys($prepared_arguments)
-			)
-		);
-		$prepared_arguments[] = $id;
-		$first_column         = array_keys($data_model)[0];
-		if (!$this->db_prime()->q(
-			"UPDATE `$table`
-			SET $columns
-			WHERE `$first_column` = '%s'
-			LIMIT 1",
-			$prepared_arguments
-		)
-		) {
-			return false;
-		}
-		if ($files_update) {
-			/** @noinspection PhpUndefinedVariableInspection */
-			$this->find_update_files_tags($id, $data_before, $arguments);
-		}
-		return true;
 	}
 	/**
 	 * @param int|string     $id
@@ -404,6 +361,211 @@ trait CRUD {
 	 * @deprecated
 	 * @todo remove in future versions
 	 *
+	 * @param array $arguments First element `id` can be omitted if it is autoincrement field
+	 *
+	 * @return false|int|string Id of created item on success, `false` otherwise
+	 */
+	protected function create_simple ($arguments) {
+		return $this->create($arguments);
+	}
+	/**
+	 * Read item
+	 *
+	 * @param int|int[]|string|string[] $id
+	 *
+	 * @return array|false
+	 */
+	protected function read ($id) {
+		//TODO remove in future versions
+		if (func_num_args() === 3) {
+			return call_user_func([$this, 'read_internal'], func_get_args());
+		}
+		return $this->read_internal($this->table, $this->data_model, $id);
+	}
+	/**
+	 * Read item
+	 *
+	 * @param string                    $table
+	 * @param callable[]|string[]       $data_model
+	 * @param int|int[]|string|string[] $id
+	 *
+	 * @return array|false
+	 */
+	private function read_internal ($table, $data_model, $id) {
+		if (is_array($id)) {
+			foreach ($id as &$i) {
+				$i = $this->read_internal($table, $data_model, $i);
+			}
+			return $id;
+		}
+		$columns      = array_filter(
+			$data_model,
+			function ($column) {
+				return !isset($column['data_model']);
+			}
+		);
+		$columns      = "`".implode("`,`", array_keys($columns))."`";
+		$first_column = array_keys($data_model)[0];
+		$data         = $this->db()->qf(
+			[
+				"SELECT $columns
+				FROM `$table`
+				WHERE `$first_column` = '%s'
+				LIMIT 1",
+				$id
+			]
+		) ?: false;
+		if (!$data) {
+			return false;
+		}
+		foreach ($this->data_model as $field => $model) {
+			if (isset($model['data_model'])) {
+				$data[$field] = $this->read_joined_table($id, $field, $model);
+				continue;
+			}
+			if (!is_string($model)) {
+				continue;
+			}
+			/**
+			 * Handle multilingual fields automatically
+			 */
+			if (strpos($model, 'ml:') === 0) {
+				$data[$field] = Text::instance()->process($this->cdb(), $data[$field], true);
+			}
+			/**
+			 * Decode JSON fields
+			 */
+			if (in_array($model, ['json', 'ml:json'])) {
+				$data[$field] = _json_decode($data[$field]);
+			}
+		}
+		return $data;
+	}
+	/**
+	 * @param int|string $id
+	 * @param string     $table
+	 * @param array      $model
+	 *
+	 * @return array
+	 */
+	private function read_joined_table ($id, $table, $model) {
+		$clang                    = $this->db()->s(Language::instance()->clang, false);
+		$id_field                 = array_keys($model['data_model'])[0];
+		$language_field_condition = isset($model['language_field'])
+			? "AND `$model[language_field]` = '$clang'"
+			: '';
+		$fields                   = '`'.implode('`,`', array_keys($model['data_model'])).'`';
+		$rows                     = $this->db_prime()->qfa(
+			[
+				"SELECT $fields
+				FROm `{$this->table}_$table`
+				WHERE
+						`$id_field`	= '%s'
+						$language_field_condition",
+				$id
+			]
+		) ?: [];
+		foreach ($rows as &$row) {
+			/**
+			 * Remove language field from array
+			 */
+			if ($language_field_condition) {
+				unset($row[$model['language_field']]);
+			}
+			/**
+			 * Drop id field
+			 */
+			unset($row[$id_field]);
+			foreach ($row as $field => &$value) {
+				if ($model['data_model'][$field] == 'json') {
+					$value = _json_decode($value);
+				}
+			}
+			if (isset($model['indexed']) && $model['indexed']) {
+				$row = array_values($row);
+			} elseif (count($row) == 1) {
+				/**
+				 * If row is array that contains only one item - lets make resulting array flat
+				 */
+				$row = array_pop($row);
+			}
+		}
+		return $rows;
+	}
+	/**
+	 * @deprecated
+	 * @todo remove in future versions
+	 *
+	 * @param int|int[]|string|string[] $id
+	 *
+	 * @return array|false
+	 */
+	protected function read_simple ($id) {
+		return $this->read($id);
+	}
+	/**
+	 * Update item
+	 *
+	 * @param array $arguments
+	 *
+	 * @return bool
+	 */
+	protected function update ($arguments) {
+		//TODO remove in future versions
+		if (func_num_args() === 3) {
+			return call_user_func([$this, 'update_internal'], func_get_args());
+		}
+		return $this->update_internal($this->table, $this->data_model, $arguments);
+	}
+	/**
+	 * Update item
+	 *
+	 * @param string              $table
+	 * @param callable[]|string[] $data_model
+	 * @param array               $arguments
+	 * @param bool                $files_update
+	 *
+	 * @return bool
+	 */
+	private function update_internal ($table, $data_model, $arguments, $files_update = true) {
+		$prepared_arguments = $arguments;
+		$id                 = array_shift($prepared_arguments);
+		if ($files_update) {
+			$data_before = $this->read_internal($table, $data_model, $id);
+		}
+		list($prepared_arguments, $joined_tables) = self::crud_arguments_preparation(array_slice($data_model, 1), $prepared_arguments, $id);
+		$columns              = implode(
+			',',
+			array_map(
+				function ($column) {
+					return "`$column` = '%s'";
+				},
+				array_keys($prepared_arguments)
+			)
+		);
+		$prepared_arguments[] = $id;
+		$first_column         = array_keys($data_model)[0];
+		if (!$this->db_prime()->q(
+			"UPDATE `$table`
+			SET $columns
+			WHERE `$first_column` = '%s'
+			LIMIT 1",
+			$prepared_arguments
+		)
+		) {
+			return false;
+		}
+		if ($files_update) {
+			$this->update_joined_tables($id, $joined_tables);
+			/** @noinspection PhpUndefinedVariableInspection */
+			$this->find_update_files_tags($id, $data_before, $arguments);
+		}
+		return true;
+	}
+	/**
+	 * @deprecated
+	 * @todo remove in future versions
+	 *
 	 * @param array $arguments
 	 *
 	 * @return bool
@@ -459,6 +621,7 @@ trait CRUD {
 					}
 				}
 			}
+			$this->update_joined_tables($i, []);
 			$this->delete_files_tags($i);
 		}
 		return $result;
