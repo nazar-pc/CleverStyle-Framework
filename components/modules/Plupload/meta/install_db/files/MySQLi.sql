f0155193abeca7fd6aac90fe506781a607b5f704
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-07 22:34:14 +0200

    `utf8mb4` in Plupload

--- components/modules/Plupload/meta/install_db/files/MySQLi.sql
+++ components/modules/Plupload/meta/install_db/files/MySQLi.sql
@@ -5,14 +5,14 @@ CREATE TABLE IF NOT EXISTS `[prefix]plupload_files` (
 	`source` varchar(255) NOT NULL COMMENT 'Path locally on storage',
 	`url` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`),
-	UNIQUE KEY `url` (`url`),
+	UNIQUE KEY `url` (`url`(191)),
 	KEY `user` (`user`),
 	KEY `uploaded` (`uploaded`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]plupload_files_tags` (
 	`id` bigint(20) unsigned NOT NULL,
 	`tag` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`,`tag`),
-	KEY `tag` (`tag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	PRIMARY KEY (`id`,`tag`(191)),
+	KEY `tag` (`tag`(191))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
