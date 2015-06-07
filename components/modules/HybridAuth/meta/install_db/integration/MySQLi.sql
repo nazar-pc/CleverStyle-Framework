9ef7f8d190c15bfd51a1f0ab87e82e6c1b5d35c1
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-07 22:07:13 +0200

    `utf8mb4` in HybridAuth

--- components/modules/HybridAuth/meta/install_db/integration/MySQLi.sql
+++ components/modules/HybridAuth/meta/install_db/integration/MySQLi.sql
@@ -3,12 +3,13 @@ CREATE TABLE IF NOT EXISTS `[prefix]users_social_integration` (
   `provider` varchar(255) NOT NULL,
   `identifier` varchar(255) NOT NULL COMMENT 'Identifier of provider (unique for every provider)',
   `profile` varchar(1024) NOT NULL,
-  UNIQUE KEY `provider` (`provider`,`identifier`),
+  UNIQUE KEY `provider` (`provider`(191),`identifier`(191)),
   KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `[prefix]users_social_integration_contacts` (
 	`id` int(10) unsigned NOT NULL COMMENT 'User id',
 	`provider` varchar(255) NOT NULL,
 	`identifier` varchar(255) NOT NULL,
 	KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
