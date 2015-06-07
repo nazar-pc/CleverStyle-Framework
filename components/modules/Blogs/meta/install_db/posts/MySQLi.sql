fcacd91cd57c4182bf0fad1542915d3bfb006c8c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-07 21:41:49 +0200

    `utf8mb4` in Blogs

--- components/modules/Blogs/meta/install_db/posts/MySQLi.sql
+++ components/modules/Blogs/meta/install_db/posts/MySQLi.sql
@@ -5,20 +5,20 @@ CREATE TABLE IF NOT EXISTS `[prefix]blogs_posts` (
   `title` varchar(1024) NOT NULL,
   `path` varchar(255) NOT NULL,
   `content` mediumtext NOT NULL,
-	`draft` tinyint(1) unsigned NOT NULL,
+  `draft` tinyint(1) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user` (`user`),
   KEY `date` (`date`),
   KEY `path` (`path`),
-	KEY `draft` (`draft`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  KEY `draft` (`draft`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]blogs_posts_sections` (
   `id` int(11) NOT NULL COMMENT 'Post id',
   `section` int(11) NOT NULL COMMENT 'Category id',
   KEY `id` (`id`),
   KEY `section` (`section`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]blogs_posts_tags` (
   `id` bigint(20) NOT NULL COMMENT 'Post id',
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `[prefix]blogs_posts_tags` (
   `lang` varchar(2) NOT NULL,
   KEY `id` (`id`),
   KEY `tag` (`tag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]blogs_sections` (
   `id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
@@ -36,22 +36,22 @@ CREATE TABLE IF NOT EXISTS `[prefix]blogs_sections` (
   PRIMARY KEY (`id`),
   KEY `parent` (`parent`),
   KEY `path` (`path`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]blogs_tags` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `text` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `text` (`text`(255))
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  UNIQUE KEY `text` (`text`(191))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]texts` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `label` varchar(1024) NOT NULL,
   `group` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `label` (`label`(255),`group`(255))
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  KEY `label` (`label`(191),`group`(191))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]texts_data` (
   `id` bigint(20) NOT NULL COMMENT 'id from texts table',
@@ -60,4 +60,4 @@ CREATE TABLE IF NOT EXISTS `[prefix]texts_data` (
   `text` mediumtext NOT NULL,
   PRIMARY KEY (`id`,`lang`),
   KEY `id_` (`id_`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
