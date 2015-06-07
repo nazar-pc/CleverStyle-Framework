d397b480984867158ee683b341312d9140918457
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-07 21:56:29 +0200

    `utf8mb4` in Content

--- components/modules/Content/meta/install_db/content/MySQLi.sql
+++ components/modules/Content/meta/install_db/content/MySQLi.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS `[prefix]content` (
 	`title` varchar(1024) NOT NULL,
 	`content` text NOT NULL,
 	`type` set('text','html') NOT NULL,
-	PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	PRIMARY KEY (`key`(191))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS `[prefix]texts` (
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS `[prefix]texts` (
   `label` varchar(1024) NOT NULL,
   `group` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `label` (`label`(255),`group`(255))
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  KEY `label` (`label`(191),`group`(191))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `[prefix]texts_data` (
   `id` bigint(20) NOT NULL COMMENT 'id from texts table',
@@ -22,4 +22,4 @@ CREATE TABLE IF NOT EXISTS `[prefix]texts_data` (
   `text` mediumtext NOT NULL,
   PRIMARY KEY (`id`,`lang`),
   KEY `id_` (`id_`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
