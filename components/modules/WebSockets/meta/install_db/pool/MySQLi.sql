ab4d97e4aa8b288dd03ef399bf9e46b72c6bebf9
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-09 05:18:11 +0200

    Tiny fix

--- components/modules/WebSockets/meta/install_db/pool/MySQLi.sql
+++ components/modules/WebSockets/meta/install_db/pool/MySQLi.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `[prefix]websockets_pool` (
 	`address` varchar(1024) NOT NULL
-) ENGINE=MEMORY DEFAULT CHARSET=utf8;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4_unicode_ci;
 
 ALTER TABLE `[prefix]websockets_pool`
 ADD PRIMARY KEY (`address`(191));
