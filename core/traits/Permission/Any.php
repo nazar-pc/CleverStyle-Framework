43e61b808a2c856c705122f91fbd985674e075cc
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-06-11 22:26:18 +0200

    Simplification of permissions management, less DB request on change

--- core/traits/Permission/Any.php
+++ core/traits/Permission/Any.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2013-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CleverStyle CMS
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2013-2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs\Permission;
-use			cs\Cache;
+namespace cs\Permission;
+use
+	cs\Cache;
 /**
  * Class Any with common methods for User and Group classes
  *
@@ -15,156 +16,141 @@ use			cs\Cache;
  */
 trait Any {
 	/**
-	 * @param int			$id
-	 * @param string		$type
+	 * @param int    $id
+	 * @param string $type
 	 *
 	 * @return array|false
 	 */
 	protected function get_any_permissions ($id, $type) {
-		if (!($id = (int)$id)) {
+		$id = (int)$id;
+		if (!$id) {
 			return false;
 		}
 		switch ($type) {
 			case 'user':
-				$table	= '[prefix]users_permissions';
+				$table = '[prefix]users_permissions';
 				break;
 			case 'group':
-				$table	= '[prefix]groups_permissions';
+				$table = '[prefix]groups_permissions';
 				break;
 			default:
 				return false;
 		}
-		return $this->cache->get("permissions/$id", function () use ($id, $table) {
-			$permissions	= false;
-			if ($permissions_array = $this->db()->qfa(
-				"SELECT
-					`permission`,
-					`value`
-				FROM `$table`
-				WHERE `id` = '$id'"
-			)) {
-				$permissions = [];
-				foreach ($permissions_array as $permission) {
-					$permissions[$permission['permission']] = (int)(bool)$permission['value'];
+		return $this->cache->get(
+			"permissions/$id",
+			function () use ($id, $table) {
+				$permissions       = false;
+				$permissions_array = $this->db()->qfa(
+					"SELECT
+						`permission`,
+						`value`
+					FROM `$table`
+					WHERE `id` = '$id'"
+				);
+				if ($permissions_array) {
+					$permissions = [];
+					foreach ($permissions_array as $permission) {
+						$permissions[$permission['permission']] = (int)(bool)$permission['value'];
+					}
 				}
+				return $permissions;
 			}
-			return $permissions;
-		});
+		);
 	}
 	/**
-	 * @param array		$data
-	 * @param int		$id
-	 * @param string	$type
+	 * @param array  $data
+	 * @param int    $id
+	 * @param string $type
 	 *
 	 * @return bool
 	 */
 	protected function set_any_permissions ($data, $id, $type) {
-		$id		= (int)$id;
+		$id = (int)$id;
 		if (!is_array($data) || empty($data) || !$id) {
 			return false;
 		}
 		switch ($type) {
 			case 'user':
-				$table	= '[prefix]users_permissions';
+				$table = '[prefix]users_permissions';
 				break;
 			case 'group':
-				$table	= '[prefix]groups_permissions';
+				$table = '[prefix]groups_permissions';
 				break;
 			default:
 				return false;
 		}
-		$delete	= [];
-		foreach ($data as $i => $val) {
-			if ($val == -1) {
-				$delete[] = (int)$i;
-				unset($data[$i]);
+		$insert_update = [];
+		$delete        = [];
+		foreach ($data as $permission => $value) {
+			if ($value == -1) {
+				$delete[] = (int)$permission;
+			} else {
+				$insert_update[] = [$permission, (int)(bool)$value];
 			}
 		}
-		unset($i, $val);
-		$return	= true;
-		if (!empty($delete)) {
-			$delete	= implode(', ', $delete);
-			$return	= $this->db_prime()->q(
-				"DELETE FROM `$table` WHERE `id` = '$id' AND `permission` IN ($delete)"
+		unset($permission, $value);
+		$return = true;
+		if ($delete) {
+			$delete = implode(', ', $delete);
+			$return = $this->db_prime()->q(
+				"DELETE FROM `$table`
+				WHERE
+					`id` = '$id' AND
+					`permission` IN ($delete)"
 			);
 		}
 		unset($delete);
-		if (!empty($data)) {
-			$exiting	= $this->get_any_permissions($id, $type);
-			if (!empty($exiting)) {
-				$update		= [];
-				foreach ($exiting as $permission => $value) {
-					if (isset($data[$permission]) && $data[$permission] != $value) {
-						$value		= (int)(bool)$data[$permission];
-						$update[]	=
-							"UPDATE `$table`
-							SET `value` = '$value'
-							WHERE
-								`permission`	= '$permission' AND
-								`id`			= '$id'";
-					}
-					unset($data[$permission]);
-				}
-				unset($exiting, $permission, $value);
-				if (!empty($update)) {
-					$return = $return && $this->db_prime()->q($update);
-				}
-				unset($update);
-			}
-			/** @noinspection NotOptimalIfConditionsInspection */
-			if (!empty($data)) {
-				$insert	= [];
-				foreach ($data as $permission => $value) {
-					$insert[] = $id.', '.(int)$permission.', '.(int)(bool)$value;
-				}
-				unset($permission, $value);
-				if (!empty($insert)) {
-					$insert	= implode('), (', $insert);
-					$return	= $return && $this->db_prime()->q(
-						"INSERT INTO `$table`
-							(
-								`id`,
-								`permission`,
-								`value`
-							) VALUES (
-								$insert
-							)"
-					);
-				}
-			}
+		if ($insert_update) {
+			$return =
+				$return &&
+				$this->db_prime()->insert(
+					"INSERT INTO `$table`
+						(
+							`id`,
+							`permission`,
+							`value`
+						) VALUES (
+							'$id',
+							'%d',
+							'%d'
+						)
+					ON DUPLICATE KEY UPDATE `value` = VALUES(`value`)",
+					$insert_update
+				);
 		}
-		$Cache	= $this->cache;
-		unset($Cache->{"permissions/$id"});
+		unset($this->cache->{"permissions/$id"});
 		if ($type == 'group') {
 			unset(Cache::instance()->{'users/permissions'});
 		}
 		return (bool)$return;
 	}
 	/**
-	 * @param int		$id
-	 * @param string	$type
+	 * @param int    $id
+	 * @param string $type
 	 *
 	 * @return bool
 	 */
 	protected function del_any_permissions_all ($id, $type) {
-		$id			= (int)$id;
+		$id = (int)$id;
 		if (!$id) {
 			return false;
 		}
 		switch ($type) {
 			case 'user':
-				$table	= '[prefix]users_permissions';
-			break;
+				$table = '[prefix]users_permissions';
+				break;
 			case 'group':
-				$table	= '[prefix]groups_permissions';
-			break;
+				$table = '[prefix]groups_permissions';
+				break;
 			default:
 				return false;
 		}
-		$return = $this->db_prime()->q("DELETE FROM `$table` WHERE `id` = '$id'");
+		$return = $this->db_prime()->q(
+			"DELETE FROM `$table`
+			WHERE `id` = '$id'"
+		);
 		if ($return) {
-			$Cache	= $this->cache;
-			unset($Cache->{"permissions/$id"});
+			unset($this->cache->{"permissions/$id"});
 			if ($type == 'group') {
 				unset(Cache::instance()->{'users/permissions'});
 			}
