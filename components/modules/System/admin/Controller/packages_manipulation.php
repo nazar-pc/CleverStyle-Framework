nazar-pc/CleverStyle-Framework

commit 61ac5b520a4a41c5d94bac02bde5ad6c89d3ef13

Simplification of files extraction checks, fixes for HHVM in packages installation/extraction similarly to system installation

components/modules/System/admin/Controller/packages_manipulation.php
@@ -59,16 +59,20 @@ trait packages_manipulation {
 	static protected function install_extract ($target_directory, $source_phar) {
 		$tmp_dir = "phar://$source_phar";
 		$fs      = file_get_json("$tmp_dir/fs.json");
-		$extract = array_product(
+		$extract = array_filter(
 			array_map(
 				function ($index, $file) use ($tmp_dir, $target_directory) {
 					if (
 						!file_exists(dirname("$target_directory/$file")) &&
 						!mkdir(dirname("$target_directory/$file"), 0770, true)
 					) {
-						return 0;
+						return false;
 					}
-					return (int)copy("$tmp_dir/fs/$index", "$target_directory/$file");
+					/**
+					 * TODO: copy() + file_exists() is a hack for HHVM, when bug fixed upstream (copying of empty files) this should be simplified
+					 */
+					copy("$tmp_dir/fs/$index", "$target_directory/$file");
+					return file_exists("$target_directory/$file");
 				},
 				$fs,
 				array_keys($fs)
@@ -103,16 +107,20 @@ trait packages_manipulation {
 		 */
 		$tmp_dir = "phar://$source_phar";
 		$fs      = file_get_json("$tmp_dir/fs.json");
-		$extract = array_product(
+		$extract = array_filter(
 			array_map(
 				function ($index, $file) use ($tmp_dir, $target_directory) {
 					if (
 						!file_exists(dirname("$target_directory/$file")) &&
 						!mkdir(dirname("$target_directory/$file"), 0770, true)
 					) {
-						return 0;
+						return false;
 					}
-					return (int)copy("$tmp_dir/fs/$index", "$target_directory/$file");
+					/**
+					 * TODO: copy() + file_exists() is a hack for HHVM, when bug fixed upstream (copying of empty files) this should be simplified
+					 */
+					copy("$tmp_dir/fs/$index", "$target_directory/$file");
+					return file_exists("$target_directory/$file");
 				},
 				$fs,
 				array_keys($fs)

install/functions.php
@@ -223,20 +223,20 @@ function install_process ($fs, $argv = null) {
 	/**
 	 * Extracting of engine's files
 	 */
-	$extract = array_product(
+	$extract = array_filter(
 		array_map(
 			function ($index, $file) {
 				if (
 					!file_exists(dirname(ROOT."/$file")) &&
 					!mkdir(dirname(ROOT."/$file"), 0770, true)
 				) {
-					return 0;
+					return false;
 				}
 				/**
 				 * TODO: copy() + file_exists() is a hack for HHVM, when bug fixed upstream (copying of empty files) this should be simplified
 				 */
 				copy(DIR."/fs/$index", ROOT."/$file");
-				return (int)file_exists(ROOT."/$file");
+				return file_exists(ROOT."/$file");
 			},
 			$fs,
 			array_keys($fs)

commit 252839ca51536e6e6c810d28ef9e84cee4080cdd

Fix for incorrect extraction success detection, introduced in 61ac5b5

components/modules/System/admin/Controller/packages_manipulation.php
@@ -57,9 +57,9 @@ trait packages_manipulation {
 	 * @return bool
 	 */
 	static protected function install_extract ($target_directory, $source_phar) {
-		$tmp_dir = "phar://$source_phar";
-		$fs      = file_get_json("$tmp_dir/fs.json");
-		$extract = array_filter(
+		$tmp_dir   = "phar://$source_phar";
+		$fs        = file_get_json("$tmp_dir/fs.json");
+		$extracted = array_filter(
 			array_map(
 				function ($index, $file) use ($tmp_dir, $target_directory) {
 					if (
@@ -79,10 +79,11 @@ trait packages_manipulation {
 			)
 		);
 		unlink($source_phar);
-		if ($extract) {
+		if (count($extracted) === count($fs)) {
 			file_put_json("$target_directory/fs.json", array_keys($fs));
+			return true;
 		}
-		return (bool)$extract;
+		return false;
 	}
 	/**
 	 * Generic extraction of files from phar distributive for CleverStyle CMS (system and components update)
@@ -105,9 +106,9 @@ trait packages_manipulation {
 		/**
 		 * Extracting new versions of files
 		 */
-		$tmp_dir = "phar://$source_phar";
-		$fs      = file_get_json("$tmp_dir/fs.json");
-		$extract = array_filter(
+		$tmp_dir   = "phar://$source_phar";
+		$fs        = file_get_json("$tmp_dir/fs.json");
+		$extracted = array_filter(
 			array_map(
 				function ($index, $file) use ($tmp_dir, $target_directory) {
 					if (
@@ -128,7 +129,7 @@ trait packages_manipulation {
 		);
 		unlink($source_phar);
 		unset($tmp_dir);
-		if (!$extract) {
+		if (count($extracted) === count($fs)) {
 			return false;
 		}
 		unset($extract);

install/functions.php
@@ -223,7 +223,7 @@ function install_process ($fs, $argv = null) {
 	/**
 	 * Extracting of engine's files
 	 */
-	$extract = array_filter(
+	$extracted = array_filter(
 		array_map(
 			function ($index, $file) {
 				if (
@@ -243,7 +243,7 @@ function install_process ($fs, $argv = null) {
 		)
 	);
 	if (
-		!$extract ||
+		count($extracted) !== count($fs) ||
 		!(file_exists(ROOT.'/storage') || mkdir(ROOT.'/storage', 0770)) ||
 		!file_put_contents(ROOT.'/storage/.htaccess', "Deny from all\nRewriteEngine Off\n<Files *>\n\tSetHandler default-handler\n</Files>")
 	) {