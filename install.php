4c9196742c27c58fd679855e72774250bb293c38
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-07-15 03:35:29 +0200

    Long keys in installer now uses two dashed, not one, namely `--help` instead of `-help`, however, old way is kept for backward compatibility until 3.0

--- install.php
+++ install.php
@@ -29,6 +29,7 @@ require DIR.'/fs/'.$fs['core/classes/h/Base.php'];
 require DIR.'/fs/'.$fs['core/classes/h.php'];
 require DIR.'/install/functions.php';
 date_default_timezone_set('UTC');
+// TODO remove in future versions long keys with single dash, like `-help`, modern version `--help` should be used instead
 if ($cli) {
 	$help        = false;
 	$interactive = false;
@@ -36,62 +37,77 @@ if ($cli) {
 		switch ($argv[$i]) {
 			case '-h':
 			case '-help':
+			case '--help':
 				$help = true;
 				break;
 			case '-sn':
 			case '-site_name':
+			case '--site_name':
 				$_POST['site_name'] = $argv[$i + 1];
 				break;
 			case '-su':
 			case '-site_url':
+			case '--site_url':
 				$_POST['site_url'] = $argv[$i + 1];
 				break;
 			case '-de':
 			case '-db_engine':
+			case '--db_engine':
 				$_POST['db_engine'] = $argv[$i + 1];
 				break;
 			case '-dh':
 			case '-db_host':
+			case '--db_host':
 				$_POST['db_host'] = $argv[$i + 1];
 				break;
 			case '-dn':
 			case '-db_name':
+			case '--db_name':
 				$_POST['db_name'] = $argv[$i + 1];
 				break;
 			case '-du':
 			case '-db_user':
+			case '--db_user':
 				$_POST['db_user'] = $argv[$i + 1];
 				break;
 			case '-dp':
 			case '-db_password':
+			case '--db_password':
 				$_POST['db_password'] = $argv[$i + 1];
 				break;
 			case '-dr':
 			case '-db_prefix':
+			case '--db_prefix':
 				$_POST['db_prefix'] = $argv[$i + 1];
 				break;
 			case '-dc':
 			case '-db_charset':
+			case '--db_charset':
 				$_POST['db_charset'] = $argv[$i + 1];
 				break;
 			case '-t':
 			case '-timezone':
+			case '--timezone':
 				$_POST['timezone'] = $argv[$i + 1];
 				break;
 			case '-l':
 			case '-language':
+			case '--language':
 				$_POST['language'] = $argv[$i + 1];
 				break;
 			case '-ae':
 			case '-admin_email':
+			case '--admin_email':
 				$_POST['admin_email'] = $argv[$i + 1];
 				break;
 			case '-ap':
 			case '-admin_password':
+			case '--admin_password':
 				$_POST['admin_password'] = $argv[$i + 1];
 				break;
 			case '-i':
 			case '-interactive':
+			case '--interactive':
 				$interactive = true;
 				break;
 		}
@@ -130,51 +146,51 @@ if ($cli) {
 		echo "CleverStyle CMS installer
 Installer is used for installation of CleverStyle CMS and built-in components from distributive.
 Usage: php $argv[0]
-         -site_name <site_name>
-         -site_url <site_url>
-         -db_name <db_name>
-         -db_user <db_user>
-         -db_password <db_password>
-         -admin_email <admin_email>
-         -admin_password <admin_password>
-         [-help]
-         [-interactive]
-         [-db_engine <db_engine>]
-         [-db_host <db_host>]
-         [-db_prefix <db_prefix>]
-         [-db_charset <db_charset>]
-         [-timezone <timezone>]
-         [-language <language>]
+         --site_name <site_name>
+         --site_url <site_url>
+         --db_name <db_name>
+         --db_user <db_user>
+         --db_password <db_password>
+         --admin_email <admin_email>
+         --admin_password <admin_password>
+         [--help]
+         [--interactive]
+         [--db_engine <db_engine>]
+         [--db_host <db_host>]
+         [--db_prefix <db_prefix>]
+         [--db_charset <db_charset>]
+         [--timezone <timezone>]
+         [--language <language>]
   -h
-  -help           - This information
+  --help           - This information
   -i
-  -interactive    - Interactive mode (can be combined with other options, will ask only required)
+  --interactive    - Interactive mode (can be combined with other options, will ask only required)
   -sn
-  -site_name      - Name of future site, in case of few words, do not forget to take into quotes
+  --site_name      - Name of future site, in case of few words, do not forget to take into quotes
   -su
-  -site_url       - Site url with protocol prefix, without final slash
+  --site_url       - Site url with protocol prefix, without final slash
   -de
-  -db_engine      - Database engine, only MySQLi currently supported
+  --db_engine      - Database engine, only MySQLi currently supported
   -dh
-  -db_host        - Database host
+  --db_host        - Database host
   -dn
-  -db_name        - Database name
+  --db_name        - Database name
   -du
-  -db_user        - Database user
+  --db_user        - Database user
   -dp
-  -db_password    - Database password
+  --db_password    - Database password
   -dr
-  -db_prefix      - Is used for prefixing all tables names
+  --db_prefix      - Is used for prefixing all tables names
   -dc
-  -db_charset     - Database charset
+  --db_charset     - Database charset
   -t
-  -timezone       - Timezone, check http://php.net/manual/en/suffixtimezones.php for possible values
+  --timezone       - Timezone, check http://php.net/manual/en/suffixtimezones.php for possible values
   -l
-  -language       - Language, currently English, Українська and Русский languages supported
+  --language       - Language, currently English, Українська and Русский languages supported
   -ae
-  -admin_email    - Email of first, primary administrator
+  --admin_email    - Email of first, primary administrator
   -ap
-  -admin_password - Password of first administrator
+  --admin_password - Password of first administrator
 Examples:
   php $argv[0] -sn Web-site -su http://web.site -dn web.site -du web.site -dp pass -ae admin@web.site -ap pass
   php $argv[0] -i
