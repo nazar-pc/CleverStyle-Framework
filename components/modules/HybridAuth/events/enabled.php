737b09a0e458b6f741bef8a16af75538ba6921f4
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-08-29 09:07:04 +0200

    UIkit's dropdown is not used anywhere

--- components/modules/HybridAuth/events/enabled.php
+++ components/modules/HybridAuth/events/enabled.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package		HybridAuth
- * @category	modules
- * @author		HybridAuth authors
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com> (integration with CleverStyle CMS)
- * @copyright	HybridAuth authors
- * @license		MIT License, see license.txt
+ * @package        HybridAuth
+ * @category       modules
+ * @author         HybridAuth authors
+ * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com> (integration with CleverStyle CMS)
+ * @copyright      HybridAuth authors
+ * @license        MIT License, see license.txt
  */
-namespace	cs\modules\HybridAuth;
+namespace cs\modules\HybridAuth;
 use
 	h,
 	cs\Config,
@@ -19,18 +19,19 @@ use
 Event::instance()->on(
 	'System/Page/external_sign_in_list',
 	function ($data) {
-		$Config			= Config::instance();
-		$Page			= Page::instance();
-		$User			= User::instance();
+		$Config = Config::instance();
+		$Page   = Page::instance();
+		$User   = User::instance();
 		if (!(
 			$Page->interface &&
 			$Config->core['allow_user_registration'] &&
 			$User->guest() &&
 			!$User->bot()
-		)) {
+		)
+		) {
 			return;
 		}
-		$providers		= $Config->module('HybridAuth')->providers;
+		$providers = $Config->module('HybridAuth')->providers;
 		foreach ($providers as $provider => $pdata) {
 			if (!$pdata['enabled']) {
 				unset($providers[$provider]);
@@ -40,49 +41,37 @@ Event::instance()->on(
 		if (!count($providers)) {
 			return;
 		}
-		$L				= Language::instance();
-		$icon_mapper	= function ($provider) {
+		$L            = Language::instance();
+		$icon_mapper  = function ($provider) {
 			switch ($provider) {
-				case 'Facebook':
-					return 'facebook';
-				case 'Foursquare':
-					return 'foursquare';
-				case 'GitHub':
-					return 'github';
 				case 'Google':
 					return 'google-plus';
-				case 'Instagram':
-					return 'instagram';
-				case 'LinkedIn':
-					return 'linkedin';
-				case 'Tumblr':
-					return 'tumbrl';
-				case 'Twitter':
-					return 'twitter';
 				case 'Vkontakte':
 					return 'vk';
 				default:
-					return false;
+					return strtolower($provider);
 			}
 		};
-		$data['list']	= h::{'ul.cs-hybrid-auth-providers-list li'}(
-			[
+		$data['list'] = h::{'nav.cs-hybrid-auth-providers-list[is=cs-nav-dropdown] nav[is=cs-nav-button-group][vertical]'}(
+			h::p(
 				$L->or_sign_in_with,
 				[
-					'class'	=> 'uk-nav-header'
+					'class' => 'uk-nav-header'
 				]
-			],
-			array_map(
-				function ($provider) use ($L, $icon_mapper) {
-					return [
-						h::a(h::icon($icon_mapper($provider)).$L->$provider),
-						[
-							'data-provider'	=> $provider,
-							'class'			=> "cs-hybrid-auth-$provider"
-						]
-					];
-				},
-				array_keys($providers)
+			).
+			h::{'a[is=cs-link-button]'}(
+				array_map(
+					function ($provider) use ($L, $icon_mapper) {
+						return [
+							$L->$provider,
+							[
+								'href' => "/HybridAuth/$provider",
+								'icon' => $icon_mapper($provider)
+							]
+						];
+					},
+					array_keys($providers)
+				)
 			)
 		);
 	}
@@ -100,9 +89,9 @@ Event::instance()->on(
 	'System/User/del/after',
 	function ($data) {
 		/**
-		 *	@var \cs\DB\_Abstract $cdb
+		 * @var \cs\DB\_Abstract $cdb
 		 */
-		$cdb			= DB::instance()->{Config::instance()->module('HybridAuth')->db('integration')}();
+		$cdb = DB::instance()->{Config::instance()->module('HybridAuth')->db('integration')}();
 		$cdb->q(
 			[
 				"DELETE FROM `[prefix]users_social_integration`
@@ -117,9 +106,11 @@ Event::instance()->on(
 Event::instance()->on(
 	'System/User/get_contacts',
 	function ($data) {
-		$data['contacts']	= array_unique(array_merge(
-			$data['contacts'],
-			Social_integration::instance()->get_contacts($data['id'])
-		));
+		$data['contacts'] = array_unique(
+			array_merge(
+				$data['contacts'],
+				Social_integration::instance()->get_contacts($data['id'])
+			)
+		);
 	}
 );
--- components/modules/HybridAuth/includes/js/general.coffee
+++ components/modules/HybridAuth/includes/js/general.coffee
@@ -7,21 +7,16 @@
  * @license		MIT License, see license.txt
 ###
 $ ->
-	auth_list	= $('.cs-hybrid-auth-providers-list')
-	if auth_list.length
-		login_field	= $('.cs-header-sign-in-email')
-		list_items	= auth_list.children(':not(:first-child)')
-		auth_list.addClass('uk-nav uk-nav-dropdown')
-		login_field
-			.wrap('<span/>')
-			.parent()
-				.append(
-					$('<div/>')
-						.append(auth_list.detach())
-						.addClass('uk-dropdown uk-dropdown-small')
-				)
-				.addClass('uk-button-dropdown')
-				.attr('data-uk-dropdown', '')
-		list_items.click ->
-			location.href = cs.base_url+'/HybridAuth/'+$(this).data('provider')
+	dropdown = document.querySelector('.cs-hybrid-auth-providers-list')
+	if dropdown
+		login_field		= $('.cs-header-sign-in-email')
+		timeout			= null
+		login_field.focus ->
+			dropdown.target	= @
+			timeout			= setTimeout (->
+				dropdown.open()
+			), 1000
+		login_field.blur ->
+			clearTimeout(timeout)
+			dropdown.open()
 	return
