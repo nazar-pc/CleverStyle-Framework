ea426498eb4cd1dab5fb144a56a9f5754a552f91
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-09-05 19:31:42 +0200

    Deferred tasks module UI adjusted for changes in system core

--- components/modules/Deferred_tasks/admin/index.php
+++ components/modules/Deferred_tasks/admin/index.php
@@ -11,42 +11,37 @@ use
 	h,
 	cs\Language\Prefix;
 include __DIR__.'/save.php';
-$Config      = Config::instance();
-$module_data = $Config->module('Deferred_tasks');
-$Index       = Index::instance();
-$L           = new Prefix('deferred_tasks_');
-$base_url    = $Config->base_url();
+$Config                       = Config::instance();
+$module_data                  = $Config->module('Deferred_tasks');
+$Index                        = Index::instance();
+$L                            = new Prefix('deferred_tasks_');
+$base_url                     = $Config->base_url();
+$Index->form_attributes['is'] = 'cs-form';
 $Index->content(
-	h::{'table.cs-table[right-left] tr| td'}(
+	h::{'label info'}('deferred_tasks_security_key').
+	h::{'input[is=cs-input-text][full-width][name=general[security_key]]'}(
 		[
-			h::info('deferred_tasks_security_key'),
-			h::{'input[name=general[security_key]]'}(
-				[
-					'value' => $module_data->security_key
-				]
-			)
-		],
-		[
-			h::info('deferred_tasks_max_number_of_workers'),
-			h::{'input[type=number][min=1][name=general[max_number_of_workers]]'}(
-				[
-					'value' => $module_data->max_number_of_workers
-				]
-			)
+			'value' => $module_data->security_key
 		]
 	).
-	h::p(
-		$L->insert_line_into_cron,
-		h::{'input[readonly]'}(
-			[
-				'value' => "* * * * wget -O /dev/null $base_url/Deferred_tasks/$module_data->security_key"
-			]
-		),
-		$L->or_use_online_services,
-		h::{'input[readonly]'}(
-			[
-				'value' => "$base_url/Deferred_tasks/$module_data->security_key"
-			]
-		)
-	)
+	h::{'label info'}('deferred_tasks_max_number_of_workers').
+	h::{'input[is=cs-input-text][full-width][type=number][min=1][name=general[max_number_of_workers]]'}(
+		[
+			'value' => $module_data->max_number_of_workers
+		]
+	).
+	h::br(2).
+	h::label($L->insert_line_into_cron).
+	h::{'input[is=cs-input-text][full-width][readonly]'}(
+		[
+			'value' => "* * * * wget -O /dev/null $base_url/Deferred_tasks/$module_data->security_key"
+		]
+	).
+	h::label($L->or_use_online_services).
+	h::{'input[is=cs-input-text][full-width][readonly]'}(
+		[
+			'value' => "$base_url/Deferred_tasks/$module_data->security_key"
+		]
+	).
+	h::br()
 );
