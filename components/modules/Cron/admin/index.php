d3fd1f3bae70c43b0654a9389b7bf2f251dc18e4
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-09-05 19:23:09 +0200

    Cron module UI adjusted for changes in system core

--- components/modules/Cron/admin/index.php
+++ components/modules/Cron/admin/index.php
@@ -7,10 +7,13 @@
  * @license   MIT License, see license.txt
  */
 namespace cs;
-use            h;
-Index::instance()->content(
+use
+	h;
+$Index                        = Index::instance();
+$Index->form_attributes['is'] = 'cs-button';
+$Index->content(
 	h::{'p.cs-text-center'}(Language::instance()->crontab_content).
-	h::{'textarea[is=cs-textarea][autosize][name=tasks]'}(
+	h::{'textarea[is=cs-textarea][full-width][autosize][name=tasks][rows=10]'}(
 		isset($_POST['tasks']) ? $_POST['tasks'] : shell_exec('crontab -l')
 	).
 	h::br(2)
