53ac3b9663474c140c9932cf5ef7a5138b072737
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-09-12 16:39:23 +0200

    API for block deletion, some refactoring

--- components/modules/System/api/Controller/admin/blocks.php
+++ components/modules/System/api/Controller/admin/blocks.php
@@ -25,30 +25,31 @@ trait blocks {
 			$block['content'] = $block['content'] ? $Text->process($db_id, $block['content'], true) : '';
 		}
 		unset($block);
-		if (isset($route_ids[0])) {
-			foreach ($blocks as $block) {
-				if ($block['index'] == $route_ids[0]) {
-					$Page->json(
-						[
-							'title'    => $block['title'],
-							'type'     => $block['type'],
-							'active'   => (int)$block['active'],
-							'template' => $block['template'],
-							'start'    => date('Y-m-d\TH:i', $block['start'] ?: TIME),
-							'expire'   => [
-								'date'  => date('Y-m-d\TH:i', $block['expire'] ?: TIME),
-								'state' => (int)($block['expire'] != 0)
-							],
-							'content'  => $block['content']
-						]
-					);
-					return;
-				}
-			}
-			error_code(404);
-		} else {
+		if (!isset($route_ids[0])) {
 			$Page->json($blocks ?: []);
+			return;
 		}
+		foreach ($blocks as $block) {
+			if ($block['index'] != $route_ids[0]) {
+				continue;
+			}
+			$Page->json(
+				[
+					'title'    => $block['title'],
+					'type'     => $block['type'],
+					'active'   => (int)$block['active'],
+					'template' => $block['template'],
+					'start'    => date('Y-m-d\TH:i', $block['start'] ?: TIME),
+					'expire'   => [
+						'date'  => date('Y-m-d\TH:i', $block['expire'] ?: TIME),
+						'state' => (int)($block['expire'] != 0)
+					],
+					'content'  => $block['content']
+				]
+			);
+			return;
+		}
+		error_code(404);
 	}
 	static function admin_blocks_post () {
 		static::save_block_data($_POST);
@@ -59,6 +60,36 @@ trait blocks {
 			return;
 		}
 		static::save_block_data($_POST, $route_ids[0]);
+	}
+	static function admin_blocks_delete ($route_ids) {
+		if (!$route_ids[0]) {
+			error_code(400);
+			return;
+		}
+		$Config     = Config::instance();
+		$db_id      = $Config->module('System')->db('texts');
+		$Permission = Permission::instance();
+		$Text       = Text::instance();
+		foreach ($Config->components['blocks'] as $i => &$block) {
+			if ($block['index'] == $route_ids[0]) {
+				unset($Config->components['blocks'][$i]);
+				break;
+			}
+		}
+		/** @noinspection PhpUndefinedVariableInspection */
+		if ($block['index'] != $route_ids[0]) {
+			error_code(404);
+			return;
+		}
+		$block_permission = $Permission->get(null, 'Block', $block['index']);
+		if ($block_permission) {
+			$Permission->del($block_permission[0]['id']);
+		}
+		$Text->del($db_id, 'System/Config/blocks/title', $block['index']);
+		$Text->del($db_id, 'System/Config/blocks/content', $block['index']);
+		if (!$Config->save()) {
+			error_code(500);
+		}
 	}
 	static function admin_blocks_templates () {
 		Page::instance()->json(
@@ -73,9 +104,12 @@ trait blocks {
 	/**
 	 * @param array     $block_new
 	 * @param false|int $index Index of existing block, if not specified - new block being added
+	 *
+	 * @return bool
 	 */
 	protected static function save_block_data ($block_new, $index = false) {
 		$Config = Config::instance();
+		$db_id  = $Config->module('System')->db('texts');
 		$Text   = Text::instance();
 		$block  = [
 			'position' => 'floating',
@@ -93,41 +127,25 @@ trait blocks {
 				return;
 			}
 		}
-		$block['title']    = $Text->set(
-			$Config->module('System')->db('texts'),
-			'System/Config/blocks/title',
-			$block['index'],
-			$block_new['title']
-		);
+		$block['title']    = $Text->set($db_id, 'System/Config/blocks/title', $block['index'], $block_new['title']);
 		$block['active']   = $block_new['active'];
 		$block['type']     = $block_new['type'];
 		$block['template'] = $block_new['template'];
 		$block['start']    = $block_new['start'];
 		$block['start']    = strtotime($block_new['start']);
-		$block['expire']   = 0;
+		$block['expire']   = $block_new['expire']['state'] ? strtotime($block_new['expire']['date']) : 0;
 		$block['content']  = '';
-		if ($block_new['expire']['state']) {
-			$block['expire'] = strtotime($block_new['expire']['date']);
-		}
 		if ($block['type'] == 'html') {
-			$block['content'] = $Text->set(
-				$Config->module('System')->db('texts'),
-				'System/Config/blocks/content',
-				$block['index'],
-				xap($block_new['content'], true)
-			);
+			$block['content'] = $Text->set($db_id, 'System/Config/blocks/content', $block['index'], xap($block_new['content'], true));
 		} elseif ($block['type'] == 'raw_html') {
-			$block['content'] = $Text->set(
-				$Config->module('System')->db('texts'),
-				'System/Config/blocks/content',
-				$block['index'],
-				$block_new['content']
-			);
+			$block['content'] = $Text->set($db_id, 'System/Config/blocks/content', $block['index'], $block_new['content']);
 		}
 		if (!$index) {
 			$Config->components['blocks'][] = $block;
 			Permission::instance()->add('Block', $block['index']);
 		}
-		$Config->save();
+		if (!$Config->save()) {
+			error_code(500);
+		}
 	}
 }
