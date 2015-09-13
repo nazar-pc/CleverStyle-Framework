da8a1f24281f8a45e0a89336dc18555ca33a1cd9
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-09-13 18:03:49 +0200

    Reuse code to avoid duplication

--- components/modules/Shop/api/admin/attributes.get.php
+++ components/modules/Shop/api/admin/attributes.get.php
@@ -6,37 +6,4 @@
  * @copyright Copyright (c) 2014-2015, Nazar Mokrynskyi
  * @license   MIT License, see license.txt
  */
-namespace cs\modules\Shop;
-use
-	cs\ExitException,
-	cs\Page,
-	cs\Route;
-
-$Route      = Route::instance();
-$Page       = Page::instance();
-$Attributes = Attributes::instance();
-if (isset($_GET['ids'])) {
-	$attributes = $Attributes->get(explode(',', $_GET['ids']));
-	if (!$attributes) {
-		throw new ExitException(404);
-	} else {
-		$Page->json($attributes);
-	}
-} elseif (isset($Route->ids[0])) {
-	$attribute = $Attributes->get($Route->ids[0]);
-	if (!$attribute) {
-		throw new ExitException(404);
-	} else {
-		$Page->json($attribute);
-	}
-} elseif (isset($Route->path[2]) && $Route->path[2] == 'types') {
-	$Page->json(
-		$Attributes->get_type_to_name_array()
-	);
-} else {
-	$Page->json(
-		$Attributes->get(
-			$Attributes->get_all()
-		)
-	);
-}
+include '../attributes.get.php';
--- components/modules/Shop/api/attributes.get.php
+++ components/modules/Shop/api/attributes.get.php
@@ -10,7 +10,8 @@ namespace cs\modules\Shop;
 use
 	cs\ExitException,
 	cs\Page,
-	cs\Route;
+	cs\Route,
+	cs\User;
 
 $Route      = Route::instance();
 $Page       = Page::instance();
@@ -32,6 +33,12 @@ if (isset($_GET['ids'])) {
 } elseif (isset($Route->path[1]) && $Route->path[1] == 'types') {
 	$Page->json(
 		$Attributes->get_type_to_name_array()
+	);
+} elseif (User::instance()->admin()) { //Hack to re-use contents of this file from `api/admin/attributes.get.php`
+	$Page->json(
+		$Attributes->get(
+			$Attributes->get_all()
+		)
 	);
 } else {
 	throw new ExitException(400);
