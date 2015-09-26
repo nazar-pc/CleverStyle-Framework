4f917253796cb222f8490b91846050da9d82d44a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-09-26 20:31:08 +0200

    Small refactoring

--- core/classes/Page/Meta.php
+++ core/classes/Page/Meta.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2014-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package        CleverStyle CMS
+ * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright      Copyright (c) 2014-2015, Nazar Mokrynskyi
+ * @license        MIT License, see license.txt
  */
-namespace	cs\Page;
+namespace cs\Page;
 use
 	cs\Config,
 	cs\Language,
@@ -26,20 +26,20 @@ class Meta {
 	 * Is used as <head prefix="$head_prefix">
 	 * @var string
 	 */
-	public	$head_prefix	= '';
+	public $head_prefix = '';
 	/**
 	 * If false - &lt;head&gt; will not be added automatically, and should be in template if needed
 	 * @var bool
 	 */
-	public	$no_head		= false;
-	protected	$links		= '';
-	protected	$og_data	= [];
-	protected	$og_type	= '';
-	protected	$image_src	= false;
+	public    $no_head   = false;
+	protected $links     = '';
+	protected $og_data   = [];
+	protected $og_type   = '';
+	protected $image_src = false;
 	/**
 	 * Common wrapper to add all necessary meta tags with images
 	 *
-	 * @param string|string[]	$images
+	 * @param string|string[] $images
 	 *
 	 * @return Meta
 	 */
@@ -47,13 +47,15 @@ class Meta {
 		if (!$images) {
 			return $this;
 		}
-		$images	= (array)$images;
+		$images = (array)$images;
 		if (!$this->image_src) {
-			$this->image_src	= true;
-			$this->links		.= h::link([
-				'href'	=> $images[0],
-				'rel'	=> 'image_src'
-			]);
+			$this->image_src = true;
+			$this->links .= h::link(
+				[
+					'href' => $images[0],
+					'rel'  => 'image_src'
+				]
+			);
 		}
 		$this->__call('og', ['image', $images]);
 		return $this;
@@ -61,18 +63,20 @@ class Meta {
 	/**
 	 * Common wrapper for generation of various Open Graph protocol meta tags
 	 *
-	 * @param string	$type
-	 * @param mixed[]	$params
+	 * @param string  $type
+	 * @param mixed[] $params
 	 *
 	 * @return $this
 	 */
 	function __call ($type, $params) {
 		if (!$params) {
-			$this->og_type			= $type;
-			$this->og_data['type']	= h::meta([
-				'property'	=> "og:type",
-				'content'	=> $type
-			]);
+			$this->og_type         = $type;
+			$this->og_data['type'] = h::meta(
+				[
+					'property' => "og:type",
+					'content'  => $type
+				]
+			);
 			return $this;
 		}
 		if (!$params[0]) {
@@ -84,12 +88,14 @@ class Meta {
 			}
 		} elseif ($params[1] || $params[1] === 0) {
 			if (!isset($this->og_data[$params[0]])) {
-				$this->og_data[$params[0]]	= '';
+				$this->og_data[$params[0]] = '';
 			}
-			$this->og_data[$params[0]]	.= h::meta([
-				'property'	=> "$type:$params[0]",
-				'content'	=> $params[1]
-			]);
+			$this->og_data[$params[0]] .= h::meta(
+				[
+					'property' => "$type:$params[0]",
+					'content'  => $params[1]
+				]
+			);
 		}
 		return $this;
 	}
@@ -99,10 +105,10 @@ class Meta {
 	 * Usually called by system itself, there is no need to call it manually
 	 */
 	function render () {
-		$og			= &$this->og_data;
+		$og = &$this->og_data;
 		$this->fill_required_properties($og);
-		$prefix		= 'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#';
-		$type		= explode('.', $this->og_type, 2)[0];
+		$prefix = 'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#';
+		$type   = explode('.', $this->og_type, 2)[0];
 		switch ($type) {
 			case 'article':
 			case 'blog':
@@ -110,19 +116,19 @@ class Meta {
 			case 'profile':
 			case 'video':
 			case 'website':
-				$prefix	.= " $type: http://ogp.me/ns/$type#";
-			break;
+				$prefix .= " $type: http://ogp.me/ns/$type#";
+				break;
 		}
-		$Page		= Page::instance();
-		$Page->Head	=
+		$Page       = Page::instance();
+		$Page->Head =
 			$Page->Head.
 			implode('', $og).
 			$this->links;
 		if (!$this->no_head) {
-			$Page->Head	= h::head(
+			$Page->Head = h::head(
 				$Page->Head,
 				[
-					'prefix'	=> $prefix.$this->head_prefix
+					'prefix' => $prefix.$this->head_prefix
 				]
 			);
 		}
@@ -133,14 +139,14 @@ class Meta {
 	 * @param array $og
 	 */
 	protected function fill_required_properties (&$og) {
-		$Page		= Page::instance();
+		$Page = Page::instance();
 		if (!@$og['title']) {
 			$this->og('title', $Page->Title);
 		}
 		if (!@$og['description']) {
 			$this->og('description', $Page->Description);
 		}
-		$Config		= Config::instance();
+		$Config = Config::instance();
 		if (!@$og['url']) {
 			/** @noinspection NestedTernaryOperatorInspection */
 			$this->og(
@@ -156,19 +162,27 @@ class Meta {
 		if (!@$og['type']) {
 			$this->og('type', 'website');
 		}
-		if ($Config->core['multilingual']) {
-			$L	= Language::instance();
-			if (!@$og['locale']) {
-				$this->og('locale', $L->clocale);
-			}
-			if (
-				!@$og['locale:alternate'] &&
-				count($Config->core['active_languages']) > 1
-			) {
-				foreach ($Config->core['active_languages'] as $lang) {
-					if ($lang != $L->clanguage) {
-						$this->og('locale:alternate', $L->get('clocale', $lang));
-					}
+		$this->fill_required_properties_multilingual($og);
+	}
+	/*
+	 * @param array $og
+	 */
+	protected function fill_required_properties_multilingual (&$og) {
+		$Config = Config::instance();
+		if (!$Config->core['multilingual']) {
+			return;
+		}
+		$L = Language::instance();
+		if (!@$og['locale']) {
+			$this->og('locale', $L->clocale);
+		}
+		if (
+			!@$og['locale:alternate'] &&
+			count($Config->core['active_languages']) > 1
+		) {
+			foreach ($Config->core['active_languages'] as $lang) {
+				if ($lang != $L->clanguage) {
+					$this->og('locale:alternate', $L->get('clocale', $lang));
 				}
 			}
 		}
