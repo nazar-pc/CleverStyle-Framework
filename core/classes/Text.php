2416359147fcd7f5b2d074f17ef4875e9262ae5a
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-10-12 08:39:46 +0200

    Refactoring in `\cs\Text` class

--- core/classes/Text.php
+++ core/classes/Text.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package		CleverStyle CMS
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CleverStyle CMS
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2011-2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
 namespace cs;
 
@@ -15,26 +15,47 @@ class Text {
 	/**
 	 * Gets text on current language
 	 *
-	 * @param int			$database
-	 * @param string		$group
-	 * @param string		$label
-	 * @param int|null		$id					Getting may be done with group and label or with id
-	 * @param bool			$store_in_cache		If <b>true</b> - text will be stored in cache
+	 * @param int      $database
+	 * @param string   $group
+	 * @param string   $label
+	 * @param int|null $id             Getting may be done with group and label or with id
+	 * @param bool     $store_in_cache If `true` - text will be stored in cache
 	 *
 	 * @return false|string
 	 */
 	function get ($database, $group, $label, $id = null, $store_in_cache = false) {
-		$Cache		= Cache::instance();
-		$L			= Language::instance();
-		$id			= (int)$id;
-		$cache_key	= "texts/$database/".($id ?: md5($group).md5($label))."_$L->clang";
+		$Cache     = Cache::instance();
+		$L         = Language::instance();
+		$id        = (int)$id;
+		$cache_key = "texts/$database/".($id ?: md5($group).md5($label))."_$L->clang";
 		if ($store_in_cache && ($text = $Cache->$cache_key) !== false) {
 			return $text;
 		}
-		$db			= DB::instance();
-		$cdb		= $db->$database;
+		$db  = DB::instance();
+		$cdb = $db->$database;
 		if ($id) {
-			$text = $cdb->qf([
+			$text = $this->get_text_by_id($id, $cdb, $L);
+		} else {
+			$text = $this->get_text_by_group_and_label($group, $label, $cdb, $L);
+		}
+		if (!$text) {
+			return false;
+		}
+		if ($store_in_cache) {
+			$Cache->$cache_key = $text['text'];
+		}
+		return $text['text'];
+	}
+	/**
+	 * @param int          $id
+	 * @param DB\_Abstract $cdb
+	 * @param Language     $L
+	 *
+	 * @return false|string[]
+	 */
+	protected function get_text_by_id ($id, $cdb, $L) {
+		$text = $cdb->qf(
+			[
 				"SELECT
 					`d`.`id`,
 					`d`.`lang`,
@@ -47,9 +68,11 @@ class Text {
 					`d`.`lang`	= '%s'
 				LIMIT 1",
 				$L->clang
-			]);
-			if (!$text) {
-				$text = $cdb->qf([
+			]
+		);
+		if (!$text) {
+			$text = $cdb->qf(
+				[
 					"SELECT
 						`d`.`id`,
 						`d`.`lang`,
@@ -60,10 +83,22 @@ class Text {
 					WHERE `t`.`id` = $id
 					LIMIT 1",
 					$L->clang
-				]);
-			}
-		} else {
-			$text = $cdb->qf([
+				]
+			);
+		}
+		return $text;
+	}
+	/**
+	 * @param string       $group
+	 * @param string       $label
+	 * @param DB\_Abstract $cdb
+	 * @param Language     $L
+	 *
+	 * @return false|string[]
+	 */
+	protected function get_text_by_group_and_label ($group, $label, $cdb, $L) {
+		$text = $cdb->qf(
+			[
 				"SELECT
 					`t`.`id`,
 					`d`.`lang`,
@@ -79,9 +114,11 @@ class Text {
 				$group,
 				$label,
 				$L->clang
-			]);
-			if (!$text) {
-				$text = $cdb->qf([
+			]
+		);
+		if (!$text) {
+			$text = $cdb->qf(
+				[
 					"SELECT
 						`t`.`id`,
 						`d`.`lang`,
@@ -96,30 +133,25 @@ class Text {
 					$group,
 					$label,
 					$L->clang
-				]);
-			}
+				]
+			);
 		}
-		if (!$text) {
-			return false;
-		}
-		if ($store_in_cache) {
-			$Cache->$cache_key	= $text['text'];
-		}
-		return $text['text'];
+		return $text;
 	}
 	/**
 	 * Search for text regardless language
 	 *
-	 * @param int			$database
-	 * @param string		$group
-	 * @param string		$label
-	 * @param string		$text
+	 * @param int    $database
+	 * @param string $group
+	 * @param string $label
+	 * @param string $text
 	 *
-	 * @return array[]|false				Array of items ['id' => <i>id</i>, 'lang' => <i>lang</i>] on success, <i>false</i> otherwise
+	 * @return array[]|false Array of items `['id' => id, 'lang' => lang]` on success, `false` otherwise
 	 */
 	function search ($database, $group, $label, $text) {
-		return DB::instance()->$database->qfa([
-			"SELECT
+		return DB::instance()->$database->qfa(
+			[
+				"SELECT
 				`t`.`id`,
 				`d`.`lang`
 			FROM `[prefix]texts` AS `t`
@@ -129,42 +161,47 @@ class Text {
 				`t`.`group`		= '%s' AND
 				`t`.`label`		= '%s' AND
 				`d`.`text_md5`	= '%s'",
-			$group,
-			$label,
-			md5($text)
-		]);
+				$group,
+				$label,
+				md5($text)
+			]
+		);
 	}
 	/**
 	 * Sets text on current language
 	 *
-	 * @param int			$database
-	 * @param string		$group
-	 * @param string		$label
-	 * @param string		$text
+	 * @param int    $database
+	 * @param string $group
+	 * @param string $label
+	 * @param string $text
 	 *
-	 * @return false|string				If multilingual support enabled or was enabled and then disabled but translations remains - returns {¶<i>id</i>}<br>
-	 * 									otherwise returns original text
+	 * @return false|string If multilingual support enabled or was enabled and then disabled but translations remains - returns {¶<i>id</i>}, otherwise returns
+	 *                      original text
 	 */
 	function set ($database, $group, $label, $text) {
-		$Cache	= Cache::instance();
-		$Config	= Config::instance();
-		$L		= Language::instance();
-		$cdb	= DB::instance()->$database();
+		$Cache  = Cache::instance();
+		$Config = Config::instance();
+		$L      = Language::instance();
+		$cdb    = DB::instance()->$database();
 		/**
 		 * @var \cs\DB\_Abstract $cdb
 		 */
-		$text	= str_replace('{¶', '{&para;', $text);
-		$id		= $cdb->qfs([
-			"SELECT `id`
-			FROM `[prefix]texts`
-			WHERE
-				`label`	= '%s' AND
-				`group`	= '%s'
-			LIMIT 1",
-			$label,
-			$group
-		]);
+		$text = str_replace('{¶', '{&para;', $text);
+		// Find existing text id
+		$id = $cdb->qfs(
+			[
+				"SELECT `id`
+				FROM `[prefix]texts`
+				WHERE
+					`label`	= '%s' AND
+					`group`	= '%s'
+				LIMIT 1",
+				$label,
+				$group
+			]
+		);
 		if (!$id) {
+			// If not found - either return text directly or add new text entry and obtain id
 			if (!$Config->core['multilingual']) {
 				return $text;
 			} else {
@@ -180,7 +217,8 @@ class Text {
 					$label,
 					$group
 				);
-				if (!($id = $cdb->id())) {
+				$id = $cdb->id();
+				if (!$id) {
 					return $text;
 				}
 			}
@@ -189,16 +227,31 @@ class Text {
 			$Cache->{"texts/$database/{$id}_$L->clang"},
 			$Cache->{"texts/$database/".md5($group).md5($label)."_$L->clang"}
 		);
-		if ($cdb->qfs([
-			"SELECT `id`
-			FROM `[prefix]texts_data`
-			WHERE
-				`id`	= '%s' AND
-				`lang`	= '%s'
-			LIMIT 1",
-			$id,
-			$L->clang
-		])) {
+		return $this->set_text($id, $text, $cdb, $Config, $L);
+	}
+	/**
+	 * @param int          $id
+	 * @param string       $text
+	 * @param DB\_Abstract $cdb
+	 * @param Config       $Config
+	 * @param Language     $L
+	 *
+	 * @return mixed
+	 */
+	protected function set_text ($id, $text, $cdb, $Config, $L) {
+		$exists_for_current_language = $cdb->qfs(
+			[
+				"SELECT `id`
+				FROM `[prefix]texts_data`
+				WHERE
+					`id`	= '%s' AND
+					`lang`	= '%s'
+				LIMIT 1",
+				$id,
+				$L->clang
+			]
+		);
+		if ($exists_for_current_language) {
 			if ($cdb->q(
 				"UPDATE `[prefix]texts_data`
 				SET `text` = '%s'
@@ -209,13 +262,12 @@ class Text {
 				$text,
 				$id,
 				$L->clang
-			)) {
+			)
+			) {
 				return "{¶$id}";
-			} else {
-				return $text;
 			}
 		} elseif ($Config->core['multilingual']) {
-			if (!$cdb->q(
+			if ($cdb->q(
 				"INSERT INTO `[prefix]texts_data`
 					(
 						`id`,
@@ -232,46 +284,39 @@ class Text {
 				"{¶$id}",
 				$L->clang,
 				$text
-			)) {
-				$cdb->q(
-					"DELETE FROM `[prefix]texts`
-					WHERE `id` = $id"
-				);
-				return $text;
-			}
-			if ($id) {
+			)
+			) {
 				return "{¶$id}";
-			} else {
-				return $text;
 			}
-		} else {
-			return $text;
 		}
+		return $text;
 	}
 	/**
 	 * Deletes text on all languages
 	 *
-	 * @param int        $database
-	 * @param string     $group
-	 * @param string     $label
+	 * @param int    $database
+	 * @param string $group
+	 * @param string $label
 	 *
 	 * @return bool
 	 */
 	function del ($database, $group, $label) {
-		$Cache	= Cache::instance();
-		$db		= DB::instance();
-		$id		= $db->$database()->qfs([
-			"SELECT `id`
-			FROM `[prefix]texts`
-			WHERE
-				`group`	= '%s' AND
-				`label`	= '%s'
-			LIMIT 1",
-			$group,
-			$label
-		]);
+		$Cache = Cache::instance();
+		$db    = DB::instance();
+		$id    = $db->$database()->qfs(
+			[
+				"SELECT `id`
+				FROM `[prefix]texts`
+				WHERE
+					`group`	= '%s' AND
+					`label`	= '%s'
+				LIMIT 1",
+				$group,
+				$label
+			]
+		);
 		if ($id) {
-			$L		= Language::instance();
+			$L = Language::instance();
 			unset(
 				$Cache->{"texts/$database/{$id}_$L->clang"},
 				$Cache->{"texts/$database/".md5($group).md5($label)."_$L->clang"}
@@ -285,31 +330,27 @@ class Text {
 				],
 				$id
 			);
-		} else {
-			return true;
 		}
+		return true;
 	}
 	/**
 	 * Process text, and replace {¶([0-9]+)} on real text, is used before showing multilingual information
 	 *
-	 * @param int					$database
-	 * @param string|string[]		$data
-	 * @param bool					$store_in_cache		If <b>true</b> - text will be stored in cache
+	 * @param int             $database
+	 * @param string|string[] $data
+	 * @param bool            $store_in_cache If <b>true</b> - text will be stored in cache
 	 *
 	 * @return string|string[]
 	 */
 	function process ($database, $data, $store_in_cache = false) {
-		if (empty($data)) {
-			return '';
-		}
 		if (is_array($data)) {
 			foreach ($data as &$d) {
-				$d	= $this->process($database, $d);
+				$d = $this->process($database, $d);
 			}
 			return $data;
 		}
 		return preg_replace_callback(
-			'/^\{¶([0-9]+)\}$/',
+			'/^\{¶(\d+)\}$/',
 			function ($input) use ($database, $store_in_cache) {
 				return $this->get($database, null, null, $input[1], $store_in_cache);
 			},
