9ad3e697cfd8eda513f5822d9bf2d3e5c7261d66
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-10-14 07:12:45 +0200

    Small refactoring to eliminate a lot of `isset()` statements

--- core/classes/_SERVER.php
+++ core/classes/_SERVER.php
@@ -13,20 +13,20 @@ use
 /**
  * Generic wrapper for `$_SERVER` to make its usage easier and more secure
  *
- * @property string $language        Language accepted by client, `''` by default
- * @property string $version         Version accepted by client, will match `/^[0-9\.]+$/`, useful for API, `1` by default
- * @property string $content_type    Content type, `''` by default
- * @property bool   $dnt             Do not track
- * @property string $host            The best guessed host
- * @property string $ip              The best guessed IP of client (based on all known headers), `$this->remote_addr` by default
- * @property string $protocol        Protocol `http` or `https`
- * @property string $query_string    Query string
- * @property string $referer         HTTP referer, `''` by default
- * @property string $remote_addr     Where request came from, not necessary real IP of client
- * @property string $request_uri     Request uri
- * @property string $request_method  Request method
- * @property bool   $secure          Is requested with HTTPS
- * @property string $user_agent      User agent
+ * @property string $language       Language accepted by client, `''` by default
+ * @property string $version        Version accepted by client, will match `/^[0-9\.]+$/`, useful for API, `1` by default
+ * @property string $content_type   Content type, `''` by default
+ * @property bool   $dnt            Do not track
+ * @property string $host           The best guessed host
+ * @property string $ip             The best guessed IP of client (based on all known headers), `$this->remote_addr` by default
+ * @property string $protocol       Protocol `http` or `https`
+ * @property string $query_string   Query string
+ * @property string $referer        HTTP referer, `''` by default
+ * @property string $remote_addr    Where request came from, not necessary real IP of client
+ * @property string $request_uri    Request uri
+ * @property string $request_method Request method
+ * @property bool   $secure         Is requested with HTTPS
+ * @property string $user_agent     User agent
  */
 class _SERVER implements ArrayAccess, Iterator {
 	public    $language       = '';
@@ -50,21 +50,35 @@ class _SERVER implements ArrayAccess, Iterator {
 		$this->update($SERVER);
 	}
 	protected function update ($SERVER) {
-		$this->language       = isset($SERVER['HTTP_ACCEPT_LANGUAGE']) ? $SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
-		$this->version        =
-			isset($SERVER['HTTP_ACCEPT_VERSION']) && preg_match('/^[0-9\.]+$/', $SERVER['HTTP_ACCEPT_VERSION']) ? $SERVER['HTTP_ACCEPT_VERSION'] : 1;
-		$this->content_type   = isset($SERVER['CONTENT_TYPE']) ? $SERVER['CONTENT_TYPE'] : '';
-		$this->dnt            = isset($SERVER['HTTP_DNT']) && $SERVER['HTTP_DNT'] == 1;
+		/**
+		 * Add some defaults to avoid isset() hell below
+		 */
+		$SERVER += [
+			'HTTP_ACCEPT_LANGUAGE' => '',
+			'HTTP_ACCEPT_VERSION'  => '',
+			'CONTENT_TYPE'         => '',
+			'HTTP_DNT'             => 0,
+			'QUERY_STRING'         => '',
+			'HTTP_REFERER'         => '',
+			'REMOTE_ADDR'          => '127.0.0.1',
+			'REQUEST_URI'          => '',
+			'REQUEST_METHOD'       => '',
+			'HTTP_USER_AGENT'      => ''
+		];
+		$this->language       = $SERVER['HTTP_ACCEPT_LANGUAGE'];
+		$this->version        = preg_match('/^[0-9\.]+$/', $SERVER['HTTP_ACCEPT_VERSION']) ? $SERVER['HTTP_ACCEPT_VERSION'] : 1;
+		$this->content_type   = $SERVER['CONTENT_TYPE'];
+		$this->dnt            = $SERVER['HTTP_DNT'] == 1;
 		$this->secure         = $this->secure($SERVER);
 		$this->protocol       = $this->secure ? 'https' : 'http';
 		$this->host           = $this->host($SERVER);
 		$this->ip             = $this->ip($_SERVER);
-		$this->query_string   = isset($SERVER['QUERY_STRING']) ? $SERVER['QUERY_STRING'] : '';
-		$this->referer        = isset($SERVER['HTTP_REFERER']) && filter_var($SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL) ? $SERVER['HTTP_REFERER'] : '';
-		$this->remote_addr    = isset($SERVER['REMOTE_ADDR']) ? $SERVER['REMOTE_ADDR'] : '127.0.0.1';
-		$this->request_uri    = isset($SERVER['REQUEST_URI']) ? $SERVER['REQUEST_URI'] : '';
-		$this->request_method = isset($SERVER['REQUEST_METHOD']) ? $SERVER['REQUEST_METHOD'] : '';
-		$this->user_agent     = isset($SERVER['HTTP_USER_AGENT']) ? $SERVER['HTTP_USER_AGENT'] : '';
+		$this->query_string   = $SERVER['QUERY_STRING'];
+		$this->referer        = filter_var($SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL) ? $SERVER['HTTP_REFERER'] : '';
+		$this->remote_addr    = $SERVER['REMOTE_ADDR'];
+		$this->request_uri    = $SERVER['REQUEST_URI'];
+		$this->request_method = $SERVER['REQUEST_METHOD'];
+		$this->user_agent     = $SERVER['HTTP_USER_AGENT'];
 	}
 	/**
 	 * The best guessed IP of client (based on all known headers), `$this->remote_addr` by default
