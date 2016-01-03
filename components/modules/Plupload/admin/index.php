1be1abccc7b92dfaeeffaf471b76dfc3d7a494e5
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-04 00:51:07 +0100

    Plupload module doesn't rely on `cs\Index`'s form anymore

--- components/modules/Plupload/admin/index.php
+++ components/modules/Plupload/admin/index.php
@@ -8,25 +8,38 @@
  * @license   GNU GPL v2, see license.txt
  */
 namespace cs;
-use            h;
-include __DIR__.'/save.php';
-$Config                       = Config::instance();
-$L                            = Language::instance();
-$Index                        = Index::instance();
-$Index->form_attributes['is'] = 'cs-form';
-$Index->content(
-	h::label("$L->upload_size_limit (b, kb, mb, gb)").
-	h::{'input[is=cs-input-text][compact][name=max_file_size]'}(
-		[
-			'value' => $Config->module('Plupload')->max_file_size
-		]
-	).
-	h::{'label info'}('plupload_confirmation_time').
-	h::{'input[is=cs-input-text][compact][name=confirmation_time]'}(
-		[
-			'value' => $Config->module('Plupload')->confirmation_time
-		]
-	).
-	$L->seconds.
-	h::br()
+use
+	h;
+
+if (isset($_POST['max_file_size'], $_POST['confirmation_time'])) {
+	$module_data                    = Config::instance()->module('Plupload');
+	$module_data->max_file_size     = xap($_POST['max_file_size']);
+	$module_data->confirmation_time = (int)$_POST['confirmation_time'];
+	Index::instance()->save(true);
+}
+
+$Config = Config::instance();
+$L      = Language::instance();
+Page::instance()->content(
+	h::{'form[is=cs-form]'}(
+		h::label("$L->upload_size_limit (b, kb, mb, gb)").
+		h::{'input[is=cs-input-text][compact][name=max_file_size]'}(
+			[
+				'value' => $Config->module('Plupload')->max_file_size
+			]
+		).
+		h::{'label info'}('plupload_confirmation_time').
+		h::{'input[is=cs-input-text][compact][name=confirmation_time]'}(
+			[
+				'value' => $Config->module('Plupload')->confirmation_time
+			]
+		).
+		$L->seconds.
+		h::{'p button[is=cs-button][type=submit]'}(
+			$L->save,
+			[
+				'tooltip' => $L->save_info
+			]
+		)
+	)
 );
