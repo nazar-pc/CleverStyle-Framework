dd6bebd8336589132ce6f2a04d9a9c66b58fd463
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-03 23:26:01 +0100

    Content module doesn't rely on `cs\Index`'s form anymore

--- components/modules/Content/admin/index.php
+++ components/modules/Content/admin/index.php
@@ -6,16 +6,15 @@
  * @copyright Copyright (c) 2014-2016, Nazar Mokrynskyi
  * @license   MIT License, see license.txt
  */
-
 namespace cs\modules\Content;
 
 use
 	h,
 	cs\Config,
 	cs\Index,
-	cs\Language\Prefix;
+	cs\Language\Prefix,
+	cs\Page;
 
-$Index       = Index::instance();
 $L           = new Prefix('content_');
 $Content     = Content::instance();
 $all_items   = $Content->get($Content->get_all());
@@ -23,53 +22,59 @@ $module_data = Config::instance()->module('Content');
 
 if (isset($_POST['simple_insert'])) {
 	$module_data->simple_insert = $_POST['simple_insert'];
-	$Index->save(true);
+	Index::instance()->save(true);
 }
-$Index->form_attributes['is'] = 'cs-form';
-$Index->content(
-	h::{'table.cs-table[center][list]'}(
-		h::{'tr th'}(
+Page::instance()->content(
+	h::{'form[is=cs-form]'}(
+		h::{'table.cs-table[center][list]'}(
+			h::{'tr th'}(
+				[
+					$L->key,
+					$L->title,
+					$L->type,
+					$L->action
+				]
+			).
+			h::{'tr| td'}(
+				array_map(
+					function ($item) use ($L) {
+						return [
+							$item['key'],
+							$item['title'],
+							$item['type'],
+							h::{'button.cs-content-edit[is=cs-button]'}(
+								$L->edit,
+								[
+									'data-key' => $item['key']
+								]
+							).
+							h::{'button.cs-content-delete[is=cs-button]'}(
+								$L->delete,
+								[
+									'data-key' => $item['key']
+								]
+							)
+						];
+					},
+					$all_items
+				) ?: false
+			)
+		).
+		h::{'p button.cs-content-add[is=cs-button]'}($L->add).
+		h::{'label info'}('content_simple_insert').
+		h::{'div radio'}(
 			[
-				$L->key,
-				$L->title,
-				$L->type,
-				$L->action
+				'name'    => 'simple_insert',
+				'value'   => [0, 1],
+				'in'      => [$L->no, $L->yes],
+				'checked' => $module_data->simple_insert
 			]
 		).
-		h::{'tr| td'}(
-			array_map(
-				function ($item) use ($L) {
-					return [
-						$item['key'],
-						$item['title'],
-						$item['type'],
-						h::{'button.cs-content-edit[is=cs-button]'}(
-							$L->edit,
-							[
-								'data-key' => $item['key']
-							]
-						).
-						h::{'button.cs-content-delete[is=cs-button]'}(
-							$L->delete,
-							[
-								'data-key' => $item['key']
-							]
-						)
-					];
-				},
-				$all_items
-			) ?: false
+		h::{'p button[is=cs-button][type=submit]'}(
+			$L->save,
+			[
+				'tooltip' => $L->save_info
+			]
 		)
-	).
-	h::{'p button.cs-content-add[is=cs-button]'}($L->add).
-	h::{'label info'}('content_simple_insert').
-	h::{'div radio'}(
-		[
-			'name'    => 'simple_insert',
-			'value'   => [0, 1],
-			'in'      => [$L->no, $L->yes],
-			'checked' => $module_data->simple_insert
-		]
-	).
-	h::br()
+	)
 );
