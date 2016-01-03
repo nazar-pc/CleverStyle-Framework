54d925f1b694f61576af61e427d23fc45c4c1de8
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-03 23:37:31 +0100

    Disqus module doesn't rely on `cs\Index`'s form anymore

--- components/modules/Disqus/admin/index.php
+++ components/modules/Disqus/admin/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package   Cron
+ * @package   Disqus
  * @category  modules
  * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @copyright Copyright (c) 2011-2016, Nazar Mokrynskyi
@@ -9,15 +9,27 @@
 namespace cs;
 use
 	h;
-include __DIR__.'/save.php';
-$Index                        = Index::instance();
-$Index->form_attributes['is'] = 'cs-form';
-$Index->content(
-	h::label('Shortname').
-	h::{'input[is=cs-input-text][name=shortname]'}(
-		[
-			'value' => Config::instance()->module('Disqus')->shortname ?: ''
-		]
-	).
-	h::br()
+
+$module_data = Config::instance()->module('Disqus');
+if (isset($_POST['shortname'])) {
+	$module_data->shortname = $_POST['shortname'];
+	Index::instance()->save(true);
+}
+
+$L = Language::instance();
+Page::instance()->content(
+	h::{'form[is=cs-form]'}(
+		h::label('Shortname').
+		h::{'input[is=cs-input-text][name=shortname]'}(
+			[
+				'value' => $module_data->shortname ?: ''
+			]
+		).
+		h::{'p button[is=cs-button][type=submit]'}(
+			$L->save,
+			[
+				'tooltip' => $L->save_info
+			]
+		)
+	)
 );
