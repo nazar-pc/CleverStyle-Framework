f3532bf8a25b56240ce767649e542be5cbf049e0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-04 01:22:47 +0100

    Uploader module doesn't rely on `cs\Index`'s form anymore

--- components/modules/Uploader/admin/index.php
+++ components/modules/Uploader/admin/index.php
@@ -7,25 +7,37 @@
  * @license   MIT License, see license.txt
  */
 namespace cs;
-use            h;
-include __DIR__.'/save.php';
-$Config                       = Config::instance();
-$L                            = Language::instance();
-$Index                        = Index::instance();
-$Index->form_attributes['is'] = 'cs-form';
-$Index->content(
-	h::label("$L->upload_size_limit (b, kb, mb, gb)").
-	h::{'input[is=cs-input-text][compact][name=max_file_size]'}(
-		[
-			'value' => $Config->module('Uploader')->max_file_size
-		]
-	).
-	h::{'label info'}('uploader_confirmation_time').
-	h::{'input[is=cs-input-text][compact][name=confirmation_time]'}(
-		[
-			'value' => $Config->module('Uploader')->confirmation_time
-		]
-	).
-	$L->seconds.
-	h::br()
+use
+	h;
+
+$module_data = Config::instance()->module('Uploader');
+if (isset($_POST['max_file_size'], $_POST['confirmation_time'])) {
+	$module_data->max_file_size     = xap($_POST['max_file_size']);
+	$module_data->confirmation_time = (int)$_POST['confirmation_time'];
+	Index::instance()->save(true);
+}
+
+$L = Language::instance();
+Page::instance()->content(
+	h::{'form[is=cs-form]'}(
+		h::label("$L->upload_size_limit (b, kb, mb, gb)").
+		h::{'input[is=cs-input-text][compact][name=max_file_size]'}(
+			[
+				'value' => $module_data->max_file_size
+			]
+		).
+		h::{'label info'}('uploader_confirmation_time').
+		h::{'input[is=cs-input-text][compact][name=confirmation_time]'}(
+			[
+				'value' => $module_data->confirmation_time
+			]
+		).
+		$L->seconds.
+		h::{'p button[is=cs-button][type=submit]'}(
+			$L->save,
+			[
+				'tooltip' => $L->save_info
+			]
+		)
+	)
 );
