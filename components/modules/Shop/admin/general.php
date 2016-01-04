2b36329aa0a9a51e2b7e6ef5907ccb8d079cd2ef
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-04 01:06:20 +0100

    Shop module doesn't rely on `cs\Index`'s form anymore

--- components/modules/Shop/admin/general.php
+++ components/modules/Shop/admin/general.php
@@ -14,128 +14,160 @@ use
 	cs\Language\Prefix,
 	cs\Page;
 
-include __DIR__.'/save.php';
+if (isset(
+	$_POST['price_formatting'],
+	$_POST['items_per_page'],
+	$_POST['items_per_page_admin'],
+	$_POST['allow_guests_orders'],
+	$_POST['automatically_reduce_in_stock_value'],
+	$_POST['default_order_status'],
+	$_POST['default_paid_order_status']
+)) {
+	$currencies = file_get_json(__DIR__.'/../currencies_codes.json');
+	$currency   = $_POST['currency'];
+	$currency   = isset($currencies['regular'][$currency]) || isset($currencies['crypto'][$currency]) ? $currency : 'USD';
+	Config::instance()->module('Shop')->set(
+		[
+			'currency'                            => $currency,
+			'price_formatting'                    => xap($_POST['price_formatting']),
+			'items_per_page'                      => (int)$_POST['items_per_page'],
+			'items_per_page_admin'                => (int)$_POST['items_per_page_admin'],
+			'allow_guests_orders'                 => (int)$_POST['allow_guests_orders'],
+			'automatically_reduce_in_stock_value' => (int)$_POST['automatically_reduce_in_stock_value'],
+			'default_order_status'                => (int)$_POST['default_order_status'],
+			'default_paid_order_status'           => (int)$_POST['default_paid_order_status']
+		]
+	);
+	Index::instance()->save(true);
+}
 
-$L           = new Prefix('shop_');
-$Config      = Config::instance();
-$Index       = Index::instance();
-$Page        = Page::instance();
-$module_data = $Config->module('Shop');
-$Page->title($L->general);
+$L              = new Prefix('shop_');
+$Config         = Config::instance();
+$module_data    = $Config->module('Shop');
 $Order_statuses = Order_statuses::instance();
 $order_statuses = $Order_statuses->get($Order_statuses->get_all());
 $currencies     = file_get_json(__DIR__.'/../currencies_codes.json');
-$Index->content(
-	h::{'table.cs-table[right-left] tr| td'}(
-		[
-			$L->currency,
-			h::{'select[is=cs-select][name=currency]'}(
-				h::optgroup(
-					[
-						'in'    => array_values(
-							array_map(
-								function ($key, $value) {
-									return "$key: $value";
-								},
-								array_keys($currencies['regular']),
-								array_values($currencies['regular'])
-							)
-						),
-						'value' => array_keys($currencies['regular'])
-					],
-					[
-						'selected' => $module_data->currency,
-						'label'    => $L->regular_currencies
-					]
-				).
-				h::optgroup(
-					[
-						'in'    => array_values(
-							array_map(
-								function ($key, $value) {
-									return "$key: $value";
-								},
-								array_keys($currencies['crypto']),
-								array_values($currencies['crypto'])
-							)
-						),
-						'value' => array_keys($currencies['crypto'])
-					],
-					[
-						'selected' => $module_data->currency,
-						'label'    => $L->cryptocurrencies
-					]
-				)
-			)
-		],
-		[
-			$L->price_formatting,
-			h::{'input[is=cs-input-text][name=price_formatting][required]'}(
+Page::instance()
+	->title($L->general)
+	->content(
+		h::form(
+			h::{'table.cs-table[right-left] tr| td'}(
 				[
-					'value'       => $module_data->price_formatting,
-					'placeholder' => '$%s USD'
-				]
-			)
-		],
-		[
-			$L->items_per_page,
-			h::{'input[is=cs-input-text][name=items_per_page][required][type=number][min=1]'}(
-				[
-					'value' => $module_data->items_per_page
-				]
-			)
-		],
-		[
-			$L->items_per_page_admin,
-			h::{'input[is=cs-input-text][name=items_per_page_admin][required][type=number][min=1]'}(
-				[
-					'value' => $module_data->items_per_page_admin
-				]
-			)
-		],
-		[
-			h::info('shop_allow_guests_orders'),
-			h::{'radio[name=allow_guests_orders]'}(
-				[
-					'checked' => $module_data->allow_guests_orders,
-					'value'   => [0, 1],
-					'in'      => [$L->no, $L->yes]
-				]
-			)
-		],
-		[
-			h::info('shop_automatically_reduce_in_stock_value'),
-			h::{'radio[name=automatically_reduce_in_stock_value]'}(
-				[
-					'checked' => $module_data->automatically_reduce_in_stock_value,
-					'value'   => [0, 1],
-					'in'      => [$L->no, $L->yes]
-				]
-			)
-		],
-		[
-			h::info('shop_default_order_status'),
-			h::{'select[is=cs-select][name=default_order_status]'}(
-				[
-					'in'    => array_column($order_statuses, 'title'),
-					'value' => array_column($order_statuses, 'id')
+					$L->currency,
+					h::{'select[is=cs-select][name=currency]'}(
+						h::optgroup(
+							[
+								'in'    => array_values(
+									array_map(
+										function ($key, $value) {
+											return "$key: $value";
+										},
+										array_keys($currencies['regular']),
+										array_values($currencies['regular'])
+									)
+								),
+								'value' => array_keys($currencies['regular'])
+							],
+							[
+								'selected' => $module_data->currency,
+								'label'    => $L->regular_currencies
+							]
+						).
+						h::optgroup(
+							[
+								'in'    => array_values(
+									array_map(
+										function ($key, $value) {
+											return "$key: $value";
+										},
+										array_keys($currencies['crypto']),
+										array_values($currencies['crypto'])
+									)
+								),
+								'value' => array_keys($currencies['crypto'])
+							],
+							[
+								'selected' => $module_data->currency,
+								'label'    => $L->cryptocurrencies
+							]
+						)
+					)
 				],
 				[
-					'selected' => $module_data->default_order_status
-				]
-			)
-		],
-		[
-			h::info('shop_default_paid_order_status'),
-			h::{'select[is=cs-select][name=default_paid_order_status]'}(
-				[
-					'in'    => array_column($order_statuses, 'title'),
-					'value' => array_column($order_statuses, 'id')
+					$L->price_formatting,
+					h::{'input[is=cs-input-text][name=price_formatting][required]'}(
+						[
+							'value'       => $module_data->price_formatting,
+							'placeholder' => '$%s USD'
+						]
+					)
 				],
 				[
-					'selected' => $module_data->default_paid_order_status
+					$L->items_per_page,
+					h::{'input[is=cs-input-text][name=items_per_page][required][type=number][min=1]'}(
+						[
+							'value' => $module_data->items_per_page
+						]
+					)
+				],
+				[
+					$L->items_per_page_admin,
+					h::{'input[is=cs-input-text][name=items_per_page_admin][required][type=number][min=1]'}(
+						[
+							'value' => $module_data->items_per_page_admin
+						]
+					)
+				],
+				[
+					h::info('shop_allow_guests_orders'),
+					h::{'radio[name=allow_guests_orders]'}(
+						[
+							'checked' => $module_data->allow_guests_orders,
+							'value'   => [0, 1],
+							'in'      => [$L->no, $L->yes]
+						]
+					)
+				],
+				[
+					h::info('shop_automatically_reduce_in_stock_value'),
+					h::{'radio[name=automatically_reduce_in_stock_value]'}(
+						[
+							'checked' => $module_data->automatically_reduce_in_stock_value,
+							'value'   => [0, 1],
+							'in'      => [$L->no, $L->yes]
+						]
+					)
+				],
+				[
+					h::info('shop_default_order_status'),
+					h::{'select[is=cs-select][name=default_order_status]'}(
+						[
+							'in'    => array_column($order_statuses, 'title'),
+							'value' => array_column($order_statuses, 'id')
+						],
+						[
+							'selected' => $module_data->default_order_status
+						]
+					)
+				],
+				[
+					h::info('shop_default_paid_order_status'),
+					h::{'select[is=cs-select][name=default_paid_order_status]'}(
+						[
+							'in'    => array_column($order_statuses, 'title'),
+							'value' => array_column($order_statuses, 'id')
+						],
+						[
+							'selected' => $module_data->default_paid_order_status
+						]
+					)
+				]
+			).
+			h::{'p button[is=cs-button][type=submit]'}(
+				$L->save,
+				[
+					'tooltip' => $L->save_info
 				]
 			)
-		]
-	)
-);
+		)
+	);
--- components/modules/Shop/admin/order_statuses.php
+++ components/modules/Shop/admin/order_statuses.php
@@ -9,14 +9,10 @@
 namespace cs\modules\Shop;
 use
 	h,
-	cs\Index,
 	cs\Language\Prefix,
 	cs\Page;
 
-$Index = Index::instance();
-$L     = new Prefix('shop_');
-$Page  = Page::instance();
-$Page->title($L->order_statuses);
+$L                  = new Prefix('shop_');
 $Order_statuses     = Order_statuses::instance();
 $all_order_statuses = $Order_statuses->get($Order_statuses->get_all());
 $order_status_types = $Order_statuses->get_type_to_name_array();
@@ -26,47 +22,50 @@ usort(
 		return $order_status1['title'] > $order_status2['title'] ? 1 : -1;
 	}
 );
-$Index->buttons = false;
-$Index->content(
-	h::h2($L->order_statuses).
-	h::{'table.cs-table[list]'}(
-		h::{'tr th'}(
-			'id',
-			"$L->title ".h::icon('caret-down'),
-			$L->order_status_type,
-			$L->send_update_status_email,
-			$L->action
-		).
-		h::{'tr| td'}(
-			array_map(
-				function ($order_status) use ($L, $order_status_types) {
-					return [
-						[
-							$order_status['id'],
-							$order_status['title'],
-							$order_status_types[$order_status['type']],
-							h::icon($order_status['send_update_status_email'] ? 'check' : 'minus'),
-							h::{'button.cs-shop-order-status-edit[is=cs-button]'}(
-								$L->edit,
+Page::instance()
+	->title($L->order_statuses)
+	->content(
+		h::{'form[is=cs-form]'}(
+			h::h2($L->order_statuses).
+			h::{'table.cs-table[list]'}(
+				h::{'tr th'}(
+					'id',
+					"$L->title ".h::icon('caret-down'),
+					$L->order_status_type,
+					$L->send_update_status_email,
+					$L->action
+				).
+				h::{'tr| td'}(
+					array_map(
+						function ($order_status) use ($L, $order_status_types) {
+							return [
 								[
-									'data-id' => $order_status['id']
-								]
-							).
-							h::{'button.cs-shop-order-status-delete[is=cs-button]'}(
-								$L->delete,
+									$order_status['id'],
+									$order_status['title'],
+									$order_status_types[$order_status['type']],
+									h::icon($order_status['send_update_status_email'] ? 'check' : 'minus'),
+									h::{'button.cs-shop-order-status-edit[is=cs-button]'}(
+										$L->edit,
+										[
+											'data-id' => $order_status['id']
+										]
+									).
+									h::{'button.cs-shop-order-status-delete[is=cs-button]'}(
+										$L->delete,
+										[
+											'data-id' => $order_status['id']
+										]
+									)
+								],
 								[
-									'data-id' => $order_status['id']
+									'style' => $order_status['color'] ? "background: $order_status[color]" : ''
 								]
-							)
-						],
-						[
-							'style' => $order_status['color'] ? "background: $order_status[color]" : ''
-						]
-					];
-				},
-				$all_order_statuses
-			) ?: false
+							];
+						},
+						$all_order_statuses
+					) ?: false
+				)
+			).
+			h::{'p button.cs-shop-order-status-add[is=cs-button]'}($L->add)
 		)
-	).
-	h::{'button.cs-shop-order-status-add[is=cs-button]'}($L->add)
-);
+	);
