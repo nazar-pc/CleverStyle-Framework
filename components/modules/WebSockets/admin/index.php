dd2ca31fabe8a4efcd00ab3fd22615d0026b431e
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-04 01:25:23 +0100

    WebSockets module doesn't rely on `cs\Index`'s form anymore

--- components/modules/WebSockets/admin/index.php
+++ components/modules/WebSockets/admin/index.php
@@ -6,14 +6,14 @@
  * @copyright Copyright (c) 2015-2016, Nazar Mokrynskyi
  * @license   MIT License, see license.txt
  */
-
 namespace cs\modules\WebSockets;
 use
 	h,
 	cs\Config,
 	cs\Index,
-	cs\Language\Prefix;
-$Index       = Index::instance();
+	cs\Language\Prefix,
+	cs\Page;
+
 $L           = new Prefix('websockets_');
 $Config      = Config::instance();
 $module_data = $Config->module('WebSockets');
@@ -38,42 +38,52 @@ if (isset($_POST['start_server']) && !is_server_running()) {
 			'listen_locally' => (int)$_POST['listen_locally']
 		]
 	);
-	$Index->save(true);
+	Index::instance()->save(true);
 }
-$Index->form_attributes['is'] = 'cs-form';
-$Index->content(
-	h::{'label info'}('websockets_security_key').
-	h::{'input[is=cs-input-text]'}(
-		[
-			'name'  => 'security_key',
-			'value' => $module_data->security_key
-		]
-	).
-	h::{'label info'}('websockets_listen_port').
-	h::{'input[is=cs-input-text]'}(
-		[
-			'name'  => 'listen_port',
-			'value' => $module_data->listen_port ?: 8080
-		]
-	).
-	h::{'label info'}('websockets_listen_on').
-	h::{'div radio'}(
-		[
-			'name'    => 'listen_locally',
-			'checked' => $module_data->listen_locally,
-			'value'   => [1, 0],
-			'in'      => ['127.0.0.1', '0.0.0.0']
-		]
-	).
-	h::{'label info'}('websockets_dns_server').
-	h::{'input[is=cs-input-text]'}(
-		[
-			'name'  => 'dns_server',
-			'value' => $module_data->dns_server ?: '127.0.0.1'
-		]
-	).
-	(is_server_running()
-		? h::{'p.cs-block-success.cs-text-success'}($L->server_is_running)
-		: h::{'p.cs-block-warning.cs-text-warning'}("$L->server_is_not_running. ".h::{'button[is=cs-button][name=start_server][type=submit]'}($L->start_server))
+Page::instance()->content(
+	h::{'form[is=cs-form]'}(
+		h::{'label info'}('websockets_security_key').
+		h::{'input[is=cs-input-text]'}(
+			[
+				'name'  => 'security_key',
+				'value' => $module_data->security_key
+			]
+		).
+		h::{'label info'}('websockets_listen_port').
+		h::{'input[is=cs-input-text]'}(
+			[
+				'name'  => 'listen_port',
+				'value' => $module_data->listen_port ?: 8080
+			]
+		).
+		h::{'label info'}('websockets_listen_on').
+		h::{'div radio'}(
+			[
+				'name'    => 'listen_locally',
+				'checked' => $module_data->listen_locally,
+				'value'   => [1, 0],
+				'in'      => ['127.0.0.1', '0.0.0.0']
+			]
+		).
+		h::{'label info'}('websockets_dns_server').
+		h::{'input[is=cs-input-text]'}(
+			[
+				'name'  => 'dns_server',
+				'value' => $module_data->dns_server ?: '127.0.0.1'
+			]
+		).
+		h::{'p button[is=cs-button][type=submit]'}(
+			$L->save,
+			[
+				'tooltip' => $L->save_info
+			]
+		).
+		(is_server_running()
+			? h::{'p.cs-block-success.cs-text-success'}($L->server_is_running)
+			: h::{'p.cs-block-warning.cs-text-warning'}(
+				"$L->server_is_not_running. ".
+				h::{'button[is=cs-button][name=start_server][type=submit]'}($L->start_server)
+			)
+		)
 	)
 );
