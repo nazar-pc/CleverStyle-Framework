21098ebe2c9da1e8fdf178352982f37cb6d34024
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-01-18 07:36:25 +0100

    WOFF2 support added

---
+++ components/modules/System/meta/update/3.149.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    CleverStyle CMS
+ * @subpackage System module
+ * @category   modules
+ * @author     Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright  Copyright (c) 2015-2016, Nazar Mokrynskyi
+ * @license    MIT License, see license.txt
+ */
+$htaccess = file_get_contents(DIR.'/.htaccess');
+$htaccess = str_replace(
+	'<FilesMatch "\.(css|js|gif|jpg|jpeg|png|ico|eot|ttc|ttf|svg|svgz|woff)$">',
+	'<FilesMatch "\.(css|js|gif|jpg|jpeg|png|ico|eot|ttc|ttf|svg|svgz|woff|woff2)$">',
+	$htaccess
+);
+file_put_contents(DIR.'/.htaccess', $htaccess);
