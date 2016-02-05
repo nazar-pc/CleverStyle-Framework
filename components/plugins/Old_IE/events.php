9d10261fd38f0effbfd98a3a3395b7031b0a2f67
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-02-05 23:05:05 +0100

    URL polyfiill added to Old_IE plugin

--- components/plugins/Old_IE/events.php
+++ components/plugins/Old_IE/events.php
@@ -24,6 +24,7 @@ Event::instance()->on(
 				h::{'link[rel=stylesheet][shim-shadowdom]'}(['href' => 'components/plugins/Old_IE/includes/css/hidden.css']).
 				h::script(['src' => 'components/plugins/Old_IE/includes/js/a.WeakMap.js']).
 				h::script(['src' => 'components/plugins/Old_IE/includes/js/b.MutationObserver.js']);
+				h::script(['src' => 'components/plugins/Old_IE/includes/js/c.URL.js']);
 		}
 	}
 );
