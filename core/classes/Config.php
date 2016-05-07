nazar-pc/CleverStyle-Framework

commit 59df2241bd5ec0d12e58380cbf46c759910b8c4f

Using `cs\CRUD` trait in `cs\Config` and some additional simplifications

core/classes/Config.php
@@ -19,6 +19,7 @@ namespace cs;
  */
 class Config {
 	use
+		CRUD,
 		Singleton;
 	const SYSTEM_MODULE = 'System';
 	const SYSTEM_THEME  = 'CleverStyle';
@@ -56,7 +57,18 @@ class Config {
 	 *
 	 * @var array
 	 */
-	public $mirrors;
+	public    $mirrors;
+	protected $data_model = [
+		'domain'     => 'text',
+		'core'       => 'json',
+		'db'         => 'json',
+		'storage'    => 'json',
+		'components' => 'json'
+	];
+	protected $table      = '[prefix]config';
+	protected function cdb () {
+		return 0;
+	}
 	/**
 	 * Loading of configuration, initialization of $Config, $Cache, $L and Page objects, Routing processing
 	 *
@@ -64,21 +76,7 @@ class Config {
 	 */
 	protected function construct () {
 		Event::instance()->fire('System/Config/init/before');
-		/**
-		 * Reading settings from cache and defining missing data
-		 */
-		$config = Cache::instance()->config;
-		/**
-		 * Cache reloading, if necessary
-		 */
-		if (!is_array($config)) {
-			$this->load_config_from_db();
-		} else {
-			foreach ($config as $part => $value) {
-				$this->$part = $value;
-			}
-			unset($part, $value);
-		}
+		$this->load_configuration();
 		date_default_timezone_set($this->core['timezone']);
 		$this->fill_mirrors();
 		Event::instance()->fire('System/Config/init/after');
@@ -110,30 +108,20 @@ class Config {
 	 *
 	 * @throws ExitException
 	 */
-	protected function load_config_from_db () {
-		$result = DB::instance()->qf(
-			[
-				"SELECT
-					`core`,
-					`db`,
-					`storage`,
-					`components`
-				FROM `[prefix]config`
-				WHERE `domain` = '%s'
-				LIMIT 1",
-				Core::instance()->domain
-			]
-		);
-		if (is_array($result)) {
-			foreach ($result as $part => $value) {
-				$this->$part = _json_decode($value);
+	protected function load_configuration () {
+		$config = Cache::instance()->get(
+			'config',
+			function () {
+				return $this->read(Core::instance()->domain);
 			}
-			unset($part, $value);
-		} else {
-			return false;
+		);
+		if (!$config) {
+			throw new ExitException('Failed to load system configuration', 500);
 		}
-		$this->apply_internal(false);
-		return true;
+		foreach ($config as $part => $value) {
+			$this->$part = $value;
+		}
+		return $this->apply_internal(false);
 	}
 	/**
 	 * Applying settings without saving changes into db
@@ -173,7 +161,7 @@ class Config {
 		) {
 			return false;
 		}
-		unset($Cache->{'languages'});
+		$Cache->del('languages');
 		date_default_timezone_set($this->core['timezone']);
 		$this->fill_mirrors();
 		Event::instance()->fire('System/Config/changed');
@@ -196,22 +184,7 @@ class Config {
 				unset($this->core[$key]);
 			}
 		}
-		if (DB::instance()->db_prime(0)->q(
-			"UPDATE `[prefix]config`
-			SET
-				`core`			= '%s',
-				`db`			= '%s',
-				`storage`		= '%s',
-				`components`	= '%s'
-			WHERE `domain` = '%s'
-			LIMIT 1",
-			_json_encode($this->core),
-			_json_encode($this->db),
-			_json_encode($this->storage),
-			_json_encode($this->components),
-			Core::instance()->domain
-		)
-		) {
+		if (!$this->update(Core::instance()->domain, $this->core, $this->db, $this->storage, $this->components)) {
 			$this->apply_internal(false);
 			return true;
 		}
@@ -233,7 +206,8 @@ class Config {
 	 * @throws ExitException
 	 */
 	function cancel () {
-		return $this->load_config_from_db() && $this->apply_internal(false);
+		Cache::instance()->del('config');
+		return $this->load_configuration();
 	}
 	/**
 	 * Get base url of current mirror including language suffix

core/traits/DB/Accessor.php
@@ -8,6 +8,7 @@
 namespace cs\DB;
 use
 	cs\DB;
+
 /**
  * Accessor trait
  *
@@ -18,29 +19,29 @@ trait Accessor {
 	 * Link to db object
 	 * @var false|_Abstract
 	 */
-	private $db = false;
+	private $_db = false;
 	/**
 	 * Link to primary db object
 	 * @var false|_Abstract
 	 */
-	private $db_prime = false;
+	private $_db_prime = false;
 	/**
 	 * Returns link to the object of db for reading (can be mirror of main DB)
 	 *
 	 * @return _Abstract
 	 */
 	function db () {
-		if (is_object($this->db)) {
-			return $this->db;
+		if (is_object($this->_db)) {
+			return $this->_db;
 		}
-		if (is_object($this->db_prime)) {
-			return $this->db = $this->db_prime;
+		if (is_object($this->_db_prime)) {
+			return $this->_db = $this->_db_prime;
 		}
 		/**
 		 * Save reference for faster access
 		 */
-		$this->db = DB::instance()->{(string)$this->cdb()}();
-		return $this->db;
+		$this->_db = DB::instance()->{(string)$this->cdb()}();
+		return $this->_db;
 	}
 	/**
 	 * Returns link to the object of db for writing (always main DB)
@@ -48,14 +49,14 @@ trait Accessor {
 	 * @return _Abstract
 	 */
 	function db_prime () {
-		if (is_object($this->db_prime)) {
-			return $this->db_prime;
+		if (is_object($this->_db_prime)) {
+			return $this->_db_prime;
 		}
 		/**
 		 * Save reference for faster access
 		 */
-		$this->db_prime = DB::instance()->{(string)$this->cdb()}();
-		return $this->db_prime;
+		$this->_db_prime = DB::instance()->{(string)$this->cdb()}();
+		return $this->_db_prime;
 	}
 	/**
 	 * Returns database index

commit 0d010509aee6978159484c50026673c46f6f82dd

Fix for bug introduced in 59df224

core/classes/Config.php
@@ -185,10 +185,9 @@ class Config {
 			}
 		}
 		if (!$this->update(Core::instance()->domain, $this->core, $this->db, $this->storage, $this->components)) {
-			$this->apply_internal(false);
-			return true;
+			return false;
 		}
-		return false;
+		return $this->apply_internal(false);
 	}
 	/**
 	 * Whether configuration was applied (not saved) and can be canceled