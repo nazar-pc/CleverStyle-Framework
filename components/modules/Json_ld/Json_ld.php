03983a3a1446cc666051b188ca759d14c830491d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-06-17 03:55:21 +0300

    Json ld is module now instead of plugin, no code changes

--- components/modules/Json_ld/Json_ld.php
+++ components/modules/Json_ld/Json_ld.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package   Json_ld
- * @category  plugins
+ * @category  modules
  * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @copyright Copyright (c) 2015-2016, Nazar Mokrynskyi
  * @license   MIT License, see license.txt
  */
 
-namespace cs\plugins\Json_ld;
+namespace cs\modules\Json_ld;
 use
 	cs\User;
 
