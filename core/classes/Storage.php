364b6f083a68f33666cd4ac10f728faaa9f81883
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-07-29 13:15:56 +0300

    Added tests for `cs\Storage` class. Refactoring and simplifications in `cs\Storage` class. Potentially breaking change: `cs\Storage::get_connections_list()` method now requires first argument to be specified explicitly, also requires arguments to be `cs\Storage::CONNECTIONS_*` constants. New constants: * `cs\Storage::CONNECTIONS_ACTIVE` * `cs\Storage::CONNECTIONS_SUCCESSFUL` * `cs\Storage::CONNECTIONS_FAILED`

--- core/classes/Storage.php
+++ core/classes/Storage.php
@@ -11,105 +11,91 @@ namespace cs;
  * @method static $this instance($check = false)
  */
 class Storage {
-	use    Singleton;
+	use
+		Singleton;
+	const CONNECTIONS_ACTIVE     = 0;
+	const CONNECTIONS_SUCCESSFUL = 1;
+	const CONNECTIONS_FAILED     = 2;
 	/**
-	 * @var Storage\_Abstract[]|False_class[]
+	 * @var array[]
 	 */
-	protected $connections            = [];
-	protected $successful_connections = [];
-	protected $failed_connections     = [];
+	protected $connections = [
+		self::CONNECTIONS_ACTIVE     => [],
+		self::CONNECTIONS_SUCCESSFUL => [],
+		self::CONNECTIONS_FAILED     => []
+	];
 	/**
 	 * Get list of connections of specified type
 	 *
-	 * @param bool|null|string $status `null`    - returns array of connections with objects<br>
-	 *                                 `true|1`  - returns array of names of successful connections<br>
-	 *                                 `false|0` - returns array of names of failed connections
+	 * @param int $type One of constants `self::CONNECTIONS_*`
 	 *
-	 * @return array|null
+	 * @return array For `self::CONNECTIONS_ACTIVE` array of successful connections with corresponding objects as values of array, otherwise array where keys
+	 *               are storage ids and values are strings with information about storage
 	 */
-	public function get_connections_list ($status = null) {
-		if ($status === null) {
-			return $this->connections;
-		} elseif ($status == 0) {
-			return $this->failed_connections;
-		} elseif ($status == 1) {
-			return $this->successful_connections;
-		}
-		return null;
+	public function get_connections_list ($type) {
+		return $this->connections[$type];
 	}
 	/**
 	 * Processing of getting storage instance
 	 *
-	 * @param int $connection
+	 * @param int $storage_id
 	 *
 	 * @return Storage\_Abstract|False_class
 	 */
-	public function storage ($connection) {
-		if (!is_int($connection) && $connection != '0') {
-			return False_class::instance();
-		}
-		return $this->connecting($connection);
-	}
-	/**
-	 * Processing of all storage requests
-	 *
-	 * @param int $connection
-	 *
-	 * @return Storage\_Abstract|False_class
-	 */
-	protected function connecting ($connection) {
+	public function storage ($storage_id) {
 		/**
 		 * If connection found in list of failed connections - return instance of False_class
 		 */
-		if (isset($this->failed_connections[$connection])) {
+		if (isset($this->connections[self::CONNECTIONS_FAILED][$storage_id])) {
 			return False_class::instance();
 		}
 		/**
 		 * If connection already exists - return reference on the instance of Storage engine object
 		 */
-		if (isset($this->connections[$connection])) {
-			return $this->connections[$connection];
+		if (isset($this->connections[self::CONNECTIONS_ACTIVE][$storage_id])) {
+			return $this->connections[self::CONNECTIONS_ACTIVE][$storage_id];
 		}
-		$Config = Config::instance();
 		/**
 		 * If connection to the local storage
 		 */
-		if ($connection == 0) {
-			$Core                  = Core::instance();
-			$storage['connection'] = $Core->storage_type;
-			$storage['url']        = $Core->storage_url;
-			$storage['host']       = $Core->storage_host;
-			$storage['user']       = $Core->storage_user;
-			$storage['password']   = $Core->storage_password;
-		} elseif (isset($Config->storage[$connection])) {
-			$storage = &$Config->storage[$connection];
+		if ($storage_id == 0) {
+			$Core    = Core::instance();
+			$storage = [
+				'connection' => $Core->storage_type,
+				'url'        => $Core->storage_url,
+				'host'       => $Core->storage_host,
+				'user'       => $Core->storage_user,
+				'password'   => $Core->storage_password
+			];
 		} else {
-			return False_class::instance();
+			$storage = Config::instance()->storage[$storage_id];
 		}
 		/**
-		 * Create new Storage connection
+		 * Establish new connection
+		 *
+		 * @var Storage\_Abstract $connection
 		 */
-		$engine_class                   = "\\cs\\Storage\\$storage[connection]";
-		$this->connections[$connection] = new $engine_class($storage['url'], $storage['host'], $storage['user'], $storage['password']);
+		$engine_class    = "\\cs\\Storage\\$storage[connection]";
+		$connection      = new $engine_class(
+			$storage['url'],
+			$storage['host'],
+			$storage['user'],
+			$storage['password']
+		);
+		$connection_name = "$storage_id/$storage[host]/$storage[connection]";
 		/**
-		 * If successfully - add connection to the list of success connections and return instance of DB engine object
+		 * If successfully - add connection to the list of success connections and return instance of Storage engine object
 		 */
-		if (is_object($this->connections[$connection]) && $this->connections[$connection]->connected()) {
-			$this->successful_connections[] = "$connection/$storage[host]/$storage[connection]";
-			unset($storage);
-			$this->$connection = $this->connections[$connection];
-			return $this->connections[$connection];
-			/**
-			 * If failed - add connection to the list of failed connections and display connection error
-			 */
-		} else {
-			unset($this->$connection);
-			$this->failed_connections[$connection] = "$connection/$storage[host]/$storage[connection]";
-			unset($storage);
-			trigger_error('Error connecting to storage '.$this->failed_connections[$connection], E_USER_WARNING);
-			$return        = False_class::instance();
-			$return->error = 'Connection failed';
-			return $return;
+		if (is_object($connection) && $connection->connected()) {
+			$this->connections[self::CONNECTIONS_ACTIVE][$storage_id] = $connection;
+			$this->connections[self::CONNECTIONS_SUCCESSFUL][]        = $connection_name;
+			return $connection;
 		}
+		/**
+		 * If failed - add connection to the list of failed connections and log error
+		 */
+		$this->connections[self::CONNECTIONS_FAILED][$storage_id] = $connection_name;
+		trigger_error("Error connecting to storage $connection_name", E_USER_WARNING);
+		return False_class::instance();
 	}
 }
---
+++ tests/quick/Storage/Storage_basic.phpt
@@ -0,0 +1,133 @@
+--FILE--
+<?php
+namespace cs\Storage {
+	include __DIR__.'/../../unit.php';
+	class Fake extends _Abstract {
+		/**
+		 * @var bool
+		 */
+		public static $connected_fake = true;
+		public function __construct ($base_url, $host, $user = '', $password = '') {
+			$this->base_url  = $base_url;
+			$this->connected = Fake::$connected_fake;
+		}
+		public function get_files_list (
+			$dir,
+			$mask = false,
+			$mode = 'f',
+			$prefix_path = false,
+			$subfolders = false,
+			$sort = false,
+			$exclusion = false,
+			$system_files = false,
+			$apply = null,
+			$limit = null
+		) {
+		}
+		public function file ($filename, $flags = null) { }
+		public function file_get_contents ($filename, $flags = null) { }
+		public function file_put_contents ($filename, $data, $flags = null) { }
+		public function copy ($source, $dest) { }
+		public function unlink ($filename) { }
+		public function file_exists ($filename) { }
+		public function rename ($oldname, $newname) { }
+		public function mkdir ($pathname, $mode = 0777, $recursive = false) { }
+		public function rmdir ($dirname) { }
+		public function is_file ($filename) { }
+		public function is_dir ($filename) { }
+		public function url_by_source ($source) { }
+		public function source_by_url ($url) { }
+	}
+}
+namespace cs {
+	function trigger_error ($error) {
+	}
+
+	Config::instance_stub(
+		[
+			'storage' => [
+				1 => [
+					'connection' => 'Fake',
+					'url'        => 'http://cscms.travis/storage/public',
+					'host'       => 'localhost',
+					'user'       => 'user',
+					'password'   => 'storage 1'
+				]
+			]
+		]
+	);
+	Core::instance_stub(
+		[
+			'storage_type'     => 'Fake',
+			'storage_url'      => 'http://cscms.travis/storage/public',
+			'storage_host'     => 'localhost',
+			'storage_user'     => 'user',
+			'storage_password' => 'storage 0'
+		]
+	);
+
+	var_dump('Get storage instance');
+	$Storage  = Storage::instance();
+	$instance = $Storage->storage(0);
+	var_dump($instance instanceof Storage\Fake);
+
+	var_dump('Get same storage instance');
+	var_dump($Storage->storage(0) === $instance);
+
+	var_dump('Connections list (active, successful, failed)');
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_ACTIVE));
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_SUCCESSFUL));
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_FAILED));
+
+	var_dump('Connection fails');
+	Storage\Fake::$connected_fake = false;
+	Storage::instance_reset();
+	$Storage  = Storage::instance();
+	$instance = $Storage->storage(1);
+	var_dump($instance instanceof False_class);
+
+	var_dump('Get failed instance again');
+	Storage\Fake::$connected_fake = false;
+	Storage::instance_reset();
+	var_dump($Storage->storage(1) === $instance);
+
+	var_dump('Connections list (active, successful, failed)');
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_ACTIVE));
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_SUCCESSFUL));
+	var_dump($Storage->get_connections_list(Storage::CONNECTIONS_FAILED));
+}
+?>
+--EXPECTF--
+string(20) "Get storage instance"
+bool(true)
+string(25) "Get same storage instance"
+bool(true)
+string(45) "Connections list (active, successful, failed)"
+array(1) {
+  [0]=>
+  object(cs\Storage\Fake)#%d (%d) {
+    ["connected":protected]=>
+    bool(true)
+    ["base_url":protected]=>
+    string(34) "http://cscms.travis/storage/public"
+  }
+}
+array(1) {
+  [0]=>
+  string(16) "0/localhost/Fake"
+}
+array(0) {
+}
+string(16) "Connection fails"
+bool(true)
+string(25) "Get failed instance again"
+bool(true)
+string(45) "Connections list (active, successful, failed)"
+array(0) {
+}
+array(0) {
+}
+array(1) {
+  [1]=>
+  string(16) "1/localhost/Fake"
+}
