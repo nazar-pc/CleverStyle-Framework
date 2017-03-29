663a82cc5beffe18085188f6977bbf1353e702cb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-03-29 17:30:33 +0300

    Tiny simplification

--- core/classes/Page/Assets/RequireJS.php
+++ core/classes/Page/Assets/RequireJS.php
@@ -114,8 +114,7 @@ class RequireJS {
 	protected static function find_package_npm ($dir, $allowed_extensions) {
 		$package = @file_get_json("$dir/package.json");
 		// If we have browser-specific declaration - use it
-		/** @noinspection NestedTernaryOperatorInspection */
-		$main = $package['browser'] ?? ($package['jspm']['main'] ?? @$package['main']);
+		$main = $package['browser'] ?? $package['jspm']['main'] ?? @$package['main'];
 		if (preg_match('/\.js$/', $main)) {
 			$main = substr($main, 0, -3);
 		}
