2c4aacb5bfb6036186b11d88a8e13778b8ce26e0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-05-12 06:19:53 +0300

    New upstream version of html5sortable

--- modules/System/events.php
+++ modules/System/events.php
@@ -129,7 +129,7 @@ Event::instance()
 			$data['paths'] += [
 				'jssha'         => DIR.'/assets/js/modules/jsSHA-2.1.0',
 				'autosize'      => DIR.'/assets/js/modules/autosize.min',
-				'html5sortable' => DIR.'/assets/js/modules/html5sortable-0.5.1.min',
+				'html5sortable' => DIR.'/assets/js/modules/html5sortable-0.5.3.min',
 				'sprintf-js'    => DIR.'/assets/js/modules/sprintf-1.1.0.min'
 			];
 		}
